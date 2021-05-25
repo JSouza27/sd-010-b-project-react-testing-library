9359fb6e4a0b58bd2d1048a7444155a997b304fd
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-05-25 18:22:54 -0300

    Requisito 2 realizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { getByText, render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import RenderWithRouter from './RenderWithRouter';
 
 import App from '../App';
 
-describe('testes do componente App.js ', () => {
+describe('testes do componente <App.js /> ', () => {
   test('testa se a rota "/" leva para a pagina principal', () => {
     const { history } = RenderWithRouter(<App />);
     history.push('/');
@@ -87,6 +85,68 @@ describe('Testa o redirecionamento  das páginas', () => {
     expect(imgDaNotFound).toBeInTheDocument();
   });
 });
-describe('', () => {
 
+describe('Testando do componente <About.js/>', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    RenderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', {
+      name: /About/i,
+    });
+
+    userEvent.click(linkAbout);
+
+    const h1 = screen.getByText('this application simulates a pokédex', { exact: false });
+    expect(h1).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    RenderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', {
+      name: /About/i,
+    });
+    userEvent.click(linkAbout);
+
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+
+    expect(h2).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    RenderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', {
+      name: /About/i,
+    });
+    userEvent.click(linkAbout);
+
+    const firstParagraph = screen.getByText(
+      'This application simulates a Pokédex', { exact: false },
+    );
+    expect(firstParagraph).toBeInTheDocument();
+
+    const secondParagraph = screen.getByText(
+      'This application simulates a Pokédex', { exact: false },
+    );
+    expect(secondParagraph).toBeInTheDocument();
+  });
+  test('Teste se a página contém uma determinada imagem de uma Pokédex', () => {
+    RenderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', {
+      name: /About/i,
+    });
+    userEvent.click(linkAbout);
+
+    const specificImg = screen.getByRole('img', {
+      name: /Pokédex/i,
+    });
+
+    expect(specificImg).toHaveAttribute(
+      'src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
 });
