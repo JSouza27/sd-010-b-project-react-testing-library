17cc78cbda08e65e0ac4fdd14f2fdecad11fbe04
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-25 17:49:15 -0300

    Teste do heading do Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRoute from './renderWithRoute';
+import App from '../App';
+
+test('Testando se renderiza o texto "Encountered pokémons" na tela', () => {
+  const { getByRole } = renderWithRoute(<App />);
+
+  const heading = getByRole('heading', {
+    level: 2,
+  });
+
+  expect(heading).toHaveTextContent('Encountered pokémons');
+});
