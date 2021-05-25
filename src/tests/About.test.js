781c2edd21e445fc6733a6b3f74cf9259f425504
nascimento <esio.392@protonmail.com>
2021-05-25 18:15:06 -0400

    add arquivo renderWithRouter, tentando req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste component About', () => {
+  test('Pagina contem um heading com text About Pokedex', () => {
+    renderWithRouter(<App />);
+
+    const about = screen.getByRole('link', {
+      name: 'About',
+    });
+    userEvent.click(about);
+    const aboutText = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutText).toBeInTheDocument();
+  });
+});
