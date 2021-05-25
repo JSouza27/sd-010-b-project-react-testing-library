9ab74e724449eef1ea1d1253e9ff9093ffb79b7a
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-25 16:53:24 -0300

    testa se a pagina contem a imagem da pokedex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -16,4 +16,11 @@ describe('Testando o componente about', () => {
     expect(paragraph1).toBeInTheDocument();
     expect(paragraph2).toBeInTheDocument();
   });
+
+  it('Testa se a página contém a imagem da Pokédex', () => {
+    const { getByRole } = render(<About />);
+    const pokedexImage = getByRole('img');
+    expect(pokedexImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(pokedexImage).toHaveAttribute('alt', 'Pokédex');
+  });
 });
