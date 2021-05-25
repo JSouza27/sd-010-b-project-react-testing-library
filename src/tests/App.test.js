d46b110b274c071fa9f266754a1b637c6763d57a
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-25 15:16:28 -0300

    req 1 home, about, favorite

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouters';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testa requisito 1 App.js', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(
+      <App />,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('shows the Pokédex when the route is "/" ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const route = '/';
+    history.push(route);
+
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favorite = getByText('Favorite Pokémons');
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 });
