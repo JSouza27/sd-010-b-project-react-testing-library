ae6dc3f86d2d3d3559d9f095bf5f95c1ae34d71b
Emerson Saturnino <programador.emerson@hotmail.com>
2021-05-25 16:17:48 -0300

    Verificando os links no documento

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
 describe('Teste requisito1', () => {
@@ -11,5 +12,16 @@ describe('Teste requisito1', () => {
       </MemoryRouter>,
     );
     expect(getByText('Pokédex')).toBeInTheDocument();
+  });
+
+  test('O topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favorite = getByText('Favorite Pokémons');
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
   });
 });
