a6a106e442fc417402867af179c8572d28788533
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-05-25 18:02:26 -0300

    req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  const linkToAbout = screen.getByRole('link', {
+    name: /About/i,
+  });
+  userEvent.click(linkToAbout);
+  const h2 = screen.getByRole('heading', {
+    level: 2,
+    name: /About Pokédex/i,
+  });
+  expect(h2).toBeInTheDocument();
+
+  const p1 = screen.getByText(/This application simulates a Pokédex/i);
+  expect(p1).toBeInTheDocument();
+
+  const p2 = screen.getByText(/One can filter Pokémons by type,/i);
+  expect(p2).toBeInTheDocument();
+
+  const img = screen.getByRole('img');
+  expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
