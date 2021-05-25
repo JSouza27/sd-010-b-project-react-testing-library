bccf1b910e7f6a93bda50fb25ee498f3a05de920
EderPaiva <edergomesdepaiva12@gmail.com>
2021-05-25 18:08:36 -0300

    lint configurado conteudo 1 sendo aprovado mas falta testar os links interagindo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,52 +1,50 @@
 import React from 'react';
-import { getByRole, render, screen } from '@testing-library/react';
-import App from '../App';
-import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import App from '../App';
 // component
 
 describe('Check if Pokedex is the home page and if the route links work ', () => {
-     // Home
-  test('check home', () => {
-      const history = createMemoryHistory();    // checando a home page
-        render( <Router history={ history }>
-                  <App />
-                </Router>); 
-        // page have a home button?
-        const home = screen.getByRole('link', {
-          name :'Home',
-        });
-        expect(home).toBeInTheDocument();      
+  // Home
+  test('Check home', () => {
+    const history = createMemoryHistory(); // checando a home page
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    // page have a home button?
+    const home = screen.getByRole('link', {
+      name: 'Home',
     });
-    // About
-    test('check About', () => {
-      const history = createMemoryHistory();    // checando a home page
-        render( <Router history={ history }>
-                  <App />
-                </Router>); 
-        const About = screen.getByRole('link', {
-          name :'About',
-        });
-        expect(About).toBeInTheDocument();      
+    expect(home).toBeInTheDocument();
+  });
+  // About
+  test('Check About', () => {
+    const history = createMemoryHistory(); // checando a home page
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const About = screen.getByRole('link', {
+      name: 'About',
     });
-        // About
-    test('check Favorite Pokémons', () => {
-      const history = createMemoryHistory();    // checando a home page
-        render( <Router history={ history }>
-                  <App />
-                </Router>); 
-        // page have a home button?
-        const favoritePokemons = screen.getByRole('link', {
-          name :'Favorite Pokémons',
-        });
-        expect(favoritePokemons).toBeInTheDocument();      
+    expect(About).toBeInTheDocument();
+  });
+  // About
+  test('Check Favorite Pokémons', () => {
+    const history = createMemoryHistory(); // checando a home page
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    // page have a home button?
+    const favoritePokemons = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
     });
-
+    expect(favoritePokemons).toBeInTheDocument();
+  });
 });
-// test('renders a reading with the text `Pokédex`', () => {
-//   const { getByText } = render(
-
-//   );
-//   const heading = getByText(/Pokédex/i);
-//   expect(heading).toBeInTheDocument();
-// });
