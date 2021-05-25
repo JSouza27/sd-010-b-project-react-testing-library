d8b4b95210c4c152da9862a688e621372b68bf37
Carlos Vieira <appencigr@gmail.com>
2021-05-25 16:17:47 -0300

    Alterei o idioma para inglês!

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,8 +3,8 @@ import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-describe('Testando App', () => {
-test('shows the Pokédex when the route is `/`', () => {
+describe('Testing App', () => {
+  test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={ ['/'] }>
       <App />
