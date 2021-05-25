1057a2da5eaca4d4e28b5d608f07c4837129e6f9
Cristian <cristian.bugs@gmail.com>
2021-05-25 20:52:01 -0300

    third task completed and second task partialy ccompleted

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import { About } from '../components';
+
+test('check if the about page has a heading named Pokedex', () => {
+  renderWithRouter(<About />);
+
+  const aboutMeText = screen.getByRole('heading', {
+    level: 2,
+    name: /Pokédex/i,
+  });
+  expect(aboutMeText).toBeInTheDocument();
+});
+
+test('check if there are two paragraphs', () => {
+  renderWithRouter(<About />);
+
+  const paragraphs = screen.getByText((element) => element.toContain('One can filter'));
+
+  expect(paragraphs).toBeInTheDocument();
+});
+
+test('check if there is an image of a pokedex', () => {
+  renderWithRouter(<About />);
+
+  const image = screen.getByAltText('Pokédex');
+
+  expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
+
+// https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src;
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './RenderWithRouter';
+
+test('check if a message appears when there is no favorite pokemon', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const message = screen.getByText('No favorite pokemon found');
+
+  expect(message).toBeInTheDocument();
+});
