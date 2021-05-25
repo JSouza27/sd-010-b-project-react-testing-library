af846b2ff628904d8d9db2de22d8b47fe4eba1af
renanpbraga <renanpbraga@gmail.com>
2021-05-25 15:56:01 -0300

    adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import RenderWithRouter from '../RenderWithRouter';
+import userEvent from '@testing-library/user-event'; 
 import { render } from '@testing-library/react';
 import App from '../App';
 
@@ -11,4 +13,22 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('Verifica se o link com o texto Home existe', () => {
+  const { getByText } = RenderWithRouter(<App />);
+  const home = getByText(/Home/);
+  expect(home).toBeInTheDocument();
+});
+
+test('Verifica se o link com o texto About existe', () => {
+  const { getByText } = RenderWithRouter(<App />);
+  const about = getByText(/About/);
+  expect(about).toBeInTheDocument();
+});
+
+test('Verifica se o link com o texto Favorite Pokémons existe', () => {
+  const { getByText } = RenderWithRouter(<App />);
+  const favPkmn = getByText(/Favorite Pokémons/);
+  expect(favPkmn).toBeInTheDocument();
 });
