f22eb7bc69ccc30d531acf76604edaeac3fb8b82
joao966 <joaooliveira966@gmail.com>
2021-05-25 19:45:47 -0300

    add: req-1 && req-2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+
+import About from '../components/About';
+
+describe('Testa about', () => {
+  test('testa h2', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const about = getByRole('heading', { level: 2 });
+    expect(about).toHaveTextContent('About Pokédex');
+  });
+
+  test('Testa se renderiza 2 paragrafos, com as informações da Pokedex', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const texts = getAllByText(/Pokédex/i);
+    expect(texts[0]).toBeInTheDocument();
+    expect(texts[1]).toBeInTheDocument();
+  });
+
+  test('Testa se renderiza a imagem da Pokedex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = getByRole('img');
+    expect(img).toHaveAttribute('src', expect.stringContaining(url));
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('testa app', () => {
+  test('Testa se há um h1 com o texto pokédex', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const h1 = getByRole('heading', { level: 1 });
+    expect(h1).toHaveTextContent(/Pokédex/i);
+  });
+
+  test('Menu com os links: Home, About e Favorite Pokémons', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+
+    const menu = getAllByRole('link');
+    expect(menu[0]).toHaveTextContent('Home');
+    expect(menu[1]).toHaveTextContent('About');
+    expect(menu[2]).toHaveTextContent('Favorite Pokémons');
+  });
+
+  test('Ao clicar no link redireciona a pagina', () => {
+    const { getByText, getAllByRole, history } = renderWithRouter(<App />);
+
+    const menu = getAllByRole('link');
+
+    expect(history.location.pathname).toBe('/');
+
+    fireEvent.click(menu[0]);
+    expect(history.location.pathname).toBe('/');
+
+    fireEvent.click(menu[1]);
+    expect(history.location.pathname).toBe('/about');
+
+    fireEvent.click(menu[2]);
+    expect(history.location.pathname).toBe('/favorites');
+
+    history.push('/notfound');
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
+
+  test('url inválido renderiza o componente NotFound ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/notfound');
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
 });
-bora
