76b6074e998ee4c9dceb6f90e6f73a894c997ec3
Thiago Marchini <thiago@msn.com>
2021-05-25 15:14:10 -0300

    feito o teste o App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
@@ -27,10 +27,10 @@ test('renders the three navigation links', () => {
 
 test('Test if About link directs to home', () => {
   const { getByText, history } = renderWithRouter(<App />);
-  const about = getByText('About');
-  userEvent.click(about);
+  const home = getByText('Home');
+  userEvent.click(home);
   const { pathname } = history.location;
-  expect(pathname).toBe('/about');
+  expect(pathname).toBe('/');
 });
 
 test('Test if About link directs to home', () => {
@@ -47,6 +47,18 @@ test('Test if Favorite Pokémons link directs to Favorite Pokémons', () => {
   userEvent.click(favorite);
   const { pathname } = history.location;
   expect(pathname).toBe('/favorites');
+});
+
+test('test if unrecognized path redirects to NotFound', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/xablau');
+  render(
+    <MemoryRouter history={ history }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText(/page requested not found/i)).toBeInTheDocument();
 });
 
 test('shows the Pokédex when the route is `/`', () => {
