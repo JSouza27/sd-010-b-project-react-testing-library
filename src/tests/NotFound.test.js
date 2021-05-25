c382d29af946052632434ed4d8da08079f278a60
Carlos Vieira <appencigr@gmail.com>
2021-05-25 19:33:26 -0300

    Requisito 4-Iniciado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Testing App', () => {
+  test('', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/', '/about', '/favorites'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const homeText = screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+
+    expect(homeText).toBeInTheDocument();
+  });
+});
