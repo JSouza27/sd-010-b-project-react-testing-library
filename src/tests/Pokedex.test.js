3352b41242369b44b6d9351837cee2e613db70b4
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-25 18:49:55 -0300

    Testa se a pag contem um headig h2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando página Pokedex', () => {
+  it('Testa se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const h2 = getByText(/Encountered pokémons/i);
+    expect(h2).toBeInTheDocument();
+  });
+});
