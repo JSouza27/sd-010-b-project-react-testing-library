de06dc96499d195a278e439b765f770f84f861ef
Daniel <darkdanielr@gmail.com>
2021-05-25 16:35:21 -0300

    3 finish

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  const paragraph = getByText('No favorite pokemon found');
+  expect(paragraph).toBeInTheDocument();
+});
