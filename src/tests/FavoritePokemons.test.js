df11c17bff8fabb14f47cafd6591c686c3b6b360
Thiago Kachinsky <tjk091@gmail.com>
2021-05-25 18:32:33 -0300

    Requisito 4, testando o componente <NotFound />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+// import React from 'react';
+// import { fireEvent, screen } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+// import FavoritePokemons from '../components/FavoritePokemons';
+
+// describe('Testando o componente Favorite Pokemons', () => {
+//   // test('Renderiza o texto `No favorite pokemon found`', () => {
+//   //   renderWithRouter(<FavoritePokemons />);
+
+//   //   const noPokemonsFound = screen.getByText(/No favorite pokemon found/i);
+//   //   expect(noPokemonsFound).toBeInTheDocument();
+//   // });
+//   // test('É exibido todos os cards de pokémons favoritados.', () => {
+//   //   renderWithRouter(<FavoritePokemons />);
+
+//   // });
+//   // test('Nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+//   //   renderWithRouter(<FavoritePokemons />);
+
+//   // });
+// });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente <NotFound />', () => {
+  test('Contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
+    const pageNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+
+    expect(pageNotFound).toBeInTheDocument();
+  });
+
+  test('Mostra uma imagem específica', () => {
+    renderWithRouter(<NotFound />);
+
+    const alt = 'Pikachu crying because the page requested was not found';
+    const logo = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const imageNotFound = screen.getByAltText(alt);
+    expect(imageNotFound).toBeInTheDocument();
+
+    const imageAbout = screen.getAllByRole('img');
+    expect(imageAbout[1]).toHaveAttribute('src', logo);
+  });
+});
