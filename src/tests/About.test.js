b1de08a199976ad0fcadf358f998a0f5971fcfaf
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-25 16:59:54 -0300

    finalizando a 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import { About } from '../components';
 
 test('pagina About teste se posssui informações sobre a Pokédex', () => {
   const { getByText, history } = renderWithRouter(<App />);
   fireEvent.click(getByText(/About/i));
-  const { pathname } = history.location.pathname;
-  expect(pathname).toBe('/About');
-  const aboutAll = getByText(/Pokédex/);
+  const { location: { pathname } } = history;
+  expect(pathname).toBe('/about');
+  const aboutAll = getByText(/About Pokédex/);
   expect(aboutAll).toBeInTheDocument();
+});
+
+test('pagina About pussui imagem da Pokédex', () => {
+  const { getByRole } = render(<About />);
+  const img = getByRole('img');
+  expect(img).toBeInTheDocument();
+  expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  expect(img).toHaveAttribute('alt', 'Pokédex');
 });
