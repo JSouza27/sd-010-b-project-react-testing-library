648cf7e5200b07831c1e1a03ee9db4e3510bf596
RafaMaths <rmathscoach@gmail.com>
2021-05-25 18:02:04 -0300

    test image in Pokédex page

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -20,4 +20,23 @@ describe('Teste o componente <About.js /.', () => {
     const textH2 = screen.getByRole('heading', { name: /about pokédex/i });
     expect(textH2).toBeInTheDocument();
   });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const firstParagraph = screen.getByText('ication simulates a Pokédex', {
+      exact: false });
+    expect(firstParagraph).toBeInTheDocument();
+
+    const secondParagraph = screen.getByText('Pokémons by type,', { exact: false });
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const getImage = screen.getByRole('img', { name: /pokédex/i });
+    expect(getImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    /* fonte para uso do toHaveAttribute <https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f> */
+  });
 });
