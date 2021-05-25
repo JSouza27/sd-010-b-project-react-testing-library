22f5ee1a62459390cfee980987353ce5e29844cd
Thiago Leite <leite.tjs@gmail.com>
2021-05-25 18:34:50 -0300

    4th requirement done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testint the 4th requirement: NotFound component', () => {
+  it('render h2 with text "Page request not found"', () => {
+    const { getByRole } = render(<NotFound />);
+    const message = getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+      level: 2,
+    });
+    expect(message).toBeInTheDocument();
+  });
+  it('render image with a crying pikachu', () => {
+    const { getByRole } = render(<NotFound />);
+    const image = getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
