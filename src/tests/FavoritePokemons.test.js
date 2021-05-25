dab06d2b1f4d4a6ba15980e4c80e3a2e7143d4af
Felippe Correa <felippecorrea2010@gmail.com>
2021-05-25 18:57:17 -0300

    Adicionando requisito 3 e corrigindo eslint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
+
+describe('Test the component FavoritePokemons', () => {
+  test('renders text "No favorite pokemon found", if person not have pokemons', () => {
+    const { getByText, getByRole, history } = renderWithRouter(<App />);
+
+    const linkFavoritePokemons = getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    fireEvent.click(linkFavoritePokemons);
+    const FavoritePage = history.location.pathname;
+    expect(FavoritePage).toBe('/favorites');
+
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
