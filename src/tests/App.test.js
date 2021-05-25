1dd2e1b534cfe12d8ea1dba6ccfdcb0ce34bd6ea
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-05-25 15:16:17 -0300

    Teste 1a parte requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+// it('renders a reading with the text `Pokédex`', () => {
+//   const { getByText } = render(
+//     <MemoryRouter>
+//       <App />
+//     </MemoryRouter>,
+//   );
+//   const heading = getByText(/Pokédex/i);
+//   expect(heading).toBeInTheDocument();
+// });
+
+it('shows the Pokédex when the route is `/`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const rota = '/';
+  history.push(rota);
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/');
+    expect(getByText('Home')).toBeInTheDocument();
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('O primeiro link deve possuir o texto About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/');
+    expect(getByText('About')).toBeInTheDocument();
+  });
+
+  it('O primeiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/');
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
 });
