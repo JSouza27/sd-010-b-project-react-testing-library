30033d670857e359def70b41441d24104a8e8fb6
denis-rossati <denis.rossati@hotmail.com>
2021-05-25 15:44:17 -0300

    requisito 3 - concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Test "FavoritePokemons" component', () => {
+  it('Check if the page is empty', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const emptyPokemons = getByText('No favorite pokemon found');
+    expect(emptyPokemons).toBeInTheDocument();
+    expect(emptyPokemons).toHaveTextContent('No favorite pokemon found');
+  });
+});
