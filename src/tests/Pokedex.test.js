b00cd4b29fc4c781b5a0f33a1f18751edf803e43
Emerson Saturnino <programador.emerson@hotmail.com>
2021-05-25 19:56:34 -0300

    Teste se página contém um heading h2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    }));
+  });
+});
