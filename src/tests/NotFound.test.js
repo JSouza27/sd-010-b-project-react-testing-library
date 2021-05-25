74a0629440ec42b6a6695031a4ae99cd3ca6fb8c
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-25 18:16:05 -0300

    adicionando mais um expect no primeiro teste

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,8 +4,10 @@ import NotFound from '../components/NotFound';
 
 describe('Testando página Not found', () => {
   it('Testa se a página contém um heading h2 com texto page requested not found', () => {
-    const { getByRole } = render(<NotFound />);
+    const { getByRole, getByLabelText } = render(<NotFound />);
+    const cryingEmoji = getByLabelText(/Crying emoji/i);
     const h2 = getByRole('heading', { level: 2, name: /Page requested not found/i });
     expect(h2).toBeInTheDocument();
+    expect(cryingEmoji).toBeInTheDocument();
   });
 });
