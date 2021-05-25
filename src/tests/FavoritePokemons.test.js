0ad87d89b29c8c15167dc6303fd57b42014695c8
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-05-25 18:36:11 -0300

    Colocando os testes nos arquivos corretos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RenderWithRouter from './RenderWithRouter';
+
+import App from '../App';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido na tela a mensagem No favorite', () => {
+    const { history } = RenderWithRouter(<App />);
+  });
+});
