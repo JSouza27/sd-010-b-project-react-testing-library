70d4c03089d16c84173082a2b7d1aa7a7b471db0
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-25 15:30:27 -0300

    teste de renderizaçao da pagina about

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,11 @@
+import { fireEvent } from '@testing-library/dom';
 import React from 'react';
 // import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testando App.js', () => {
-  it('Renderiza a página home quando o documento é aberto', () => {
+  it('Renderiza a página home e os links quando o documento é aberto', () => {
     const { getByText } = renderWithRouter(<App />);
     const heading = getByText(/Pokédex/i);
     const homeLink = getByText(/Home/i);
@@ -15,5 +16,16 @@ describe('Testando App.js', () => {
     expect(homeLink).toBeInTheDocument();
     expect(aboutLink).toBeInTheDocument();
     expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+
+  it('renderiza a página About, quando clicado no link', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText(/About/i);
+
+    fireEvent.click(about);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+    expect(about).toBeInTheDocument();
   });
 });
