fc80029a15af5f697d94d29a7ea14c3ca3b4eff1
Anderson <trkz.anderson@gmail.com>
2021-05-25 17:33:33 -0300

    Requisito 01 - Teste o componente app.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { getAllByRole, render } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -9,6 +9,51 @@ test('renders a reading with the text `Pokédex`', () => {
       <App />
     </MemoryRouter>,
   );
+
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+describe('Requisito 01 - Testar o componente App.js', () => {
+  test('Se o primeiro link deve possuir o texto Home.', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const linkHome = getByRole('link', {
+      name: 'Home',
+    });
+
+    expect(linkHome).toBeInTheDocument()
+  });
+
+  test('Se o segundo link deve possuir o texto About.', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const linkAbout = getByRole('link', {
+      name: 'About',
+    });
+
+    expect(linkAbout).toBeInTheDocument()
+  });
+
+  test('Se o terceiro link deve possuir o texto Favorite Pokémons.', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const linkFavorites = getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    expect(linkFavorites).toBeInTheDocument()
+  });
+})
