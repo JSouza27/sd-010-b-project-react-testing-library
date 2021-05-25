7f612dfef64d559b843104972343bb7f5dc80222
Jessie <miss_sc_jessi@hotmail.com>
2021-05-25 16:04:42 -0300

    testando o teste requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -7,8 +7,30 @@ test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-      </MemoryRouter>,
+    </MemoryRouter>,
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Requisito 1', () => {
+  it('renderiza pagina pricipal da Pokedex com a url `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+  it('testa se topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText(/Home/i)).toBeInTheDocument();
+    expect(getByText(/About/i)).toBeInTheDocument();
+    expect(getByText(/Favorite Pokémon/i)).toBeInTheDocument();
+  });
 });
