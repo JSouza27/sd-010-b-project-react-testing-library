917051989d3425d8ae372f2b5befe8fd06e18729
Camila Arruda <camilaam1802@gmail.com>
2021-05-25 19:45:39 -0300

    Requisito 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -43,7 +43,6 @@ describe('Teste o componente About.js .', () => {
     userEvent.click(AboutLink);
 
     const img = screen.getByRole('img');
-    console.log(img);
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Teste o componente About.js .', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<App />);
+
+    const FavoriteLink = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    userEvent.click(FavoriteLink);
+
+    const favoriteMensage = screen.getByText('No favorite pokemon found');
+    expect(favoriteMensage).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(moreDetails);
+
+    const isFavorite = screen.getByRole('checkbox');
+    userEvent.click(isFavorite);
+
+    const FavoriteLink = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    userEvent.click(FavoriteLink);
+
+    const favoritePokemon = screen.getAllByRole('img');
+    expect(favoritePokemon[1]).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
