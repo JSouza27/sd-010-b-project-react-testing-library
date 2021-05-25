ff34bf0c51d974b7ccec736c3013d1d7586f0b9c
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-25 18:21:51 -0300

    erro da 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -9,10 +9,9 @@ test('verificar texto da pagina não definida`', () => {
 });
 
 test('verificar imagem da pagina não definida', () => {
-  const { getAllByRole, getByTestId } = render(<NotFound />);
+  const { getAllByRole } = render(<NotFound />);
   const img = getAllByRole('img');
   expect(img.length).toBe(2);
-  const imgNotFound = getByTestId('image-not-found');
-  expect(imgNotFound).toBeInTheDocument();
-  expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  expect(img[1]).toBeInTheDocument();
+  expect(img[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
