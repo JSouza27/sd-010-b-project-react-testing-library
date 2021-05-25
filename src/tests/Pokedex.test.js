e865c28ad1077fedca745e1591092ad5d07f7147
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-25 17:55:14 -0300

    Aparentemente passou!

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -12,3 +12,59 @@ test('Testando se renderiza o texto "Encountered pokémons" na tela', () => {
 
   expect(heading).toHaveTextContent('Encountered pokémons');
 });
+
+test('Testando se renderiza o pokemon um a um quando o botão é clicado', () => {
+  const { getByRole, getByText } = renderWithRoute(<App />);
+
+  const button = getByRole('button', {
+    name: 'Próximo pokémon',
+  });
+
+  fireEvent.click(button);
+
+  const charmander = getByText('Charmander');
+
+  expect(charmander).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const caterpie = getByText('Caterpie');
+
+  expect(caterpie).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const ekans = getByText('Ekans');
+
+  expect(ekans).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const alakazam = getByText('Alakazam');
+
+  expect(alakazam).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const mew = getByText('Mew');
+
+  expect(mew).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const rapidash = getByText('Rapidash');
+
+  expect(rapidash).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const snorlax = getByText('Snorlax');
+
+  expect(snorlax).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const dragonair = getByText('Dragonair');
+
+  expect(dragonair).toBeInTheDocument();
+});
