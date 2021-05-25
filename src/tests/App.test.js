d007711313ce7f52769534cc11ccc5c5b98a099d
Adão Benites <adaobjr@gmail.com>
2021-05-25 18:03:17 -0400

    Adicionado  Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testando tela App', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica links nav', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const Home = getByText('Home');
+    const About = getByText('About');
+    const Favorite = getByText('Favorite Pokémons');
+    expect(Home).toBeInTheDocument();
+    expect(About).toBeInTheDocument();
+    expect(Favorite).toBeInTheDocument();
+  });
 });
