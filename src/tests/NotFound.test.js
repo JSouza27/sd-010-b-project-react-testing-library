972def26400bd6ca66a34309d65f4cf968931b00
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-25 20:12:03 -0300

    Primeiro teste do quarto requisito e imports

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Teste do quarto requisito', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const heading = getByRole('heading', { level: 2 });
+
+  expect(heading).toHaveTextContent('Page requested not found');
+});
