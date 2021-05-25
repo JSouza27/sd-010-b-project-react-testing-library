0ed622a91e050070c32a8f4f9889c5cea40c18ea
Cristian <cristian.bugs@gmail.com>
2021-05-25 15:43:11 -0300

    first test partialy done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './RenderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +13,23 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('check if there are a set of links to Home, About and Favorite Pokemon', () => {
+  renderWithRouter(<App />);
+
+  const home = screen.getByRole('link', {
+    name: /Home/i,
+  });
+  userEvent.click(home);
+
+  const about = screen.getByRole('link', {
+    name: /About/i,
+  });
+  userEvent.click(about);
+
+  const favorite = screen.getByRole('link', {
+    name: /Favorite Pokémons/i,
+  });
+  userEvent.click(favorite);
 });
