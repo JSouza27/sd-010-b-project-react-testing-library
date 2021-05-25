6d5acabe10875b8afc9a516670fe87e8e418f6ab
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-25 16:45:47 -0300

    adiciona os testes do titulo e dos paragrafos da pagina

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import React from 'react';
+import About from '../components/About';
+
+describe('testing component About.js', () => {
+  test('if contain text About Pokédex', () => {
+    const { getByRole } = render(<About />);
+    const about = getByRole('heading', { level: 2, name: /about pokédex/i });
+    expect(about).toBeInTheDocument();
+  });
+  test('if contain two paragraphs', () => {
+    const { getByText } = render(<About />);
+    const firstPart = 'This application simulates a Pokédex, ';
+    const secondPart = 'a digital encyclopedia containing all Pokémons';
+    const textOne = getByText(firstPart + secondPart);
+    expect(textOne).toBeInTheDocument();
+  });
+});
