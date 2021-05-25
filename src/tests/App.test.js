5d94391e68ce4aedd490bfaaf8d6a71adb9f19ca
Mariana Mohr <marianamohr@gmail.com>
2021-05-25 15:47:07 -0300

    requisito 1 - 1.4

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,34 +5,53 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../Helpers/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = renderWithRouter(<App />);
+describe('Teste App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
 
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
-test('Pokédex é renderizada ao carregar a aplicação no caminho de URL "/"', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-  const route = '/';
-  history.push(route);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+  test('Pokédex é renderizada ao carregar a aplicação no caminho de URL "/"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const route = '/';
+    history.push(route);
 
-  const aplicacao = getByText(/Encountered pokémons/i);
-  expect(aplicacao).toBeInTheDocument();
-});
-test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const home = getByText(/Home/i);
-  const about = getByText(/About/i);
-  const favoritePokemons = getByText(/Favorite Pokémons/i);
+    const aplicacao = getByText(/Encountered pokémons/i);
+    expect(aplicacao).toBeInTheDocument();
+  });
+  test('Teste se a aplicação contém um conjunto fixo de links de navegação', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    const about = getByText(/About/i);
+    const favoritePokemons = getByText(/Favorite Pokémons/i);
 
-  expect(home).toBeInTheDocument();
-  expect(about).toBeInTheDocument();
-  expect(favoritePokemons).toBeInTheDocument();
-});
-test('A é aplicação é redireciona para ahome, na URL / ao clicar no link Home', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const home = getByText(/Home/i);
-  userEvent.click(home);
-  const aplicacao = getByText(/Encountered pokémons/i);
-  expect(aplicacao).toBeInTheDocument();
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+  test('A é aplicação é redireciona para ahome, na URL / ao clicar no link Home', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    userEvent.click(home);
+    const aplicacao = getByText(/Encountered pokémons/i);
+    expect(aplicacao).toBeInTheDocument();
+  });
+  test('Ao clicar em About, de redirecionar para"/about"', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const about = getByText(/About/i);
+    userEvent.click(about);
+    const aboutPage = getByText(/About Pokédex/i);
+    expect(aboutPage).toBeInTheDocument();
+  });
+  test('Ao clicar em Pokémons Favoritados, ir para o "/favorites"', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const favorite = getByText(/Favorite Pokémons/i);
+    userEvent.click(favorite);
+    const favoritePage = getByRole('heading', {
+      level: 2,
+      name: /Favorite Pokémons/i,
+    });
+    expect(favoritePage).toBeInTheDocument();
+  });
 });
