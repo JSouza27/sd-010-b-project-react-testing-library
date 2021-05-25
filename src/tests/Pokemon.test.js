5a798223ba093967faf1d230fb3f9690775578ce
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-25 20:26:22 -0300

    test se contem link url pokemons

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -10,7 +10,7 @@ describe('Teste requisito 5 Pokemon.js ', () => {
     const { name, image } = data[0];
 
     const pokemonName = getByTestId('pokemon-name');
-    expect(pokemonName).toHaveTextContent('Pikachu');
+    expect(pokemonName).toHaveTextContent(`${name}`);
 
     const pokemonType = getByTestId('pokemon-type');
     expect(pokemonType).toHaveTextContent('Electric');
@@ -20,5 +20,14 @@ describe('Teste requisito 5 Pokemon.js ', () => {
 
     const img = getByAltText(`${name} sprite`);
     expect(img.src).toBe(image);
+  });
+
+  test(`'este se o card do Pokémon indicado na Pokédex 
+    contém um link de navegação para exibir detalhes deste 
+    Pokémon. O link deve possuir a URL /pokemons/<id>, onde 
+    <id> é o id do Pokémon exibido'`, () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link = getByText(/details/i);
+    expect(link).toHaveAttribute('href', '/pokemons/25');
   });
 });
