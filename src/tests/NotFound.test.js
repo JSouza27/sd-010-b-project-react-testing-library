3060e00b4bb34a366e0453056fb1112e46811152
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-05-25 17:45:24 -0300

    adiciona o teste da pagina notfound para o texto

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+
+describe('Teste da página Not Found', () => {
+  test('Teste se página contém um heading', () => {
+    render(<NotFound />);
+
+    const textNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found crying emoji/i,
+    });
+
+    expect(textNotFound).toBeInTheDocument();
+  });
+});
