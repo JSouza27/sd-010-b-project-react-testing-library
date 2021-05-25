fffdafa0bfba02082d451a8e66552f1aa8998038
renanpbraga <renanpbraga@gmail.com>
2021-05-25 18:19:08 -0300

    adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from '../RenderWithRouter';
+import About from '../components/About';
+
+describe('Testa requisito 2', () => {
+  it('verifica a existência de um heading H2', () => {
+    RenderWithRouter(<About />);
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: /About pokédex/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('verifica a existência de dois parágrafos com texto sobre pokédex', () => {
+    RenderWithRouter(<About />);
+    const firstParagraph = screen.getByText(
+      'This application simulates a Pokédex',
+      { exact: false },
+    );
+    expect(firstParagraph).toBeInTheDocument();
+    const secondParagraph = screen.getByText(
+      'One can filter Pokémons by type',
+      { exact: false },
+    );
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('verifica a existência de uma imagem', () => {
+    RenderWithRouter(<About />);
+    const img = screen.getByRole('img', { name: /pokédex/i });
+    expect(img).toHaveAttribute(
+      'src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import RenderWithRouter from '../RenderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
+it('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -15,29 +14,38 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-it('deve renderizar o componente Home', () => {
-  const { getByText, history } = RenderWithRouter(<App />);
-  fireEvent.click(getByText(/Home/i));
-  const pathname = history.location.pathname;
-  expect(pathname).toBe('/');
-  const home = getByText(/Encountered pokémons/i);
-  expect(home).toBeInTheDocument();
-});
+describe('Verifica o Requisito 1 do projeto', () => {
+  it('deve renderizar o componente Home', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    const home = getByText(/Encountered pokémons/i);
+    expect(home).toBeInTheDocument();
+  });
 
-it('deve renderizar o componente Favorite Pokémons', () => {
-  const { getByText, history } = RenderWithRouter(<App />);
-  fireEvent.click(getByText(/Favorite Pokémons/i));
-  const pathname = history.location.pathname;
-  expect(pathname).toBe('/favorites');
-  const favPkmn = getByText(/Favorite pokémons/);
-  expect(favPkmn).toBeInTheDocument();
-});
+  it('deve renderizar o componente Favorite Pokémons', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+    const favPkmn = getByText(/Favorite pokémons/);
+    expect(favPkmn).toBeInTheDocument();
+  });
 
-it('deve renderizar o componente About', () => {
+  it('deve renderizar o componente About', () => {
     const { getByText, history } = RenderWithRouter(<App />);
     fireEvent.click(getByText(/About/i));
-    const pathname = history.location.pathname;
+    const { pathname } = history.location;
     expect(pathname).toBe('/about');
     const aboutAll = getByText(/About Pokédex/);
     expect(aboutAll).toBeInTheDocument();
   });
+
+  it('verifica endereço não existente', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+});
