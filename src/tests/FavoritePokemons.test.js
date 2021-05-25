a7825ae3e1131e589235d115e50c8f70766ed5da
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-25 20:16:15 -0300

    Testando se a mensagem No favorite pokemon found é exibida se não tiver nenhum pokémon favorito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testando o componente <FavoritePokemons />', () => {
+  it(`Teste se é exibido na tela a mensagem No favorite pokemon found,
+se a pessoa não tiver pokémons favoritos.`, () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const favoritePokemons = getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favoritePokemons);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
