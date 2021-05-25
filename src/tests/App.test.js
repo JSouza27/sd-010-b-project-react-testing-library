903507c73cabd4065e59c42a9db315cf1c84fe02
Leandro Reis <lleandrofr@gmail.com>
2021-05-25 15:40:23 -0300

    creates a reanderWithRouter component helper

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Test the <App /> component', () => {
+  it('renders a heading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+
+    expect(heading).toBeInTheDocument();
+  });
 });
