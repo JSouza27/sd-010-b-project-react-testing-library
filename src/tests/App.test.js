9f84eb93ce0db2f8e93e2c22a45aa21930a195ce
Mariana Mohr <marianamohr@gmail.com>
2021-05-25 15:16:24 -0300

    requisito 1 - 1.1 e 1.2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+import renderWithRouter from '../Helpers/renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    // mariana
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
+
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+test('Pokédex é renderizada ao carregar a aplicação no caminho de URL "/"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const route = '/';
+  history.push(route);
+
+  const aplicacao = getByText(/Encountered pokémons/i);
+  expect(aplicacao).toBeInTheDocument();
+});
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText(/Home/i);
+  const about = getByText(/About/i);
+  const favoritePokemons = getByText(/Favorite Pokémons/i);
+
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favoritePokemons).toBeInTheDocument();
 });
