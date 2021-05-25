60d92e7fd49248d040a5eaf82cfe408513a049b2
lucasportella <lucasportellaagu@gmail.com>
2021-05-25 14:23:56 -0300

    req 7 first part

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('test PokemonDetails component', () => {
+  test('if pokemon details are displayed on screen', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const heading = getByText('Pokédex');
+    expect(heading).toBeInTheDocument();
+    const moreDetails = getByText(/More details/i);
+    expect(moreDetails).toBeInTheDocument();
+    fireEvent.click(moreDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    expect(moreDetails).not.toBeInTheDocument();
+    const pokemonName = getByText('Pikachu Details');
+    const sumario = getByText('Summary');
+    expect(sumario).toBeInTheDocument();
+    const paragraph = getByText(/This intelligent Pokémon roasts hard/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+});
