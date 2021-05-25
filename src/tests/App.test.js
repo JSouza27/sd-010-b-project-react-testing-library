aa89361fc15a5197ffdca2b7cf0556e45f66f501
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-05-25 15:36:00 -0300

    exercicio 1 - testa App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
+describe('Checks all App', () => {
+  it('Checks the link "Home" redirects to "/".', () => {
+    const { history } = renderWithRouter(<App />);
 
-      <App />
+    const linkHome = screen.getByRole('link', {
+      name: 'Home',
+    });
+    expect(linkHome).toBeInTheDocument();
 
-    </MemoryRouter>,
-  );
+    fireEvent.click(linkHome);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Checks the link "About" redirects to "/about".', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', {
+      name: 'About',
+    });
+    expect(linkAbout).toBeInTheDocument();
+
+    fireEvent.click(linkAbout);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Checks the link "Favorite Pokémons" redirects to "/favorites".', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkFavorites = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(linkFavorites).toBeInTheDocument();
+
+    fireEvent.click(linkFavorites);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Checks if an unknown url redirects to "Not Found".', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/not-exist');
+    const noMatch = screen.getByText('Page requested not found');
+
+    expect(noMatch).toBeInTheDocument();
+  });
 });
