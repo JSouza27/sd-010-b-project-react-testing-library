7b1547bd02b3a26c1ed540f29e06a99fadc35197
Daniel <darkdanielr@gmail.com>
2021-05-25 16:02:02 -0300

    1finish

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../services/RenderRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+  const { history } = renderWithRouter(<App />);
+
+  const projectsLink = screen.getByRole('link', {
+    name: /home/i,
+  });
+  expect(projectsLink).toBeInTheDocument();
+  userEvent.click(projectsLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+
+  const projectsLink2 = screen.getByRole('link', {
+    name: /About/i,
+  });
+  expect(projectsLink2).toBeInTheDocument();
+
+  const projectsLink3 = screen.getByRole('link', {
+    name: /Favorite Pokémons/i,
+  });
+  expect(projectsLink3).toBeInTheDocument();
 });
