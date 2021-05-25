0fac2081cceb661b8985094da8a5006b09e6b776
Emerson Saturnino <programador.emerson@hotmail.com>
2021-05-25 16:18:23 -0300

    Testando os clicks

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
@@ -23,5 +23,40 @@ describe('Teste requisito1', () => {
     expect(home).toBeInTheDocument();
     expect(about).toBeInTheDocument();
     expect(favorite).toBeInTheDocument();
+  });
+
+  test('Teste se a aplicação é redirecionada para a página inicial', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('Home'));
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página de About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('About'));
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+  });
+
+  test('Teste se é redirecionada para a página de Pokémons Favoritados', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('Favorite Pokémons'));
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/error');
+    const notFound = getByText(/not found/i);
+
+    expect(notFound).toBeInTheDocument();
   });
 });
