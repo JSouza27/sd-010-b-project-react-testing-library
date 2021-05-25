6b41d73215b6d57a9d91a39b4d1e3fe95cf2b4f4
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-25 20:19:17 -0300

    Testando descrição do pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRoute from './renderWithRoute';
+
+test('Testando descrição do pokemon', () => {
+  const { getByText, getByAltText, getAllByText } = renderWithRoute(<App />);
+
+  const namePokemon = getByText('Pikachu');
+  const typePokemon = getAllByText('Electric');
+  const img = getByAltText('Pikachu sprite');
+
+  expect(namePokemon).toHaveTextContent('Pikachu');
+  expect(typePokemon[0]).toHaveTextContent('Electric');
+  expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  expect(img).toHaveAttribute('alt', 'Pikachu sprite');
+});
