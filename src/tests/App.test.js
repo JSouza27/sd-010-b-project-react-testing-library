612ba38048d323aefe03d94789df26d9c05796c3
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-25 15:42:19 -0300

    adicionando o teste de exibição da rota '/'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,12 +4,7 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('Testing component <App>', () => {
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
