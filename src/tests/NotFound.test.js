a5266a6d7881d63597640cbc153b73069601b720
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-25 18:16:05 -0300

    testa o emoji e o alt da imagem do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+
+describe('testing component NotFound.js', () => {
+  test('if contain text \'Page requested not found\' Pokédex', () => {
+    const { getByRole } = render(<NotFound />);
+    const notFound = getByRole('heading', { level: 2, name: /Page requested not found/ });
+    expect(notFound).toBeInTheDocument();
+  });
+  test('if contain image', () => {
+    const { getByLabelText, getByAltText } = render(<NotFound />);
+    const emoji = getByLabelText('Crying emoji');
+    expect(emoji).toBeInTheDocument();
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toBeInTheDocument();
+  });
+});
