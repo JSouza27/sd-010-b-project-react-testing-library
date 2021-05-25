b535f4df7a167add290c5bd32eb2fb17c32c9204
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-25 17:05:20 -0300

    testa se e exibido No favorite pokemon found

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import About from '../components/About';
 
-describe('Testando o componente about', () => {
+describe('Testa o componente about', () => {
   it('Testa se a página contém um h2 com o valor About Pokédex', () => {
     const { getByRole } = render(<About />);
     const aboutPokedex = getByRole('heading', { level: 2, name: /About Pokédex/i });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+// import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+// import App from '../App';
+
+describe('Testa o componente FavoritePokemons', () => {
+  it('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const noFavorite = getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
