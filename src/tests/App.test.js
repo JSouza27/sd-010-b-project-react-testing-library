faafaab1f39af68eb6eb11bf99e8255c95f0e8a3
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-05-25 15:34:43 -0300

    primeiro teste de navegaçao do app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
+
+import renderRouter from './renderWithRoute';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +23,32 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Testa se a página principal da Pokédex é renderizada no caminho /', () => {
+  renderRouter(<App />);
+});
+
+test('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  renderRouter(<App />);
+
+  const homeLink = screen.getByRole('link', {
+    name: /home/i,
+  });
+  const aboutLink = screen.getByRole('link', {
+    name: /about/i,
+  });
+  const favLink = screen.getByRole('link', {
+    name: /favorite pokémons/i,
+  });
+
+  const navBar = screen.getByRole('navigation');
+
+  expect(navBar).toContainElement(homeLink);
+  expect(navBar).toContainElement(aboutLink);
+  expect(navBar).toContainElement(favLink);
+});
+
+test('Testa se a aplicação é redirecionada', () => {
+  renderRouter(<App />);
 });
