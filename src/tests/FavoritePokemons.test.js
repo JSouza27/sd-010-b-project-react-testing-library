fcba40e4486081af720335f3069bf7a182f64e6c
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-25 17:51:49 -0300

    testa se e exibido todos os cards favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-// import renderWithRouter from '../renderWithRouter';
+import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
-// import App from '../App';
+import App from '../App';
 
 describe('Testa o componente FavoritePokemons', () => {
   it('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
     const { getByText } = render(<FavoritePokemons />);
     const noFavorite = getByText(/No favorite pokemon found/i);
     expect(noFavorite).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    const { getByText, getByRole, getAllByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const data = getAllByTestId('pokemon-name');
+    expect(data.length).toBe(1);
   });
 });
