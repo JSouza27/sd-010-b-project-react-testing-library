c02fd209efb99c7cf5d4c196d72f4d8db31d3f1c
Carlos Vieira <appencigr@gmail.com>
2021-05-25 18:23:02 -0300

    Requisito 2-Completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -56,6 +56,7 @@ describe('Testing App', () => {
 
     const image = screen.getByRole('img');
 
-    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image.src)
+      .toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
