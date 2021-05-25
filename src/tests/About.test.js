2e8a09969001f98687beec4d2c739446a8027259
Junior030 <juniorhenrique030@gmail.com>
2021-05-25 17:31:24 -0300

    Implementando req 02 V1.0

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+test('Test whether the page contains information about Pokédex.', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const aboutOnly = getByText('This application simulates a Pokédex,'
+  + ' a digital encyclopedia containing all Pokémons').innerHTML;
+  expect(aboutOnly).toBe('This application simulates a Pokédex,'
+  + ' a digital encyclopedia containing all Pokémons');
+});
+
+test('Test if the page contains an h2 heading with the text About Pokédex.', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const h2About = getByRole('heading').innerHTML;
+  expect(h2About).toBe('About Pokédex');
+});
