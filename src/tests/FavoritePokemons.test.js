122c89e1ce6f949664cab63e7752edf4847cc8c0
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-05-25 18:13:23 -0300

    Adiciona inicio do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+// test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Tests the Favorite Pokemons page', () => {
+  it('tests the menssage No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const menssage = getByText(/No favorite pokemon found/i);
+    expect(menssage).toBeInTheDocument();
+  });
+
+  // it('tests whether all favorite Pokémon cards are applied', () => {
+
+  // });
+});
