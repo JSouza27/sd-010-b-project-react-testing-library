b732521f42aa900d7e1bb6cbdad087e37d96d3ed
Antenor <antenorzapata@gmail.com>
2021-05-25 15:26:50 -0300

    req1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,33 @@ test('renders a reading with the tex `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('Verifica se o texto "home" é renderizado`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const text = getByText(/home/i);
+  expect(text).toBeInTheDocument();
+});
+
+test('Verifica se o texto "about" é renderizado`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const text = getByText(/about/i);
+  expect(text).toBeInTheDocument();
+});
+
+test('Verifica se o texto "Favorite Pokémons" é renderizado`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const text = getByText(/Favorite Pokémons/i);
+  expect(text).toBeInTheDocument();
+});
