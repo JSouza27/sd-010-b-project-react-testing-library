0e68c2d326b3dff73b2657eb94e3b3c90caf5da3
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-25 16:23:02 -0300

    testa se pagina contem img

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -20,4 +20,21 @@ describe('Testa requisito 2 About.js', () => {
 
     expect(aboutPokedex).toBeInTheDocument();
   });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const paragraph = getAllByText(/pokémons/i);
+
+    expect(paragraph.length).toBe(2);
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const img = getByRole('img');
+
+    expect(img).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
 });
