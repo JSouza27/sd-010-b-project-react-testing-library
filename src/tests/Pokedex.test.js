092f74d2d059251718297ea07f7a87b487e89846
denis-rossati <denis.rossati@hotmail.com>
2021-05-25 16:07:00 -0300

    requisito 5 - it('test h2 tag')

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Test the pokedex', () => {
+  it('test the <h2> tag "encountered pokemons"', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const titlePokedex = getByRole('heading', {
+      level: 2,
+    });
+    expect(titlePokedex).toHaveTextContent('Encountered pokémons');
+  });
+});
