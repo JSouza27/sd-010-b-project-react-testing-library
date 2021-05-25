4dcd32bae85fd79e2014e2ab2464ed0a04ae378f
Adão Benites <adaobjr@gmail.com>
2021-05-25 14:13:17 -0400

    Fix Lint Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando toda a aplicação da tela NotFound', () => {
+  it('Verifica se o título aparece na tela', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Verifica se a imagem NotFound está na tela', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(/Pikachu crying/i);
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
