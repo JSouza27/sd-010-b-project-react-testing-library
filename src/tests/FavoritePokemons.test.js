f9fd1e02c95f525e7ec62ea20f54a28ca217111e
Anderson <trkz.anderson@gmail.com>
2021-05-25 20:07:28 -0300

    Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Se é exibido na tela a mensagem No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
+    const { getByText } = renderWithRouter(<App />, '/favorites');
+
+    const noPokemonFound = getByText('No favorite pokemon found');
+
+    expect(noPokemonFound).toBeInTheDocument();
+  });
+
+  test('Se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+
+    const moreDetails = getByText('More details');
+    userEvent.click(moreDetails);
+
+    const favoritePokemon = getByText('Pokémon favoritado?');
+    userEvent.click(favoritePokemon);
+
+    const linkFavorites = getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavorites);
+
+    const pokemon = getByText('Pikachu');
+    expect(pokemon).toBeInTheDocument();
+  });
+})
