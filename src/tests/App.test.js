5ff3e17fbc42c273edf41f30f2e1916a6e53b583
Hugo Simoes <hugolfsimoes@gmail.com>
2021-05-25 18:43:02 -0300

    feito os testes do requisito 01, verificando a renderizacao e as rotas dos links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 //
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +14,48 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Verifica os links', () => {
+  test('Renderiza os links e testa a rota', () => {
+    const history = createMemoryHistory();
+    const { getByRole, getByText } = render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const home = getByRole('link', {
+      name: 'Home',
+    });
+    const about = getByRole('link', {
+      name: 'About',
+    });
+    const favoritePokemons = getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+
+    userEvent.click(home);
+
+    let currentRoute = history.location.pathname;
+    expect(currentRoute).toBe('/');
+
+    userEvent.click(about);
+    currentRoute = history.location.pathname;
+    expect(currentRoute).toBe('/about');
+
+    userEvent.click(favoritePokemons);
+    currentRoute = history.location.pathname;
+    expect(currentRoute).toBe('/favorites');
+
+    /* currentRoute = history.location.pathname;
+    expect(currentRoute).toBe('/favorites'); */
+
+    history.push('/naoexiste');
+    const notFound = getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+  });
 });
