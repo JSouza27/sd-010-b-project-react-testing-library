c54bf5f0b64a0b7e4c88a04c212368a4b254143c
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-25 17:08:07 -0300

    passando na 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('teste se Favorite pokémons existe', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  const heading = getByText(/Favorite pokémons/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('caso não tenha favoritos exibir `No favorite pokemon found`', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  const heading = getByText(/No favorite pokemon found/i);
+  expect(heading).toBeInTheDocument();
+});
