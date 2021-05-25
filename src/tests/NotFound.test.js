d64d17d02c5dd51313333946f348212cf297f1c0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-25 20:19:38 -0300

    Adiciona a função test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,6 +3,7 @@ import { NotFound } from '../components';
 import renderWithRouter from '../components/renderWithRouter';
 
 describe('Teste do quarto requisito', () => {
+  test('Teste se página contém com o texto Page requested not found', () => {
   const { getByRole } = renderWithRouter(<NotFound />);
   const heading = getByRole('heading', { level: 2 });
 
