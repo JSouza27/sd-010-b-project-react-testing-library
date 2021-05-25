0438c175acde7f723db15da3778eb9c581f59ccd
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-25 19:19:15 -0300

    Primeiro teste do terceiro requisito e imports

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente Favorite', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const error = getByText('No favorite pokemon found');
+
+    expect(error).toBeInTheDocument();
+  });
+});
