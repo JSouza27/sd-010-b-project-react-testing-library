8c0dfcdc6c23e75b3fc6ddfa3a3b0caada0fb701
Lara Capila <laracapilak@gmail.com>
2021-05-25 20:35:04 -0300

    requirement 1 - tests the navigation links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
+
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
 
 test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('contains a set of fixed navigation links', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const home = getByRole('link', {
+    name: /home/i,
+  });
+  const about = getByRole('link', {
+    name: /about/i,
+  });
+  const favoritePokemons = getByRole('link', {
+    name: /favorite pokémon/i,
+  });
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favoritePokemons).toBeInTheDocument();
 });
