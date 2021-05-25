424f019dc8914326ef4a62cefc12112a5a9eeb7b
Fernando <fhomaia@gmail.com>
2021-05-25 15:42:55 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,65 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe ('Testando componente App.js', () => {
+  it ('Testa se a home é renderizada no caminho de url /', () => {
+    const { queryByRole, history } = renderWithRouter(<App />);
+    history.push('/');
+    const button = queryByRole('button', {
+      name: 'Próximo pokémon',
+    });
+    expect(button).toBeInTheDocument();
+  });
+
+  it ('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const { queryByRole } = renderWithRouter(<App />);
+    const homeLink = queryByRole('link', {
+      name: 'Home',
+    });
+    const aboutLink = queryByRole('link', {
+      name: 'About',
+    });
+    const favoritesLink = queryByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritesLink).toBeInTheDocument();
+  });
+
+  it ('Testa se a aplicação é redirecionada para a página inicial ao clicar no link Home', () => {
+    const { queryByRole, history } = renderWithRouter(<App />);
+    const homeLink = queryByRole('link', {
+      name: 'Home',
+    });
+    fireEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it ('Testa se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.', () => {
+    const { queryByRole, history } = renderWithRouter(<App />);
+    const aboutLink = queryByRole('link', {
+      name: 'About',
+    });
+    fireEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Testa se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.', () => {
+    const { queryByRole, history } = renderWithRouter(<App />);
+    const favoritesLink = queryByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    fireEvent.click(favoritesLink);
+    expect(history.location.pathname).toBe('/favorites');
+  })
+
+  it('Testa se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history, queryByAltText } = renderWithRouter(<App />);
+    history.push('/url-não-existente')
+    const notFound = queryByAltText('Pikachu crying because the page requested was not found');
+    expect(notFound).toBeInTheDocument();
+  })
 });
