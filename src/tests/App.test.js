6f1e84310614999d92b0889b574fcf319ab4c492
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-25 16:51:14 -0300

    Cria o primeiro teste do requisito um

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,15 +4,13 @@ import { render } from '@testing-library/react';
 import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Teste do primeiro requisito', () => {
+  test('Teste se a página principal da Pokédex é renderizada', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Pokédex');
+
+    expect(home).toBeInTheDocument();
+  });
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
