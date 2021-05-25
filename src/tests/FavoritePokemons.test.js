5b2ca250278c06880b809e1cdb47d92d1a9d8ffe
Adão Benites <adaobjr@gmail.com>
2021-05-25 14:19:06 -0400

    Fix Lint Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testando toda a aplicação da tela FavoritePokemons', () => {
+  it('Verifica se o título aparece na tela', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const title = screen.getByRole('heading', {
+      level: 2,
+      name: /Favorite pokémons/i,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Verifica se aparece na tela No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const msg = screen.getByText('No favorite pokemon found');
+    expect(msg).toBeInTheDocument();
+  });
+});
