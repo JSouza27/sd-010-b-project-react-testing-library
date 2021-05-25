994483c79b05d98aac508afbf6dcbdf8af5ec01d
RafaMaths <rmathscoach@gmail.com>
2021-05-25 19:19:19 -0300

    test favorite pokemon in screen

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 import renderWithRouter from '../renderWithRouter';
 
@@ -11,5 +12,21 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
 
     const message = screen.getByText(/no favorite pokemon found/i);
     expect(message).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+
+    const linkDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(linkDetails);
+
+    const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkbox);
+
+    const favPokemon = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favPokemon);
+
+    const cardPokemon = screen.getByRole('img', { name: /pikachu sprite/i });
+    expect(cardPokemon).toBeInTheDocument();
   });
 });
