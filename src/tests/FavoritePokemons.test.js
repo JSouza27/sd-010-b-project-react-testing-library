5bf7a243e70dae3c1361fb18b429401d31f5a589
Carlos Vieira <appencigr@gmail.com>
2021-05-25 19:13:19 -0300

    Requisito 3-Passou no teste do Striker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Testing App', () => {
+  test('check if the message "No favorite pokemon found" appears', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/favorites'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const msgFavorite = screen.getByText('No favorite pokemon found');
+
+    expect(msgFavorite).toBeInTheDocument();
+  });
+});
