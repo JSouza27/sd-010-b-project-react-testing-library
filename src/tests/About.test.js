d30dd012c69763a8b8ef3ae3bca4361df374cb3c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-25 18:39:39 -0300

    Primeiro teste do segundo requisito e imports

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente About', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', { level: 2 });
+
+    expect(heading).toHaveTextContent('About Pokédex');
+  });
+});
