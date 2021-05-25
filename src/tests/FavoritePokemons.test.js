67fe799659f5335cb1fb084138e3410ab1acde49
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-25 17:31:27 -0300

    Passando teste dos Favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,3 +1,37 @@
-test('Testando se renderiza "No favorite pokemon found" quando não há favoritos', () => {
+import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import App from '../App';
+import { FavoritePokemons } from '../components';
+import renderWithRoute from './renderWithRoute';
 
+test('Testando se renderiza "No favorite pokemon found" quando não há favoritos', () => {
+  const { getByText } = renderWithRoute(<FavoritePokemons />);
+
+  const text = getByText('No favorite pokemon found');
+
+  expect(text).toBeInTheDocument();
+});
+
+test('Testando se renderiza pokemons quando há favoritos', () => {
+  const {
+    getByText, getAllByRole, getByLabelText, history,
+  } = renderWithRoute(<App />);
+
+  const details = getByText('More details');
+
+  fireEvent.click(details);
+
+  const favorite = getByLabelText('Pokémon favoritado?');
+
+  fireEvent.click(favorite);
+
+  history.push('/');
+
+  const link = getAllByRole('link');
+
+  fireEvent.click(link[2]);
+
+  const text = getByText('Pikachu');
+
+  expect(text).toBeInTheDocument();
 });
