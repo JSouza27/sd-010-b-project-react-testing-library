96a3f189fc418822c972d331eba7867a11b71784
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-25 19:51:40 -0300

    test not found pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Test FavoritePokemons component', () => {
+  it('Should return No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemon={ [] } />);
+    const noPokemon = getByText('No favorite pokemon found');
+    expect(noPokemon).toBeInTheDocument();
+  });
+});
