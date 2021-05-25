ff9b766f0485b958302c757b35b2b465c27a9846
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-25 16:14:58 -0300

    testa infos aparecem na tela

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,6 +3,15 @@ import renderWithRouter from '../helpers/renderWithRouters';
 import { About } from '../components';
 
 describe('Testa requisito 2 About.js', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const infos1 = getByText(/This application simulates a Pokédex/i);
+    const infos2 = getByText(/One can filter Pokémons by type/i);
+
+    expect(infos1).toBeInTheDocument();
+    expect(infos2).toBeInTheDocument();
+  });
+
   test('Teste se a página contém as informações sobre a Pokédex.', () => {
     const { getByRole } = renderWithRouter(<About />);
     const aboutPokedex = getByRole('heading', {
