1b7ca6ac1ee9c6f06bf04ed755fc8c391bcd0bcc
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-25 18:10:34 -0300

    testa se contem um h2 com texto especifico

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando página Not found', () => {
+  it('Testa se a página contém um heading h2 com texto page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+    const h2 = getByRole('heading', { level: 2, name: /Page requested not found/i });
+    expect(h2).toBeInTheDocument();
+  });
+});
