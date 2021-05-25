5298245b1c61ecaa0a81df11366b661c7d3665cb
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-25 19:12:41 -0300

    adiciona o test que verifica se o texto encountered pokémons eh renderizado na tela

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+// import { render } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('testing component Pokedex.js', () => {
+  test('if contain text \'Encountered pokémons\' in Pokédex', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const pokedex = getByRole('heading', { level: 2, name: /encountered pokémons/i });
+    expect(pokedex).toBeInTheDocument();
+  });
+});
