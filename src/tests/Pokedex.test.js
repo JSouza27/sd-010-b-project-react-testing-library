7194711be17a2e5e06edcfa51c0960276f5d19d8
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-25 18:54:02 -0300

    Fazendo o teste dos botões

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/dom';
+import { fireEvent, waitFor } from '@testing-library/dom';
 import renderWithRoute from './renderWithRoute';
 import App from '../App';
 
@@ -13,56 +13,75 @@ test('Testando se renderiza o texto "Encountered pokémons" na tela', () => {
   expect(heading).toHaveTextContent('Encountered pokémons');
 });
 
-test('Testando se renderiza o pokemon um a um quando o botão é clicado', () => {
-  const { getByRole, getByText } = renderWithRoute(<App />);
+test('Testa se renderiza os butões', () => {
+  const { getAllByTestId, getByRole } = renderWithRoute(<App />);
 
-  const button = getByRole('button', {
+  const buttonAll = getByRole('button', {
+    name: 'All',
+  });
+
+  const buttonType = getAllByTestId('pokemon-type-button');
+
+  expect(buttonAll).toHaveTextContent('All');
+  expect(buttonType[0]).toHaveTextContent('Electric');
+  expect(buttonType[1]).toHaveTextContent('Fire');
+  expect(buttonType[2]).toHaveTextContent('Bug');
+  expect(buttonType[3]).toHaveTextContent('Poison');
+  expect(buttonType[4]).toHaveTextContent('Psychic');
+  expect(buttonType[5]).toHaveTextContent('Normal');
+  expect(buttonType[6]).toHaveTextContent('Dragon');
+});
+
+test('Testando se renderiza o pokemon um a um quando o botão é clicado', async () => {
+  const { getByRole, getByText, getAllByRole } = renderWithRoute(<App />);
+
+  const buttonProximo = getByRole('button', {
     name: 'Próximo pokémon',
   });
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const charmander = getByText('Charmander');
 
   expect(charmander).toBeInTheDocument();
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const caterpie = getByText('Caterpie');
 
   expect(caterpie).toBeInTheDocument();
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const ekans = getByText('Ekans');
 
   expect(ekans).toBeInTheDocument();
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const alakazam = getByText('Alakazam');
 
   expect(alakazam).toBeInTheDocument();
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const mew = getByText('Mew');
 
   expect(mew).toBeInTheDocument();
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const rapidash = getByText('Rapidash');
 
   expect(rapidash).toBeInTheDocument();
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const snorlax = getByText('Snorlax');
 
   expect(snorlax).toBeInTheDocument();
 
-  fireEvent.click(button);
+  fireEvent.click(buttonProximo);
 
   const dragonair = getByText('Dragonair');
 
