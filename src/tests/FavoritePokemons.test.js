958a2135f761d146ad6bbbe1982062f0585a1e01
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-25 17:19:15 -0300

    adiciona o teste do componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testing component About.js', () => {
+  test('if contain text About Pokédex', () => {
+    const { getByRole } = render(<FavoritePokemons />);
+    const about = getByRole('heading', { level: 2, name: /favorite pokémons/i });
+    expect(about).toBeInTheDocument();
+  });
+  test('if not contain favorite pokemon', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const notFoundtext = getByText('No favorite pokemon found');
+    expect(notFoundtext).toBeInTheDocument();
+  });
+});
