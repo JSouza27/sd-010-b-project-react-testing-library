9eab3d28568e5d7f7f433e85f71610bcbc91ff8c
Jessie <miss_sc_jessi@hotmail.com>
2021-05-25 17:57:55 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('Requisito 3', () => {
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <FavoritePokemons />
+      </MemoryRouter>,
+    );
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Requisito 4', () => {
+  it('Testa se página contém um heading h2', () => {
+    const { getByText, getByRole } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const emoji = getByRole('img', { name: 'Crying emoji' });
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+    expect(emoji).toBeInTheDocument();
+  });
+  it('Testa se página mostra a imagem', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const imagem = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imagem.src).toBe(src);
+  });
+});
