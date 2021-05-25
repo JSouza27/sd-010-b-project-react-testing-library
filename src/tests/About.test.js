f81e1f2364ed0053058c2ebf1b7ca16274872766
riquee <strikedv@hotmail.com>
2021-05-25 14:07:29 -0300

    Requisito 2 finalizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import About from '../components/About';
+
+describe('Exercicio 2"', () => {
+  it('Renderize um h2 com o texto "About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent('About Pokédex');
+  });
+
+  it('Renderize 2 paragrafos, com o texto sobre a Pokedex', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const paragraphs = getAllByText(/Pokédex/i);
+    expect(paragraphs[0]).toBeInTheDocument();
+    expect(paragraphs[1]).toBeInTheDocument();
+  });
+
+  it('Renderize a imagem de uma Pokedex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const URL = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const image = getByRole('img');
+    expect(image).toHaveAttribute('src', expect.stringContaining(URL));
+  });
+});
