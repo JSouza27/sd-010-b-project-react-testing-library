a581d007a5f0beb07bffe615675e4d52eed30697
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-25 15:49:03 -0300

    Testando se os links do topo estão na página

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './RenderWithRouter';
 
 describe('Testing component <App>', () => {
   test('shows the Pokédex when the route is `/`', () => {
     const { getByText } = renderWithRouter(<App />);
     expect(getByText('Encountered pokémons')).toBeInTheDocument();
   });
+
+  test('show fixed links in top navigation', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const home = getByRole('link', { name: /home/i });
+    const about = getByRole('link', { name: /about/i });
+    const favoritePokemons = getByRole('link', { name: /favorite pokémons/i });
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+});
