f26093ee381b218f81d95024a7e8349cbd2c75d8
Leandro Reis <lleandrofr@gmail.com>
2021-05-25 16:17:24 -0300

    creates a test for the routes and for the invalid url

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { MemoryRouter } from 'react-router-dom';
 // import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
 
@@ -21,5 +22,29 @@ describe('Test the <App /> component', () => {
     expect(home).toHaveTextContent('Home');
     expect(about).toHaveTextContent('About');
     expect(favorites).toHaveTextContent('Favorite Pokémons');
+  });
+
+  it('redirects properly for each route (Home, About and Favorite)', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favorites = getByText(/Favorite Pokémons/i);
+
+    userEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+
+    userEvent.click(favorites);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('redirects the page when an invalid url is typed', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/digimon-is-better'); // obviously no
+
+    expect(history.location.pathname).toBe('/digimon-is-better');
+    expect(getByText('Page requested not found')).toBeInTheDocument();
   });
 });
