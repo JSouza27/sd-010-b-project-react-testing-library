df338f0140f7d85e509cd5252a2ed2452aae97f0
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-25 19:02:09 -0300

    adiciona os teste se os cards favoritados aparecem na pagina favorite pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,8 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 describe('testing component About.js', () => {
   test('if contain text About Pokédex', () => {
@@ -12,5 +14,15 @@ describe('testing component About.js', () => {
     const { getByText } = render(<FavoritePokemons />);
     const notFoundtext = getByText('No favorite pokemon found');
     expect(notFoundtext).toBeInTheDocument();
+  });
+
+  // test feito com ajuda de Raphael Gumiery
+  test('if show cards favorites pokemons', () => {
+    const { getByText, getByRole, getAllByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByText(/favorite pokémons/i));
+    const data = getAllByTestId('pokemon-name');
+    expect(data.length).toBe(1);
   });
 });
