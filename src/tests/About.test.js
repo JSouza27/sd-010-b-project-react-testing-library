bd1ec179a630a372f0e16c928e9819ab06aa0751
Abimael <abimaelr.albu@gmail.com>
2021-05-25 17:15:42 -0300

    Feature 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -28,5 +28,4 @@ test('Render About page', () => {
   const img = screen.getByRole('img');
   const link = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
   expect(img.src).toEqual(link);
-
 });
