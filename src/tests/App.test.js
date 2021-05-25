269e0fcccad13542e2fc50ca007f3e4742d33471
EderPaiva <edergomesdepaiva12@gmail.com>
2021-05-25 17:37:28 -0300

    Verificando o funcionamento do stryker

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,52 @@
-// import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
-// import { render } from '@testing-library/react';
-// import App from '../App';
+import React from 'react';
+import { getByRole, render, screen } from '@testing-library/react';
+import App from '../App';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+// component
 
+describe('Check if Pokedex is the home page and if the route links work ', () => {
+     // Home
+  test('check home', () => {
+      const history = createMemoryHistory();    // checando a home page
+        render( <Router history={ history }>
+                  <App />
+                </Router>); 
+        // page have a home button?
+        const home = screen.getByRole('link', {
+          name :'Home',
+        });
+        expect(home).toBeInTheDocument();      
+    });
+    // About
+    test('check About', () => {
+      const history = createMemoryHistory();    // checando a home page
+        render( <Router history={ history }>
+                  <App />
+                </Router>); 
+        const About = screen.getByRole('link', {
+          name :'About',
+        });
+        expect(About).toBeInTheDocument();      
+    });
+        // About
+    test('check Favorite Pokémons', () => {
+      const history = createMemoryHistory();    // checando a home page
+        render( <Router history={ history }>
+                  <App />
+                </Router>); 
+        // page have a home button?
+        const favoritePokemons = screen.getByRole('link', {
+          name :'Favorite Pokémons',
+        });
+        expect(favoritePokemons).toBeInTheDocument();      
+    });
+
+});
 // test('renders a reading with the text `Pokédex`', () => {
 //   const { getByText } = render(
-//     <MemoryRouter>
-//       <App />
-//     </MemoryRouter>,
+
 //   );
 //   const heading = getByText(/Pokédex/i);
 //   expect(heading).toBeInTheDocument();
 // });
-
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
