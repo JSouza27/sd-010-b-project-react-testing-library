a48dbc239a54c8e1e5d25d8777020e60a8d877c0
Daniel <darkdanielr@gmail.com>
2021-05-25 16:17:13 -0300

    2finish

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { About } from '../components';
+
+test('Teste se a página contém as informações sobre a Pokédex', () => {
+  const { getByAltText } = render(<About />);
+  expect(screen.getByRole('heading', {
+    level: 2,
+    name: /About Pokédex/i,
+  })).toBeInTheDocument();
+
+  const image = getByAltText('Pokédex');
+  expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
