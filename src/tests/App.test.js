83e91b9a892f0b6a6fd3eda6efc7ac40b3b0f7f3
Fabio Marturano <fabiocmjor@gmail.com>
2021-05-25 15:04:21 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
 
-it('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('requirement 1', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
 
-it('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+    const heading = getByText(/pokédex/i);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('verify links if exists (Home, About, Favorite Pokémons)', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    const linkHome = getByRole('link', { name: 'Home' });
+    const linkAbout = getByRole('link', { name: 'About' });
+    const linkFavoritePokemons = getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavoritePokemons).toBeInTheDocument();
+  });
+
+  it('Teste click of links and redirect route', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+
+    history.push('/');
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    history.push('/about');
+    expect(getByText('About Pokédex')).toBeInTheDocument();
+    history.push('/favorites');
+    expect(getByText('Favorite pokémons')).toBeInTheDocument();
+    history.push('/notFoundPages');
+    expect(getByText(/Page requested not found/)).toBeInTheDocument();
+  });
 });
