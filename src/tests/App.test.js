13ff6df4398a45d670fce4ab7cb37d1673a5502e
legalvao1 <legalvao1@gmail.com>
2021-05-25 19:22:33 -0300

    Criando os testes para o link home

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('test component App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('nav links home', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByText('Home');
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
 });
