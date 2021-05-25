d12c873c21949c3000743596c602946a777c8209
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-25 17:20:13 -0300

    Teste do NotFound funcionando

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRoute from './renderWithRoute';
+
+test('Testando Pagina Not Found', () => {
+  const { getByText, getAllByRole } = renderWithRoute(<NotFound />);
+
+  const text = getByText(/Page requested not found/i);
+  const image = getAllByRole('img');
+
+  expect(text).toBeInTheDocument();
+  expect(image[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
