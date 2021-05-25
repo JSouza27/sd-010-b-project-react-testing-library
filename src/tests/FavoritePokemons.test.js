73ee6fa743a5cb9f33429f4c7be9ae7a3c5935f1
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-25 16:48:05 -0300

    req 3 test se nao tem pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouters';
+import { FavoritePokemons } from '../components';
+
+describe('Teste requisito 3 FavoritePokemons.js', () => {
+  test(
+    'Teste mensagem favoritePokemonFound, se não tiver pokémons favoritos.', () => {
+      const { getByText } = renderWithRouter(<FavoritePokemons />);
+      expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+    },
+  );
+});
