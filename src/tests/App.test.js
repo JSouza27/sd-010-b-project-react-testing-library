4f588c870c6e3304971c498f08644b9f5596a67d
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-25 15:11:34 -0300

    Adicionando o describe

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+describe('Testing component <App>', () => {
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={ ['/'] }>
