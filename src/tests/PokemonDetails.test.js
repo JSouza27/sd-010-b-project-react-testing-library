a1a1ae53f4858e1ca3badfe2edb3057a583fdd81
lucasportella <lucasportellaagu@gmail.com>
2021-05-25 14:24:46 -0300

    req 7 part 2

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,6 +4,7 @@ import { render, fireEvent } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
+import { PokemonDetails } from '../components';
 
 describe('test PokemonDetails component', () => {
   test('if pokemon details are displayed on screen', () => {
@@ -21,5 +22,23 @@ describe('test PokemonDetails component', () => {
     expect(sumario).toBeInTheDocument();
     const paragraph = getByText(/This intelligent Pokémon roasts hard/i);
     expect(paragraph).toBeInTheDocument();
+  });
+
+  test('game locations', () => {
+    const { getByText, getByRole, getAllByAltText } = renderWithRouter(<App />);
+    const moreDetails = getByText(/More details/i);
+    fireEvent.click(moreDetails);
+    const heading = getByRole('heading',
+      { name: 'Game Locations of Pikachu' },
+      { level: 2 });
+    expect(heading).toBeInTheDocument();
+    const allPikachuLocations = getAllByAltText('Pikachu location');
+    expect(allPikachuLocations[0]).toHaveProperty('src', 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(allPikachuLocations[1]).toHaveProperty('src', 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(allPikachuLocations.length).toBe(2);
+    const location1 = getByText('Kanto Viridian Forest');
+    expect(location1).toBeInTheDocument();
+    const location2 = getByText('Kanto Power Plant');
+    expect(location2).toBeInTheDocument();
   });
 });
