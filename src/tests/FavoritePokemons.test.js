33b31868ec9bb482c73713856c2f4b3d90ad4250
RafaMaths <rmathscoach@gmail.com>
2021-05-25 18:38:48 -0300

    test "No Favorite" message

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Testa mensagem No favorite pokemon found.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const message = screen.getByText(/no favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
