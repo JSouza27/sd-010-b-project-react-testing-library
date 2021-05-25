e8b92cb0fd0fa07221c1a5cfeacacea9045134ed
Felippe Correa <felippecorrea2010@gmail.com>
2021-05-25 16:07:56 -0300

    Adicionando requisito 1 (precisa consertar sintaxe)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import renderWithRouter from '../components/RenderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
+  const { getByText, getByRole, history } = renderWithRouter(<App />);
+
+  const initialPage = history.location.pathname;
+  const heading = getByText(/encountered pokémons/i);
+  expect(initialPage).toBe('/');
   expect(heading).toBeInTheDocument();
+
+  const linkHome = getByRole('link', {
+    name: 'Home',
+  });
+  expect(linkHome).toBeInTheDocument();
+
+  const linkAbout = getByRole('link', {
+    name: 'About',
+  });
+  expect(linkAbout).toBeInTheDocument();
+
+  const linkFavoritePokemons = getByRole('link', {
+    name: 'Favorite Pokémons',
+  });
+  expect(linkFavoritePokemons).toBeInTheDocument();
+
+  history.push('pagina/pagina-nao-encontrada');
 });
