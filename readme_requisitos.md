6eeb635cfba421c8c88104e5852825050d390f81
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-25 17:27:52 -0300

    Configurando o teste do componente About e testando se há um h2

---
+++ readme_requisitos.md
@@ -0,0 +1,7 @@
+- [] Teste se a página contém as informações sobre a Pokédex.
+
+- [x] Teste se a página contém um heading h2 com o texto About Pokédex.
+
+- [] Teste se a página contém dois parágrafos com texto sobre a Pokédex.
+
+- [] Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pokédex.png/800px-Gen_I_Pokédex.png.
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testando o componente <About />', () => {
+  describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+    it('Teste se nas informações contém um heading h2 com o texto About Pokédex.', () => {
+      const { getByRole } = render(<About />);
+      const heading = getByRole('heading', {
+        level: 2,
+        name: /about pokédex/i,
+      });
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
