87884b13da150a08b3e0d8a01c869e374b964238
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-05-25 16:32:38 -0300

    teste about h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from '../App';
+
+import renderRouter from './renderWithRoute';
+import { About } from '../components';
+
+describe('Testa se a página contém as informações sobre a Pokédex', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+
+    const headPage = screen.getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+
+    expect(headPage).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderRouter(<App />);
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderRouter(<App />);
+  });
+});
