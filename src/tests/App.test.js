da14ec2e141a88530991add7e6cc4a9f536abc49
tonpasso <wellpasso@gmail.com>
2021-05-25 15:00:59 -0300

    testa se pagina principal e renderizada

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -10,5 +11,16 @@ test('renders a reading with the text `Pokédex`', () => {
     </MemoryRouter>,
   );
   const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('Testa se a pagina inicial é renderizada com o caminho "/"', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i,
+  });
   expect(heading).toBeInTheDocument();
 });
