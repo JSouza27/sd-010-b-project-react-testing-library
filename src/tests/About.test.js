51b02ce8670cdcfcb7a4a0da2121ba918f54a299
Carlos Vieira <appencigr@gmail.com>
2021-05-25 18:19:38 -0300

    solução feita com ajuda do Edu no plantão!

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -56,6 +56,6 @@ describe('Testing App', () => {
 
     const image = screen.getByRole('img');
 
-    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
