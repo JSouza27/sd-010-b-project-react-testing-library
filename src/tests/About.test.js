f83504ab504287af973c1b3517707a09d30d32e3
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-25 16:06:11 -0300

    req 2 test info sobre pokedex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouters';
+import { About } from '../components';
+
+describe('Testa requisito 2 About.js', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const aboutPokedex = getByRole('heading', {
+      level: 2, name: 'About Pokédex',
+    });
+
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+});
