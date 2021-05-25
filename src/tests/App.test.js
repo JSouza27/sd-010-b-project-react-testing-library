c8c2b1cdab963f5d7aaa5b80c0048e9b9415fe33
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-05-25 15:37:04 -0300

    testing paths

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
@@ -18,5 +19,57 @@ describe('Test "App" component', () => {
     expect(home).toBeInTheDocument();
     expect(about).toBeInTheDocument();
     expect(favoritePokemon).toBeInTheDocument();
+  });
+
+  it('redirects to "/" when click on link "Home"', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const home = getByRole('link', { name: /home/i });
+
+    userEvent.click(home);
+    const {
+      location: { pathname },
+    } = history;
+
+    expect(pathname).toBe('/');
+  });
+
+  it('redirects to "/about" when click on link "About"', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const about = getByRole('link', { name: /about/i });
+
+    userEvent.click(about);
+    const {
+      location: { pathname },
+    } = history;
+
+    expect(pathname).toBe('/about');
+  });
+
+  it('redirects to "/favorites" when click on link "Favorite Pokémons"', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const favorites = getByRole('link', { name: /favorite pokémons/i });
+
+    userEvent.click(favorites);
+    const {
+      location: { pathname },
+    } = history;
+
+    expect(pathname).toBe('/favorites');
+  });
+  it('redirects to "Not found" when a invalid URL is give', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+
+    history.push('testnotfound');
+    const {
+      location: { pathname },
+    } = history;
+
+    const requestNotFound = getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(pathname).toBe('/testnotfound');
+    expect(requestNotFound).toBeInTheDocument();
   });
 });
