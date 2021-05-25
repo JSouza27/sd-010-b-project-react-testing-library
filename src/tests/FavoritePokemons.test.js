3cc176f66849e625e920585f5c7e50d412149157
lucasportella <lucasportellaagu@gmail.com>
2021-05-25 14:16:54 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('test favorite pokemons', () => {
+  test('text of no favorite pokemons', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const noPokemonsFound = getByText('No favorite pokemon found');
+    expect(noPokemonsFound).toBeInTheDocument();
+  });
+});
