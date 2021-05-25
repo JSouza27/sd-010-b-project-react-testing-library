106325926a35cc0bb84f9f6e6bcf63fa4867c5b3
Antenor <antenorzapata@gmail.com>
2021-05-25 18:30:18 -0300

    req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('se página contém um heading h2 com o texto Page requested not found', () => {
+  const { getByRole } = render(<NotFound />);
+  const heading = getByRole('heading', { level: 2 });
+  expect(heading).toHaveTextContent(/Page requested not found/i);
+});
+
+test('Verifica se a página contém uma imagem com uma determinada Url', () => {
+  const { getByAltText } = render(<NotFound />);
+  const image = getByAltText('Pikachu crying because the page requested was not found');
+  expect(image.src).toContain(
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  );
+});
