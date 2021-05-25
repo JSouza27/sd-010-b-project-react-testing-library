76fb370d46c5b6c8d0f1e05aefb00da22afd31f9
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-25 17:23:20 -0300

    test se exibe card pokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouters';
 import { FavoritePokemons } from '../components';
+import data from '../data';
+import App from '../App';
 
 describe('Teste requisito 3 FavoritePokemons.js', () => {
   test(
@@ -9,4 +12,30 @@ describe('Teste requisito 3 FavoritePokemons.js', () => {
       expect(getByText('No favorite pokemon found')).toBeInTheDocument();
     },
   );
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { getByText, getByTestId, getByLabelText } = renderWithRouter(
+      <App />,
+    );
+    const { averageWeight: { value, measurementUnit } } = data[0];
+
+    const details = getByText('More details');
+    userEvent.click(details);
+
+    const input = getByLabelText('Pokémon favoritado?');
+    userEvent.click(input);
+
+    expect(input).toBeChecked();
+
+    const link = getByText('Favorite Pokémons');
+    userEvent.click(link);
+
+    const name = getByTestId('pokemon-name');
+    const type = getByTestId('pokemon-type');
+    const weight = getByTestId('pokemon-weight');
+
+    expect(name).toHaveTextContent(data[0].name);
+    expect(type).toHaveTextContent(data[0].type);
+    expect(weight).toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+  });
 });
