1c7b219c1ba02999757c8d5e427a787b5c604380
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-25 15:27:10 -0300

    tentando a atividade 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('pagina About teste se posssui informações sobre a Pokédex', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/About/i));
+  const { pathname } = history.location.pathname;
+  expect(pathname).toBe('/About');
+  const aboutAll = getByText(/Pokédex/);
+  expect(aboutAll).toBeInTheDocument();
+});
