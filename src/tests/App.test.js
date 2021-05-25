19a20e9ae4a090903069775eb2af30ca58eb1fc0
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-25 18:24:12 -0300

    Add a decribe()

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+describe('tests the whole App', () => {
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
