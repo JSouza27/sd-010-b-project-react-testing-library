c894a7e30057aa9505e74af337bf281f7365e007
Fabio Marturano <fabiocmjor@gmail.com>
2021-05-25 15:58:48 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+describe('requirement 3', () => {
+  it('verify no favorite pokemonFound', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/favorites');
+
+    const noFound = getByText('No favorite pokemon found');
+
+    expect(noFound).toBeInTheDocument();
+  });
+
+  it('Check add favorite pokemon', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+
+    const moreInfo = getByText(/More details/);
+    expect(moreInfo).toBeInTheDocument();
+
+    fireEvent.click(moreInfo);
+    const toFavorite = getByText(/Pokémon favoritado/);
+    expect(toFavorite).toBeInTheDocument();
+
+    fireEvent.click(toFavorite);
+    const linkFavoritePokemons = getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavoritePokemons).toBeInTheDocument();
+
+    fireEvent.click(linkFavoritePokemons);
+    const pikachu = getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
+});
