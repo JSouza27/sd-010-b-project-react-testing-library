13a8dd74a5e8410b4cecc0cf2fb5c76ad0360245
tonpasso <wellpasso@gmail.com>
2021-05-25 15:30:41 -0300

    verifica se pagina contem uma imagem

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -24,3 +24,9 @@ test('Testa se a página tem 2 parágrafos', () => {
   const p2 = getByText(/and see more/i);
   expect(p2).toBeInTheDocument();
 });
+
+test('Testa se a página tem uma imagem', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const img = getByRole('img');
+  expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
