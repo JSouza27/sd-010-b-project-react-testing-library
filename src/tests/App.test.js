2ca47cf3e200c900dd4d4626b667b796d6ebe114
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-25 18:25:11 -0300

    test the 3 first links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -23,3 +23,18 @@ test('shows the Pokédex when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+  it('tests whether the top of the application contains a fixed set of navigation links',
+    () => {
+      const { getAllByRole } = render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const links = getAllByRole('link');
+      expect(links[0]).toHaveTextContent('Home');
+      expect(links[1]).toHaveTextContent('About');
+      expect(links[2]).toHaveTextContent('Favorite Pokémons');
+    });
+});
