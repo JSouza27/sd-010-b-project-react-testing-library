75dc94b1c3ce85e9ee4cb8af766bbfe925c0cb4b
root <diegoserafim1@gmail.com>
2021-05-25 20:43:15 -0300

    criado o test para verificar no documentos App.js se existe o h1 o texto indicado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { screen } from '@testing-library/react';
 import App from '../App';
+import renderWitRouter from '../components/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste o componente <App.js />', () => {
+  // renderiza uma reading() com o texto `Pokédex`
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByRole } = renderWitRouter(<App />);
+    const heading = getByRole('heading', {
+      level: 1,
+      name: /Pokédex/i,
+    });
+    // const heading = getByText(/Pokédex/i)
+    expect(heading).toBeInTheDocument();
+  });
+
+  // test('shows the Pokédex when the route is `/`', () => {
+  //   const { getByText } = render(
+  //     <MemoryRouter initialEntries={ ['/'] }>
+  //       <App />
+  //     </MemoryRouter>,
+  //   );
+
+  //   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  // });
 });
-
-// test('shows the Pokédex when the route is `/`', () => {
-//   const { getByText } = render(
-//     <MemoryRouter initialEntries={ ['/'] }>
-//       <App />
-//     </MemoryRouter>,
-//   );
-
-//   expect(getByText('Encountered pokémons')).toBeInTheDocument();
-// });
