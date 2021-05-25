4a8a792cc7f43ea04290ec8cfbdf61b71a65459a
Antenor <antenorzapata@gmail.com>
2021-05-25 16:04:21 -0300

    req2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
+  const { getByText } = render(<About />);
+
+  const heading = getByText(/About Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('Verifica se a página contém parágrafos com texto sobre a Pokédex', () => {
+  const { getAllByText } = render(<About />);
+  const arr = Array.from(getAllByText);
+
+  arr.forEach((element) => {
+    element = getByText(/Pokédex/i);
+    expect(element).toBeInTheDocument();
+  });
+});
+
+test('Verifica se a página contém parágrafos com texto sobre a Pokédex', () => {
+  const { getByAltText } = render(<About />);
+  const image = getByAltText('Pokédex');
+  expect(image.src).toContain(
+    'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+  );
+});
