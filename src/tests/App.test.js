b72212ee18bb40be6e26540217166d3887ee9ec4
Thiago Marchini <thiago@msn.com>
2021-05-25 14:33:51 -0300

    iniciando testes do App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,6 +12,16 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('renders the three navigations links', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText('Home');
+  const about = getByText('About');
+  const favorites = getByText('Favorite Pokémons');
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favorites).toBeInTheDocument();
 });
 
 test('shows the Pokédex when the route is `/`', () => {
