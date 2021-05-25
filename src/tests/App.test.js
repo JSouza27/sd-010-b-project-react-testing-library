5c73edb2bc5db3c479823c1b1e4a26c27df49344
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-05-25 20:49:05 -0300

    Requisitos 3 e 4 realizados

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,12 +8,16 @@ import App from '../App';
 describe('testes do componente <App.js /> ', () => {
   test('testa se a rota "/" leva para a pagina principal', () => {
     const { history } = RenderWithRouter(<App />);
+
     history.push('/');
+
     const { pathname } = history.location;
+
     const textContentHome = screen.getByRole('heading', {
       level: 2,
       name: /encountered pokémons/i,
     });
+
     expect(pathname).toBe('/');
     expect(textContentHome).toBeInTheDocument();
   });
@@ -22,25 +26,31 @@ describe('testes do componente <App.js /> ', () => {
 describe('Teste se o topo da aplicação contém links de navegação fixo.', () => {
   test('O primeiro link deve possuir o texto Home.', () => {
     RenderWithRouter(<App />);
+
     const linkHome = screen.getByRole('link', {
       name: /home/i,
     });
+
     expect(linkHome).toBeInTheDocument();
   });
 
   test('O segundo link deve possuir o texto About.', () => {
     RenderWithRouter(<App />);
+
     const linkAbout = screen.getByRole('link', {
       name: /about/i,
     });
+
     expect(linkAbout).toBeInTheDocument();
   });
 
   test('O terceiro link deve possuir o texto Favorite Pokémons.', () => {
     RenderWithRouter(<App />);
+
     const linkFavoritePokemons = screen.getByRole('link', {
       name: /Favorite Pokémons/i,
     });
+
     expect(linkFavoritePokemons).toBeInTheDocument();
   });
 });
@@ -48,11 +58,15 @@ describe('Teste se o topo da aplicação contém links de navegação fixo.', ()
 describe('Testa o redirecionamento  das páginas', () => {
   test('Testa o redirecionamento para a "/" ao clicar no link Home', () => {
     const { history } = RenderWithRouter(<App />);
+
     const linkHome = screen.getByRole('link', {
       name: /home/i,
     });
+
     userEvent.click(linkHome);
+
     const { pathname } = history.location;
+
     expect(pathname).toBe('/');
   });
 
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import RenderWithRouter from './RenderWithRouter';
+
+import App from '../App';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('Teste se página contém um h2 com o texto "Page requested not found"', () => {
+    const { history } = RenderWithRouter(<App />);
+    history.push('/nãoexiste');
+
+    const text = screen.getByRole(
+      'heading', { name: /Page requested not found/i },
+    );
+
+    expect(text).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    const { history } = RenderWithRouter(<App />);
+
+    history.push('/not-found');
+
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    expect(img).toHaveAttribute(
+      'src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
