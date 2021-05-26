4ce095070ce34105a1ca98ca9d25bd061ede3ced
Marconi <marconieng@gmail.com>
2021-05-26 17:05:24 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+test('Testing Not Found page', () => {
+  const { getByRole, container } = render(<NotFound />);
+  const heading = getByRole('heading', { level: 2 });
+  const image = container.querySelector('img');
+  const imageSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  expect(heading).toContainHTML('Page requested not found');
+  expect(image).toHaveAttribute('src', imageSrc);
+});
