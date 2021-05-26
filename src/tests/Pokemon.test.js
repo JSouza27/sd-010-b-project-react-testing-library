3b9b1f0ec3bc74ba37f5bd59d776e84d1758793f
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-26 17:09:52 -0300

    testa se a pagina de detalhes contem o texto 'pokemon details'

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,3 @@
-// import { render } from '@testing-library/react';
 import { fireEvent } from '@testing-library/dom';
 import React from 'react';
 import App from '../App';
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import pokemons from '../data';
+import renderWithRouter from './renderWithRouter';
+
+const getPikachu = pokemons.filter((pokemon) => pokemon.name === 'Pikachu');
+const { name } = getPikachu[0];
+
+describe('testing component PokemonsDetails.js', () => {
+  test('shows infos pokemon in page details', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText('More details'));
+    const textDatails = getByText(`${name} Details`);
+    expect(textDatails).toBeInTheDocument();
+  });
+});
