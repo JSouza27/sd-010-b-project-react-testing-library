fe3956fc26fdef4ae65aa08de4608c107cdc8c63
André Arnoni <andre_arnoni@yahoo.com.br>
2021-05-26 18:34:05 -0300

    Executado requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -15,10 +15,6 @@ describe('Testing the Pokemon.js', () => {
     image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
   };
 
-  const detailsLink = getByRole('link', {
-    name: 'More details',
-  });
-
   test('testing if the a card of pokemon is renderized', () => {
     const { getByTestId, getByAltText } = renderWithRouter(
       <App
@@ -40,7 +36,9 @@ describe('Testing the Pokemon.js', () => {
 
   test('testing if a link \'More details\' is renderized with card', () => {
     const { getByRole } = renderWithRouter(<App pokemon={ pokemonData } />);
-    expect(detailsLink).toBeInTheDocument();
+    const detailsLink = getByRole('link', {
+      name: 'More details',
+    });
     expect(detailsLink.pathname).toBe('/pokemons/25');
     fireEvent.click(detailsLink);
     const pokemonTitle = getByRole('heading', {
@@ -51,19 +49,24 @@ describe('Testing the Pokemon.js', () => {
   });
 
   test('testing if a the pathname is changed for /pokemons/<id>', () => {
-    const { history } = renderWithRouter(<App pokemon={ pokemonData } />);
-
+    const { history, getByRole } = renderWithRouter(<App pokemon={ pokemonData } />);
+    const detailsLink = getByRole('link', {
+      name: /more details/i,
+    });
     fireEvent.click(detailsLink);
     const { pathname } = history.location;
     expect(pathname).toBe('/pokemons/25');
   });
 
   test('if the pokemon card has a star after click the isFavorite', () => {
-    const { getByLabelText, getByAltText } = renderWithRouter(
+    const { getByLabelText, getByAltText, getByRole } = renderWithRouter(
       <App
         pokemon={ pokemonData }
       />,
     );
+    const detailsLink = getByRole('link', {
+      name: 'More details',
+    });
     fireEvent.click(detailsLink);
     const isFavorite = getByLabelText('Pokémon favoritado?');
     expect(isFavorite).toBeInTheDocument();
