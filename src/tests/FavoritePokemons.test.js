4b00b95950abfcf32f761b45e792b1b4e916bc7f
Matheus H. Gois <matheus.hengo@gmail.com>
2021-05-26 18:31:42 -0300

    Test if "No favorite pokemon found" shows when dont have a favorite pokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testing FavoritePokemons.js', () => {
+  it('Show no pokemon if dont have a pokemon', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemon={ [] } />);
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
