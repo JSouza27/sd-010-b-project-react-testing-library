197dab9d9de7626b60d5e9c9945f639b77bfe449
DennisMarcelo <dennismarcelo15@gmail.com>
2021-05-26 15:42:41 -0300

    requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+/* import { screen } from '@testing-library/dom';
+import React from 'react';
+import { Pokedex } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('requisito 5', () => {
+  test('page have text with Encountered pokémons in the h2 tag', () => {
+    renderWithRouter(<Pokedex />);
+    const h2Text = screen.getByRole('heading', {
+      level: 2, name: 'Encountered pokémonsS',
+    });
+    console.log(h2Text);
+  });
+});
+*/
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+import { fireEvent, screen } from '@testing-library/dom';
+import React from 'react';
+import { Pokemon } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('requisito 6', () => {
+  const pokemon = {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  };
+
+  test('card pokemon have correct details', () => {
+    const isTruePokemon = true;
+    renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon }
+        isFavorite={ isTruePokemon }
+      />,
+    );
+
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    expect(screen.getByText('Electric')).toBeInTheDocument();
+    expect(screen.getByText('Average weight: 6.0 kg')).toBeInTheDocument();
+
+    const img = screen.getByAltText('Pikachu sprite');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  test('pokemon card has redirect link to page of details pokemons', () => {
+    const isTruePokemon = true;
+    const { history } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon }
+        isFavorite={ isTruePokemon }
+      />,
+    );
+
+    const link = screen.getByText('More details');
+    expect(link).toHaveAttribute('href', '/pokemons/25');
+    fireEvent.click(link);
+    expect(history.location.pathname).toBe('/pokemons/25');
+
+    // porque as informções não aparecem?
+  });
+
+  test('the favorite pokemon has a start', () => {
+    const isTruePokemon = true;
+    renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon }
+        isFavorite={ isTruePokemon }
+      />,
+    );
+
+    const img = screen.getByAltText('Pikachu is marked as favorite');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
+
+// como pegar o link: https://stackoverflow.com/questions/57827126/how-to-test-anchors-href-with-react-testing-library
