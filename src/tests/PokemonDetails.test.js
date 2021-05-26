00135cef93c35b8ac84f252ab9a18eb8b6856098
Leandro Reis <lleandrofr@gmail.com>
2021-05-26 19:22:01 -0300

    test for PokemonDetails component - renders title and summary

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../components/renderWithRouter';
+import pokemons from '../data';
+import App from '../App';
+
+const moreDetailsText = 'More details';
+describe('Renders detailed information about the pokemon', () => {
+  it('renders title and summary', () => {
+    const { getAllByRole, getByRole, queryByText } = renderWithRouter(<App />);
+    const { name, summary } = pokemons[0];
+    userEvent.click(getByRole('link', { name: moreDetailsText }));
+
+    const headings = getAllByRole('heading', { level: 2 });
+
+    expect(headings[0]).toHaveTextContent(`${name} Details`);
+    expect(headings[1]).toHaveTextContent('Summary');
+    expect(headings[1].nextSibling).toHaveTextContent(summary);
+    expect(queryByText(moreDetailsText)).toBeNull();
+  });
+});
