e3983088a142f61c99278a9b187c70feb9d7b3ec
Daniel <darkdanielr@gmail.com>
2021-05-26 15:27:14 -0300

    6finish

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import RenderRouter from '../services/RenderRouter';
+
+test('teste se o pokemon é renderizado', () => {
+  const { getByAltText } = RenderRouter(<App />);
+  const buttonType = screen.getAllByRole('button');
+  expect(buttonType[5]).toHaveTextContent('Psychic');
+  userEvent.click(buttonType[5]);
+  expect(screen.getByText('Alakazam')).toHaveTextContent('Alakazam');
+  const typeText = screen.getAllByText('Psychic');
+  const pokeLength = 2;
+  expect(typeText).toHaveLength(pokeLength);
+  expect(screen.getByText('Average weight: 48.0 kg'))
+    .toHaveTextContent('Average weight: 48.0 kg');
+  const image = getByAltText('Alakazam sprite');
+  expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png');
+});
+
+test('Url correta do pokemon', () => {
+  const { getByAltText, history } = RenderRouter(<App />);
+  const detailsPoke = screen.getByRole('link', {
+    name: /More Details/i,
+  });
+  expect(detailsPoke).toBeInTheDocument();
+  userEvent.click(detailsPoke);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/pokemons/25');
+  const checkbox = screen.getByRole('checkbox');
+  userEvent.click(checkbox);
+  const image = getByAltText('Pikachu is marked as favorite');
+  expect(image.src).toContain('/star-icon.svg');
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+test('Se é renderizado o pokemon clicado com as info', () => {
+  RenderRouter(<App />);
+  const pokeName = screen.getByText('Pikachu');
+  const pokeType = screen.getByText('Electric');
+  const pokeWeight = screen.getByText('Average weight: 6.0 kg');
+  const detailsLink = screen.getByRole('link', {
+    name: /More Details/i,
+  });
+  userEvent.click(detailsLink);
+  expect(pokeName).toHaveTextContent(/Pikachu/i);
+  expect(pokeType).toHaveTextContent(/Electric/i);
+  expect(pokeWeight).toHaveTextContent(/Average weight: 6.0 kg/i);
+});
