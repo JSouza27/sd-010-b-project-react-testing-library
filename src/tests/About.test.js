49f504e8979d968a16c78c06239ca9c32767a91b
Lara Capila <laracapilak@gmail.com>
2021-05-26 17:22:12 -0300

    requirement 2 - contais h2 tag and two paragraphs

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+test('Contains an h2 tag containing About Pokédex', () => {
+  const { getByRole } = renderWithRouter(<About />);
+
+  const pageTitle = getByRole('heading', {
+    level: 2,
+    name: /about pokédex/i,
+  });
+
+  expect(pageTitle).toBeInTheDocument();
+});
+
+test('contains two paragraphs', () => {
+  const { getByText } = renderWithRouter(<About />);
+
+  const beginningPhase = (/This application simulates a Pokédex,/i);
+  const endPhase = (/One can filter Pokémons by type,/i);
+
+  const phaseOne = getByText(beginningPhase);
+  const phaseTwo = getByText(endPhase);
+
+  expect(phaseOne).toBeInTheDocument();
+  expect(phaseTwo).toBeInTheDocument();
+});
