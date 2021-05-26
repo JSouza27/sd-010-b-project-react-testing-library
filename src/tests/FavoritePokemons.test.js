e37016813e90ef9b54e84294c67779c099757e5b
Daniel <dsouzaborini@gmail.com>
2021-05-26 19:00:02 -0300

    add req 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -23,35 +23,3 @@ test('Teste se é exibido todos os cards de pokémons favoritados', () => {
     expect(pokeNameElement).toBeInTheDocument();
   });
 });
-
-// test('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
-//   const favPok = [
-//     {
-//       id: 25,
-//       name: 'Pikachu',
-//       type: 'Electric',
-//       averageWeight: {
-//         value: '6.0',
-//         measurementUnit: 'kg',
-//       },
-//       image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-//       moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-//       foundAt: [
-//         {
-//           location: 'Kanto Viridian Forest',
-//           map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-//         },
-//         {
-//           location: 'Kanto Power Plant',
-//           map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-//         },
-//       ],
-//       summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
-//     }];
-
-//   const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ favPok } />);
-
-// //   const pokeName = 'Charmander';
-// //   const pokeNameElement = getByText('Charmander');
-//   expect('Charmander').not.toBeInTheDocument();
-// });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testa se página mostra a imagem', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const imageLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  const imgElement = getAllByRole('img')[1];
+
+  expect(imgElement).toBeInTheDocument();
+  expect(imgElement).toHaveAttribute('src', imageLink);
+});
+
+test('A página contém um heading h2 com o texto Page requested not found 😭', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const info1 = 'Page requested not found 😭';
+
+  const h2Element = getByRole('heading');
+
+  expect(h2Element).toBeInTheDocument();
+  expect(h2Element).toHaveTextContent(info1);
+});
