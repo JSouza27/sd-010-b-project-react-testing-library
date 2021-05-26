9c36175c29044c392435477e7251f9d3b7f569a9
legalvao1 <legalvao1@gmail.com>
2021-05-26 11:09:48 -0300

    Requisito 3 - Testa se o texto no pokemons found e renderizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Test favorite pokémons page', () => {
+  test('render no favorite pokemons found text', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFoundText = screen.getByText('No favorite pokemon found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
