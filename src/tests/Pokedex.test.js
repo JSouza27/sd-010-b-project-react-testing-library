ff2044bd49fe7f1ad33b900d946b8aaf42a7e1f3
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-26 11:56:22 -0300

    Primeiro teste do requisito cinco e imports

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Teste do quinto requisito', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const heading = screen.getByRole('heading', { level: 2 });
+
+    expect(heading).toHaveTextContent('Encountered pokémons');
+  });
+});
