a816a3e7f7a937dfe1eae0b87beb681bf4ae1ac9
Misael Bueno <misa.hbueno@gmail.com>
2021-05-26 06:45:18 -0300

    requisitos 2.1 e 3.1 acertados lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import About from '../components/About';
 
 describe('Teste do requisito 2', () => {
   it('Verifica se a página contém as informação sobre a Pokedéx', () => {
-    const { getByRole, getAllByText } = renderWithRouter(<About />);
+    const { getByRole } = renderWithRouter(<About />);
 
     const headingPokedex = getByRole('heading', {
       level: 2,
       name: 'About Pokédex',
-    })
+    });
 
     expect(headingPokedex).toBeInTheDocument();
-  })
-})
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Testes do requisito 3', () => {
   it('verifica o componente <FavoritePokemons.js />', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />)
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
 
     const notPokemons = getByText(/No favorite pokemon found/i);
 
     expect(notPokemons).toBeInTheDocument();
-  })
-})
+  });
+});
