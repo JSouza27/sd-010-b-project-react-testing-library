6cdbfd99a92a1542ad224df360a3d74b67c7fd7f
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-26 01:57:21 -0300

    tested details page top content

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import PokemonDetails from '../components/PokemonDetails';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+
+describe('Test Pokemon Details Component', () => {
+  // const favoritePokes = {};
+  // pokemons.forEach(({ id }, index) => {
+  //   if (index % 2 === 0) {
+  //     favoritePokes[id] = true;
+  //   } else {
+  //     favoritePokes[id] = false;
+  //   }
+  // });
+  // const detailedPokeWithProps = (
+  //   <PokemonDetails
+  //     pokemons={ pokemons }
+  //     isPokemonFavoriteById={ favoritePokes }
+  //     match={ { params: { id: '4' } } }
+  //   />
+  // );
+
+  it('should have detailed information on the pokemon', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const { name, summary } = pokemons[0];
+    const Link = getByText('More details');
+    expect(Link).toBeInTheDocument();
+    fireEvent.click(Link);
+    expect(Link).not.toBeInTheDocument();
+
+    const nameHeading = getByText(`${name} Details`);
+    expect(nameHeading).toBeInTheDocument();
+
+    const summaryHeader = getByText('Summary');
+    expect(summaryHeader).toBeInTheDocument();
+
+    const summaryText = getByText(summary);
+    expect(summaryText).toBeInTheDocument();
+  });
+});
