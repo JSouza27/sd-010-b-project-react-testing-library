9595575daacc4387d3b78e5a0aaf6d4223ddcb07
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-26 16:11:42 -0300

    subindo a 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('nome do pokemon correto', () => {
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+  const nomePikachu = getByTestId(/pokemon-name/i);
+  expect(nomePikachu).toBeInTheDocument();
+  const nome = getByText(/Pikachu/i);
+  expect(nome).toBeInTheDocument();
+});
+
+test('type do pokemon correto', () => {
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+  const typePokemon = getByTestId(/pokemon-type/i);
+  expect(typePokemon).toBeInTheDocument();
+  const nome = getByText(/Electric/i);
+  expect(nome).toBeInTheDocument();
+});
+
+test('weight do pokemon correto', () => {
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+  const weigthPokemon = getByTestId(/pokemon-weight/i);
+  expect(weigthPokemon).toBeInTheDocument();
+  const nome = getByText(/Average weight: 6.0 kg/i);
+  expect(nome).toBeInTheDocument();
+});
