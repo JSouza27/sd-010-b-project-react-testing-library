c7f75a4166f3dcfa0fec6a6b8b6f1bcabb6d70d2
legalvao1 <legalvao1@gmail.com>
2021-05-26 16:10:25 -0300

    Requisito 5 -Testa se o proximo pokemon e renderizado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 // import Pokedex from '../components/Pokedex';
 import App from '../App';
@@ -17,5 +18,17 @@ describe('Test pokedex component', () => {
     });
 
     expect(pageTitle).toBeInTheDocument();
+  });
+
+  test('If the next pokemon is rendered', () => {
+    renderWithRouter(<App />);
+
+    const nextButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+    expect(nextButton).toBeInTheDocument();
+
+    userEvent.click(nextButton);
+
+    const pokemon = screen.getByTestId('pokemon-name');
+    expect(pokemon).toHaveTextContent('Charmander');
   });
 });
