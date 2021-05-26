41111374f46634e03dfe869a1d34637cacccc5ee
Matheus H. Gois <matheus.hengo@gmail.com>
2021-05-26 16:57:32 -0300

    Tests if have Home, About and Favorite Pokemons links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,10 +3,22 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testing App.js', () => {
-  it(' tests if App.js is rendered at "/" path', () => {
+  // Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL /.
+  it('tests if App.js is rendered at "/" path', () => {
     const { history } = renderWithRouter(<App />);
     const { location: { pathname } } = history;
 
     expect(pathname).toBe('/');
+  });
+  // Teste se o topo da aplicação contém um conjunto fixo de links de navegação.
+  it('tests if App.js have Home, About and Favorite Pokemons links, respectively', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const links = getAllByRole('link');
+    // O primeiro link deve possuir o texto Home
+    expect(links[0].textContent).toBe('Home');
+    // O segundo link deve possuir o texto About
+    expect(links[1].textContent).toBe('About');
+    // O terceiro link deve possuir o texto Favorite Pokémons
+    expect(links[2].textContent).toBe('Favorite Pokémons');
   });
 });
