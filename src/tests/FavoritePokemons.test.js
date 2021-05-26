09d87b931f517d04319d0b720381f6c6d8284234
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-26 10:37:03 -0300

    Teste se é exibido todos os cards de pokémon favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -11,4 +11,30 @@ se a pessoa não tiver pokémons favoritos.`, () => {
     userEvent.click(favoritePokemons);
     expect(getByText('No favorite pokemon found')).toBeInTheDocument();
   });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByRole, getByTestId, getByText } = renderWithRouter(<App />);
+    const favoritePokemons = getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favoritePokemons);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+
+    const home = getByRole('link', { name: /home/i });
+    userEvent.click(home);
+
+    const details = getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+
+    const inputCheckbox = getByRole('checkbox');
+    userEvent.click(inputCheckbox);
+    expect(inputCheckbox).toBeChecked();
+
+    userEvent.click(favoritePokemons);
+
+    const pokemonName = getByTestId('pokemon-name');
+    const pokemonType = getByTestId('pokemon-type');
+    const pokemonWeight = getByTestId('pokemon-weight');
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonWeight).toBeInTheDocument();
+  });
 });
