201b8252d14566365a5ee1d210a394a7dc016897
joao966 <joaooliveira966@gmail.com>
2021-05-25 23:53:28 -0300

    add: request-4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+
+import NotFound from '../components/NotFound';
+
+describe('Testa notFound', () => {
+  test('Testa h2 About', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const notFound = getByRole('heading', { level: 2 });
+    expect(notFound).toHaveTextContent('Page requested not found');
+  });
+
+  test('testa se renderiza imagem do pokemon', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(getByText((_, { src }) => src === url)).toBeInTheDocument();
+  });
+});
