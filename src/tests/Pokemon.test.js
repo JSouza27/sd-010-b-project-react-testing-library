f9db43d99005fa24bec028ef0eb25ca428bd99d7
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-26 18:02:22 -0300

    agora foi a 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,27 +1,50 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('nome do pokemon correto', () => {
-  const { getByText, getByTestId } = renderWithRouter(<App />);
-  const nomePikachu = getByTestId(/pokemon-name/i);
-  expect(nomePikachu).toBeInTheDocument();
-  const nome = getByText(/Pikachu/i);
-  expect(nome).toBeInTheDocument();
+test('atibutos do primerito pokemon', () => {
+  const { getByTestId, getByAltText } = renderWithRouter(<App />);
+  const namePokemon = getByTestId('pokemon-name');
+  const tupePokemon = getByTestId('pokemon-type');
+  const weightPokemon = getByTestId('pokemon-weight');
+
+  expect(namePokemon.textContent).toBe('Pikachu');
+  expect(tupePokemon.textContent).toBe('Electric');
+  expect(weightPokemon.textContent).toBe('Average weight: 6.0 kg');
+
+  const img = getByAltText('Pikachu sprite');
+  expect(img).toBeInTheDocument();
+
+  expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
 });
 
-test('Type do pokemon correto', () => {
-  const { getByTestId, getAllByText } = renderWithRouter(<App />);
-  const typePokemon = getByTestId('pokemon-type');
-  expect(typePokemon).toBeInTheDocument();
-  const nome = getAllByText(/Electric/i);
-  expect(nome[0]).toBeInTheDocument();
+test('link para detalhes do pokemon', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  const MoreDetails = getByText('More details');
+  expect(MoreDetails).toBeInTheDocument();
+  fireEvent.click(MoreDetails);
+  const detalhes = getByText(/details/i);
+  expect(detalhes).toBeInTheDocument();
+  const summary = getByText('Summary');
+  expect(summary).toBeInTheDocument();
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/pokemons/25');
 });
 
-test('weight do pokemon correto', () => {
-  const { getByText, getByTestId } = renderWithRouter(<App />);
-  const weigthPokemon = getByTestId(/pokemon-weight/i);
-  expect(weigthPokemon).toBeInTheDocument();
-  const nome = getByText(/Average weight: 6.0 kg/i);
-  expect(nome).toBeInTheDocument();
+it('adicionar aos favoritos', () => {
+  const { getByRole, getByAltText, getByText } = renderWithRouter(<App />);
+
+  const MoreDetails = getByText('More details');
+  fireEvent.click(MoreDetails);
+
+  const pokemonFavorite = getByText(/Pokémon favoritado/);
+  expect(pokemonFavorite).toBeInTheDocument();
+  fireEvent.click(pokemonFavorite);
+
+  const textMarked = getByAltText('Pikachu is marked as favorite');
+  expect(textMarked).toBeInTheDocument();
+  expect(textMarked).toHaveAttribute('src', '/star-icon.svg');
 });
