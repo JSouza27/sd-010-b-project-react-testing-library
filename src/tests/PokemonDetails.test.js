4b14349030dc4732afdd0a52fe43bd60f8ad4d5e
tonpasso <wellpasso@gmail.com>
2021-05-26 11:09:14 -0300

    requisito 07 - informacoes detalhadas aparecem na tela

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../RenderWithRouter';
+import App from '../App';
+
+const moreDetails = 'More details';
+
+test('Testa se informações detalhadas aparecem na tela', () => {
+  const { getByText, getByRole } = renderWithRouter(<App />);
+  const linkDetails = getByText(moreDetails);
+  userEvent.click(linkDetails);
+  const pokeDetails = getByText('Pikachu Details');
+  expect(pokeDetails).toBeInTheDocument();
+  expect(linkDetails).not.toBeInTheDocument();
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /Summary/i,
+  });
+  expect(heading).toBeInTheDocument();
+  const infoText = getByText(/This intelligent Pokémon roasts hard berries/i);
+  expect(infoText).toBeInTheDocument();
+
+});
