65db406b52e5b3ccfaa6886360fa149987d2dc3b
Leandro Reis <lleandrofr@gmail.com>
2021-05-26 19:36:21 -0300

    test for PokemonDetails component - renders maps withpokemon's locations

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -18,4 +18,23 @@ describe('Renders detailed information about the pokemon', () => {
     expect(headings[1].nextSibling).toHaveTextContent(summary);
     expect(queryByText(moreDetailsText)).toBeNull();
   });
+
+  it('renders a section with maps and pokemon\'s location', () => {
+    const {
+      getByText, getAllByRole, getByRole, getAllByAltText,
+    } = renderWithRouter(<App />);
+    const { name, foundAt } = pokemons[0];
+    userEvent.click(getByRole('link', { name: moreDetailsText }));
+
+    const headings = getAllByRole('heading', { level: 2 });
+    expect(headings[2]).toHaveTextContent(`Game Locations of ${name}`);
+
+    const locations = getAllByAltText(`${name} location`);
+    expect(locations.length).toBe(foundAt.length);
+
+    foundAt.forEach(({ location, map }, index) => {
+      expect(getByText(location)).toBeInTheDocument();
+      expect(locations[index]).toHaveAttribute('src', map);
+    });
+  });
 });
