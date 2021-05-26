d139ef42465c5bdb5eadc93140c46943946408fc
Paulo Xavier <pxavierf@gmail.com>
2021-05-26 16:30:25 -0300

    testa se contem inflormacoes

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../RenderWithRouter';
+import { About } from '../components';
+
+test('Testa se contém informações sobre a Pokedex', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const p = getByText(/This application simulates/i);
+  expect(p).toBeInTheDocument();
+});
