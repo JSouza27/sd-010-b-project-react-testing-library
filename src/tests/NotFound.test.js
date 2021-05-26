256fcdb3343535391a0d6beffb703fb676c62876
EderPaiva <edergomesdepaiva12@gmail.com>
2021-05-26 16:03:26 -0300

    testando imagem e tambem o botao... pegadinha criaram uma role

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { screen } from '@testing-library/dom';
+import NotFound from '../components/NotFound';
+
+describe('Check image url and Heading mensage', () => {
+  test('Checking if there is a <h2 /> for the title. ', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Checking url img ', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+    const imagePikachuCrying = document.querySelector('.not-found-image');
+    expect(imagePikachuCrying.src).toMatch('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
