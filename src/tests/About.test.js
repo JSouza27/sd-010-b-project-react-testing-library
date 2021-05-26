e0a608abafc9c2adbccd19cd5c6ee588f238aac0
edersonrodara <edersonrodara@gmail.com>
2021-05-25 21:08:47 -0300

    requisito 2 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/dom';
+import { render } from '@testing-library/react';
+import React from 'react';
+import About from '../components/About';
+
+describe('Testar o About.js', () => {
+  test('Verifica se o titulo aparece na aplicação', () => {
+    render(<About />);
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i, // regex
+    })).toBeInTheDocument();
+  });
+
+  test('Verifica se há informações sobre a pokedex', () => {
+    const { getByText } = render(<About />);
+
+    const textPokedex1 = /This application simulates a Pokédex, a digital encyclopedia/i;
+    const textPokedex2 = /One can filter Pokémons by type, and see more details for/i;
+
+    expect(getByText(textPokedex1)).toBeDefined();
+    expect(getByText(textPokedex2)).toBeDefined();
+  });
+
+  test('Testando se a imagem da pokedex é exibida', () => {
+    render(<About />);
+    const logo = screen.getByRole('img');
+    expect(logo).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(logo).toHaveAttribute('alt', 'Pokédex');
+  });
+});
