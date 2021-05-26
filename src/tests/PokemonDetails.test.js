edaa3ec3ded56f4400b95fa5df669cf1ed59365e
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-26 18:30:26 -0300

    testa se existe seçao com mapas

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -13,4 +13,17 @@ describe('Testa o componente PokemonDetails', () => {
     expect(summary).toBeInTheDocument();
     expect(getByText(/roasts hard berries/i)).toBeInTheDocument();
   });
+
+  it('Testa se existe seção com os mapas contendo as localizações do pokémon', () => {
+    const { getByText, getByRole, getAllByAltText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    const gameLocations = getByRole('heading', {
+      level: 2,
+      name: 'Game Locations of Pikachu' });
+    expect(gameLocations).toBeInTheDocument();
+    const locationList = getAllByAltText('Pikachu location');
+    expect(locationList.length).toBe(2);
+    expect(locationList[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(locationList[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
 });
