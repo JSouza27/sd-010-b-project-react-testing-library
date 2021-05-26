7100d09fed1800fafe36eb1133dfd20164e0b823
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-26 15:46:38 -0300

    test se existe map e location

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,8 +4,9 @@ import renderWithRouter from '../helpers/renderWithRouters';
 import App from '../App';
 import data from '../data';
 
+const moreDetails = 'More details';
+
 describe('Test requisito 7 PokemonDetails.js', () => {
-  const moreDetails = 'More details';
   test(`'Teste se as informações detalhadas do Pokémon 
     selecionado são mostradas na tela.'`, () => {
     const { getByText } = renderWithRouter(<App />);
@@ -22,5 +23,27 @@ describe('Test requisito 7 PokemonDetails.js', () => {
     expect(details).not.toBeInTheDocument();
     expect(summary).toBeInTheDocument();
     expect(resume).toBeInTheDocument();
+  });
+
+  test(`'Teste se existe na página uma seção com os mapas 
+    contendo as localizações do pokémon'`, () => {
+    const { getByText,
+      getAllByAltText,
+    } = renderWithRouter(<App />);
+
+    const details = getByText(moreDetails);
+    expect(details).toBeInTheDocument();
+    fireEvent.click(details);
+
+    const pokemonLocations = getByText(`Game Locations of ${data[0].name}`);
+    expect(pokemonLocations).toBeInTheDocument();
+
+    const img = getAllByAltText(`${data[0].name} location`);
+    data[0].foundAt.forEach(({ location, map }, index) => {
+      const locationText = getByText(location);
+      expect(locationText).toBeInTheDocument();
+      expect(img[index]).toBeInTheDocument();
+      expect(img[index].src).toBe(map);
+    });
   });
 });
