570121721a5cda4c6a3a6c4b07f7d6ee865ae414
Leandro Reis <lleandrofr@gmail.com>
2021-05-26 15:01:18 -0300

    creates a test for the notfound component - renders msg and image

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Renders not found page when a invalid url is required', () => {
+  it('render a heading with text "Page requested not found"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const message = getByRole('heading', { level: 2 });
+
+    expect(message).toHaveTextContent(/Page requested not found/i);
+  });
+
+  it('render a not found image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const notFoundImage = getByAltText(/Pikachu crying/i);
+
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
