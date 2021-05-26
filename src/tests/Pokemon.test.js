4cda6c3bcfa918654053352a695619f54e0d61f0
legalvao1 <legalvao1@gmail.com>
2021-05-26 19:44:56 -0300

    Requisito 6 - teste se o card de pokemon e renderizado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+
+import App from '../App';
+import pokemons from '../data';
+
+describe('test component Pokemon', () => {
+  test('tests if a pokemon card is rendered', () => {
+    renderWithRouter(<App />);
+
+    pokemons.forEach(({ name, type, averageWeight:
+      { value, measurementUnit }, image }) => {
+      const pokemonName = screen.getByTestId('pokemon-name');
+      const pokemonType = screen.getByTestId('pokemon-type');
+      const pokemonWeight = screen.getByTestId('pokemon-weight');
+      const pokemonImage = screen.getByRole('img');
+
+      expect(pokemonName).toHaveTextContent(name);
+      expect(pokemonType).toHaveTextContent(type);
+      expect(pokemonWeight).toHaveTextContent(`${value} ${measurementUnit}`);
+      // ** SOURCE https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f */
+      expect(pokemonImage).toHaveAttribute('src', image);
+      expect(pokemonImage).toHaveAttribute('alt', `${name} sprite`);
+
+      const nextButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+      userEvent.click(nextButton);
+    });
+  });
+});
