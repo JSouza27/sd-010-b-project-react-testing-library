191415ad5c21ed1c524938cb3d75313100dbc3c8
Roger Franco <rogerfrancogalt@gmail.com>
2021-05-26 19:30:16 -0400

    resoulção do requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/dom';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+it('Test if the page contains an h2 heading with the text Encountered Pokémon', () => {
+  const { getByText } = renderWithRouter(<Pokedex />);
+  const pokedex = getByText(/Encountered pokémons/);
+  expect(pokedex).toBeInTheDocument();
+  const pokedexText = screen.getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i,
+  });
+  expect(pokedexText).toBeInTheDocument();
+});
+
+it('Next Pokémon is displayed when the Next Pokémon button is clicked.', () => {
+  // renderWithRouter(<Pokedex />);
+  RenderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  const pikachu = screen.getByText(/Pikachu/);
+  expect(pikachu).toBeInTheDocument();
+  const nextPokemon = screen.getByRole('button', { name: /Próximo pokémon/ });
+  fireEvent.click(nextPokemon);
+  const charmander = screen.getByText(/Charmander/);
+  expect(charmander).toBeInTheDocument();
+});
+test('only one Pokémon is shown at a time', () => {
+  RenderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  const poke = screen.getAllByTestId('pokemon-name');
+  expect(poke).toHaveLength(1);
+  expect(poke[0]).toBeInTheDocument();
+});
+test('Pokédex has the filter buttons.', () => {
+  // renderWithRouter(<Pokedex />);
+  RenderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  const btns = screen.getAllByTestId('pokemon-type-button');
+  const types = 7;
+  expect(btns.length).toBe(types);
+
+  const eletric = screen.getByRole('button', { name: /Electric/ });
+  const fire = screen.getByRole('button', { name: /Fire/ });
+  const bug = screen.getByRole('button', { name: /Bug/ });
+  const poison = screen.getByRole('button', { name: /Poison/ });
+  const psy = screen.getByRole('button', { name: /Psychic/ });
+  const normal = screen.getByRole('button', { name: /Normal/ });
+  expect(eletric).toBeEnabled();
+  expect(fire).toBeEnabled();
+  expect(bug).toBeEnabled();
+  expect(poison).toBeEnabled();
+  expect(psy).toBeEnabled();
+  expect(normal).toBeEnabled();
+});
+// test('', () => { });
+// test('', () => { });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/dom';
+import RenderWithRouter from './renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+test('card with the information of a certain Pokémon.', () => {
+  RenderWithRouter(<Pokemon pokemon={ pokemons[0] } />);
+  const pikachu = screen.getByTestId('pokemon-name');
+  expect(pikachu).toHaveTextContent(/Pikachu/);
+
+  const pikachuType = screen.getByTestId('pokemon-type');
+  expect(pikachuType).toHaveTextContent(/Electric/);
+
+  const pikachuWeight = screen.getByTestId('pokemon-weight');
+  expect(pikachuWeight).toHaveTextContent(/Average weight: 6.0 kg/);
+
+  const pikachuSprite = screen.getByAltText(/Pikachu sprite/);
+  expect(pikachuSprite).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+});
+
+test('The URL displayed in the browser changes to poke id', () => {
+  const { history } = RenderWithRouter(<Pokemon pokemon={ pokemons[0] } />);
+  fireEvent.click(screen.getByText(/More details/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/pokemons/25');
+});
+test('Test if there is a star icon on favorite Pokémon.', () => {
+  RenderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite />);
+  const pikachu = screen.getByAltText(/^Pikachu is marked as favorite$/);
+  expect(pikachu).toHaveAttribute('src', '/star-icon.svg');
+});
+test('Contains a link to view details of Pokémon', () => {
+  RenderWithRouter(<Pokemon pokemon={ pokemons[0] } />);
+  const moreDetails = screen.getByRole('link', { name: /^More details$/ });
+  expect(moreDetails).toHaveAttribute('href', '/pokemons/25');
+});
