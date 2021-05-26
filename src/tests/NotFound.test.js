2f659753903e2b36d78fa9e87b662b02cb48a980
legalvao1 <legalvao1@gmail.com>
2021-05-26 15:05:30 -0300

    Requisito 4  Testando titulo pagina nao encontrada

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './renderWithRouter';
+
+import NotFound from '../components/NotFound';
+
+describe('Test page not found', () => {
+  test('if the page contains h2 with text Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const pageTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(pageTitle).toBeInTheDocument();
+  });
+});
