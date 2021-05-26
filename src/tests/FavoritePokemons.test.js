f6146fcb9298c5541ac6cea1840aa75e53fe5be6
Mariana Savoldi <marianasavodi123@gmail.com>
2021-05-26 15:20:26 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requirement 3', () => {
+  test('No favorite Pokémon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const notFavorite = getByText('No favorite pokemon found');
+    expect(notFavorite).toBeInTheDocument();
+  });
+});
