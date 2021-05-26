f338993375d95ab33e99bf04277edc188686e209
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-05-26 17:33:21 -0300

    Resolvido o primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,12 +4,9 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
+describe('Teste o componente <App.js />', () => {
+  it('Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL /.', () => {
+    const { getByText, history } = renderWithRouter(<App />)
+    const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
