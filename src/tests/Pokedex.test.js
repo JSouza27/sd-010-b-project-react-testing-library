49e3ca4cc4225e7a3b9a44585df6f98102cd0f81
legalvao1 <legalvao1@gmail.com>
2021-05-26 15:54:59 -0300

    Requisito 5 - Testando se o titulo esta na pagina

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './renderWithRouter';
+// import Pokedex from '../components/Pokedex';
+import App from '../App';
+
+// import pokemons from '../data';
+
+describe('Test pokedex component', () => {
+  test('If the page contais title "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const pageTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+
+    expect(pageTitle).toBeInTheDocument();
+  });
+});
