3803a01b03457a8a7ab575ce524ce1a11e74f9dc
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-26 19:16:31 -0300

    New Imports

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 describe('tests the whole App', () => {
   test('renders a reading with the text `Pokédex`', () => {
