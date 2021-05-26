c8c15e130452639a4c8751c705a57488885d8837
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-26 15:00:16 -0300

    Teste se a página contém um h2 com o texto Encontered pokémons

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testando o componente <Pokedex />', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const heading = getByRole('heading', { name: 'Encountered pokémons' });
+    expect(heading).toBeInTheDocument();
+  });
+});
