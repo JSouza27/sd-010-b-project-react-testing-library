89ac87b75fc258e55d51134e349e63bde4d7e42c
Misael Bueno <misa.hbueno@gmail.com>
2021-05-25 23:25:49 -0300

    requisito 3.1

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testes do requisito 3', () => {
+  it('verifica o componente <FavoritePokemons.js />', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />)
+
+    const notPokemons = getByText(/No favorite pokemon found/i);
+
+    expect(notPokemons).toBeInTheDocument();
+  })
+})
