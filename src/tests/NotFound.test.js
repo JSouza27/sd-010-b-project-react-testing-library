3759f94869d398f566ac5129b696540835429bd0
Junior030 <juniorhenrique030@gmail.com>
2021-05-26 13:46:16 -0300

    Implementando req 05 V1.0 Item 1

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,18 +2,19 @@ import React from 'react';
 // import { MemoryRouter } from 'react-router-dom';
 // import { fireEvent, render } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+// import App from '../App';
+import { NotFound } from '../components';
 
 test('Test if the page contains an h2 heading with the text '
     + 'Page requested not found', () => {
-  const { getByRole, history } = renderWithRouter(<App />);
+  const { getByRole, history } = renderWithRouter(<NotFound />);
   history.push('/que-nao-existe');
   const text = getByRole('heading', { level: 2 }).innerHTML;
   expect(text.indexOf('Page requested not')).toBe(0);
 });
 
 test('Test if the page shows the image https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
-  const { getByRole, history } = renderWithRouter(<App />);
+  const { getByRole, history } = renderWithRouter(<NotFound />);
   history.push('/que-nao-existe');
   expect(getByRole('img', { name: /Pikachu/i }).src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
