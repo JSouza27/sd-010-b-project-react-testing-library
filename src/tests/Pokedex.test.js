b37fa1cf17b44e684199b43cd9f455fe3a53a72d
Junior030 <juniorhenrique030@gmail.com>
2021-05-26 16:41:29 -0300

    Implementando req 05 V2.0 Item 2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-// import { fireEvent } from '@testing-library/jest-dom';
+import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../renderWithRouter';
 import { Pokedex } from '../components';
+import data from '../data';
 
 const isPokemonFavoriteById = {
   4: false,
@@ -14,23 +15,59 @@ const isPokemonFavoriteById = {
   148: false,
   151: false,
 };
-const pokemons = [{ id: 25, name: 'Pikachu', type: 'Electric', averageWeight: { value: '6.0', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)', foundAt: [{ location: 'Kanto Viridian Forest', map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png' }, { location: 'Kanto Power Plant', map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png' }], summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.' }, { id: 4, name: 'Charmander', type: 'Fire', averageWeight: { value: '8.5', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)', foundAt: [{ location: 'Alola Route 3', map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png' }, { location: 'Kanto Route 3', map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png' }, { location: 'Kanto Route 4', map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png' }, { location: 'Kanto Rock Tunnel', map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png' }], summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.' }, { id: 10, name: 'Caterpie', type: 'Bug', averageWeight: { value: '2.9', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)', foundAt: [{ location: 'Johto Route 30', map: 'https://cdn2.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png' }, { location: 'Johto Route 31', map: 'https://cdn2.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png' }, { location: 'Ilex Forest', map: 'https://cdn2.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png' }, { location: 'Johto National Park', map: 'https://cdn2.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png' }], summary: 'For protection, it releases a horrible stench from the antennae on its head to drive away enemies.' }, { id: 23, name: 'Ekans', type: 'Poison', averageWeight: { value: '6.9', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)', foundAt: [{ location: 'Goldenrod Game Corner', map: 'https://cdn2.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png' }], summary: 'It can freely detach its jaw to swallow large prey whole. It can become too heavy to move, however.' }, { id: 65, name: 'Alakazam', type: 'Psychic', averageWeight: { value: '48.0', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)', foundAt: [{ location: 'Unova Accumula Town', map: 'https://cdn2.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png' }], summary: 'Closing both its eyes heightens all its other senses. This enables it to use its abilities to their extremes.' }, { id: 151, name: 'Mew', type: 'Psychic', averageWeight: { value: '4.0', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/4/43/Spr_5b_151.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)', foundAt: [{ location: 'Faraway Island', map: 'https://cdn2.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png' }], summary: 'Apparently, it appears only to those people who are pure of heart and have a strong desire to see it.' }, { id: 78, name: 'Rapidash', type: 'Fire', averageWeight: { value: '95.0', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)', foundAt: [{ location: 'Kanto Route 28', map: 'https://cdn2.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png' }, { location: 'Johto Mount Silver', map: 'https://cdn2.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png' }], summary: 'At full gallop, its four hooves barely touch the ground because it moves so incredibly fast.' }, { id: 143, name: 'Snorlax', type: 'Normal', averageWeight: { value: '460.0', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)', foundAt: [{ location: 'Kanto Vermillion City', map: 'https://cdn2.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png' }], summary: 'What sounds like its cry may actually be its snores or the rumblings of its hungry belly.' }, { id: 148, name: 'Dragonair', type: 'Dragon', averageWeight: { value: '16.5', measurementUnit: 'kg' }, image: 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png', moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)', foundAt: [{ location: 'Johto Route 45', map: 'https://cdn2.bulbagarden.net/upload/2/21/Johto_Route_45_Map.png' }, { location: 'Johto Dragon\'s Den', map: 'https://cdn2.bulbagarden.net/upload/1/1e/Johto_Dragons_Den_Map.png' }], summary: 'They say that if it emits an aura from its whole body, the weather will begin to change instantly.' }];
 
 test('Test if the page contains an h2 heading with the text Encountered Pokémon', () => {
   const { getByRole } = renderWithRouter(
     <Pokedex
-      pokemons={ pokemons }
+      pokemons={ data }
       isPokemonFavoriteById={ isPokemonFavoriteById }
     />,
   );
   expect(getByRole('heading').innerHTML).toBe('Encountered pokémons');
 });
 
-// describe('Test if the next Pokémon in the list is'
-//   + ' displayed when the Next Pokémon button is clicked.', () => {
-//   it('The button should contain the text Next pokémon', () => {});
-//   it('The next Pokémon in the list must be shown,'
-//   + ' one by one, by successively clicking on the button', () => {});
-//   it('The first Pokémon on the list should be shown by clicking the button,'
-//   + ' if it is on the last Pokémon on the list', () => {});
-// });
+describe('Test if the next Pokémon in the list is'
+  + ' displayed when the Next Pokémon button is clicked.', () => {
+  it('The button should contain the text Next pokémon', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ data }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+    expect(getByText(/Próximo pokémon/i).innerHTML).toBe('Próximo pokémon');
+  });
+
+  it('The next Pokémon in the list must be shown,'
+  + ' one by one, by successively clicking on the button', () => {
+    const { getByText, history } = renderWithRouter(
+      <Pokedex
+        pokemons={ data }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+    const buttonNext = getByText(/Próximo pokémon/i);
+    data.forEach((pokemon) => {
+      const namePokemon = getByText(pokemon.name);
+      expect(namePokemon.innerHTML).toBe(pokemon.name);
+      fireEvent.click(buttonNext);
+    });
+  });
+
+  it('The first Pokémon on the list should be shown by clicking the button,'
+  + ' if it is on the last Pokémon on the list', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ data }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+    const buttonNext = getByText(/Próximo pokémon/i);
+    data.forEach((pok) => {
+      const namePokemon = getByText(pok.name);
+      const condicao = namePokemon.innerHTML === 'Dragonair';
+      fireEvent.click(buttonNext);
+      if (condicao) expect(namePokemon.innerHTML).toBe('Pikachu');
+    });
+  });
+});
