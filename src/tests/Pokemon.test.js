3edfbbd22a62415e88fcaa1009cc2e6b504ba6e5
Felippe Correa <felippecorrea2010@gmail.com>
2021-05-26 12:34:43 -0300

    Adicionando requisito 6 e corrigindo lint

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
+
+const infoPikachu = {
+  id: 25,
+  name: 'Pikachu',
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+};
+
+describe('Test the component Pokemon', () => {
+  test('renders a card with the informations the pokemon', () => {
+    const { getByTestId, getByRole } = renderWithRouter(<App />);
+
+    const name = getByTestId('pokemon-name');
+    const type = getByTestId('pokemon-type');
+    const weight = getByTestId('pokemon-weight');
+    const img = getByRole('img', { name: `${infoPikachu.name} sprite` });
+
+    expect(name).toBeInTheDocument();
+    expect(type).toBeInTheDocument();
+    expect(weight).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', infoPikachu.image);
+  });
+
+  test('renders a link with the text "More details" ', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    const linkDetails = getByRole('link', {
+      name: 'More details',
+    });
+    fireEvent.click(linkDetails);
+    const pageDetails = history.location.pathname;
+
+    expect(pageDetails).toBe('/pokemons/25');
+  });
+});
