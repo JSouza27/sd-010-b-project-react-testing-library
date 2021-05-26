29776e5ede2332ee5bbfa194f8fdcd9a82108bd9
Matheus Bodra <matheusbodra@gmail.com>
2021-05-26 11:25:02 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testa o componente "Favorite Pokémons"', () => {
+  test('Verifica se há a mensagem "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const noFavoritePokemonsText = getByText('No favorite pokemon found');
+
+    expect(noFavoritePokemonsText).toBeInTheDocument();
+  });
+});
