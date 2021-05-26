f030f24261b943e35a1c20b49283a1ab7259aebf
Paulo Xavier <pxavierf@gmail.com>
2021-05-26 16:34:38 -0300

    testa se contem msg de 'nenhum favorito' na pagina favorite pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+test('Testa mensagem No favorite pokemon found quando nao tem favorito `', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const message = getByText(/No favorite pokemon found/i);
+  expect(message).toBeInTheDocument();
+});
