54fc543f5bf86058743656cacd909c95479ee449
MarceloPelegrino <pelegrinocorrea@gmail>
2021-05-26 16:51:07 -0300

    unknown URL renders page not found -- Finalizing requirement one

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -28,13 +28,30 @@ describe('tests the <App /> component', () => {
     const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
-  // checks if the link has the text About and redirects to URL /about by clicking on the About link
-  test('checks if the link has the text About', () => {
+  test('link has the text About and redirects to /about', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const linkAbout = getByText(/about/i);
     expect(linkAbout).toBeInTheDocument();
     fireEvent.click(getByText(/About/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/about');
+  });
+  test('checks if the link has the text Favorite Pokémons and redirects to /favorites',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const linkFavoritePokémons = getByText(/Favorite Pokémons/i);
+      expect(linkFavoritePokémons).toBeInTheDocument();
+      fireEvent.click(getByText(/Favorite Pokémons/i));
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    });
+  test('unknown URL renders page not found', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('path-that-does-not-exist');
+    const noMatch = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(noMatch).toBeInTheDocument();
   });
 });
