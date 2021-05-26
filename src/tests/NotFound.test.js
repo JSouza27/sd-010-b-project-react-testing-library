bbb90d591fd9f2933939b45c17069920f5d008d3
Junior030 <juniorhenrique030@gmail.com>
2021-05-26 11:18:54 -0300

    Implementando req 04 V2.0 1 item e criado describe do req 05

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Test if the page contains an h2 heading with the text '
+    + 'Page requested not found', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+  history.push('/que-nao-existe');
+  const text = getByRole('heading', { level: 2 }).innerHTML;
+  expect(text.indexOf('Page requested not')).toBe(0);
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,10 @@
 test('', () => {});
+
+describe('Test if the next Pokémon in the list is'
+  + ' displayed when the Next Pokémon button is clicked.', () => {
+  it('The button should contain the text Next pokémon', () => {});
+  it('The next Pokémon in the list must be shown,'
+  + ' one by one, by successively clicking on the button', () => {});
+  it('The first Pokémon on the list should be shown by clicking the button,'
+  + ' if it is on the last Pokémon on the list', () => {});
+});
