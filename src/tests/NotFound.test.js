0c967c247d94dd4105af0fa42de42a08100b7b0d
Lara Capila <laracapilak@gmail.com>
2021-05-26 17:59:24 -0300

    requirement 4 - contains h2 tag with definid text

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+test('contains h2 tag with definid text', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+
+  const notFoundText = getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+
+  expect(notFoundText).toBeInTheDocument();
+});
