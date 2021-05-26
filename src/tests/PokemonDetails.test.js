17b3809a178b860d410e320c9254a775a52934cd
RafaMaths <rmathscoach@gmail.com>
2021-05-26 20:38:22 -0300

    The page must have a text <name> Details

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste se os detalhades do Pokémon selecionado são mostradas na tela.', () => {
+  test('A página deve ter um texto <name> Details;', () => {
+    renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent(details);
+
+    const titlePikachu = screen.getByRole('heading', { name: /pikachu details/i });
+    expect(titlePikachu).toBeInTheDocument();
+  });
+});
