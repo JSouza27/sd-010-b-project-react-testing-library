0c885d7c1108f14954ee3a953e88caabe05efb5a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-26 11:51:40 -0300

    Teste se contém um h2 com o texto "Page requested not found 😭"

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import renderWithRouter from './RenderWithRouter';
+import NotFound from '../components/NotFound';
+
 describe('Testando o componente <NotFound />', () => {
-  it('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+  it('Teste se a página contém um heading h2 com o texto Page requested not found 😭',
     () => {
-      
+      const { getByRole } = renderWithRouter(<NotFound />);
+      const heading = getByRole('heading', {
+        level: 2,
+        name: 'Page requested not found Crying emoji',
+      });
+      const cryingEmoji = getByRole('img', { name: 'Crying emoji' });
+      expect(heading).toBeInTheDocument();
+      expect(cryingEmoji).toBeInTheDocument();
     });
 
   it('Teste se página mostra a imagem <https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif>', () => {
