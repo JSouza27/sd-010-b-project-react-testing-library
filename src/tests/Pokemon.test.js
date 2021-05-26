06772e2c830583741be91e3f60aae5172415f498
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-26 17:27:48 -0300

    testa se renderiza um card com as infos do pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente Pokemon.js', () => {
+  it('Testa se é renderizado um card com as informações de determinado pokémon', () => {
+    const { getByRole, getByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByRole('button', { name: 'Poison' }));
+    const img = getByRole('img');
+    expect(getByTestId('pokemon-name').textContent).toBe('Ekans');
+    expect(getByTestId('pokemon-type').textContent).toBe('Poison');
+    expect(getByTestId('pokemon-weight').textContent).toBe('Average weight: 6.9 kg');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png');
+    expect(img).toHaveAttribute('alt', 'Ekans sprite');
+  });
+});
