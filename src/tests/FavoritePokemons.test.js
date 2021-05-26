748c4703b9aa66a959f469aece3de1c1edbca454
Paulo Xavier <pxavierf@gmail.com>
2021-05-26 16:35:04 -0300

    testa se contem cards de favoritos  na pagina favorite pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -10,3 +10,15 @@ test('Testa mensagem No favorite pokemon found quando nao tem favorito `', () =>
   const message = getByText(/No favorite pokemon found/i);
   expect(message).toBeInTheDocument();
 });
+
+
+test('Testa se é exibido cards de favorito `', () => {
+  const { getByRole, getByLabelText } = renderWithRouter(<App />);
+  const moreDetails = getByRole('link', { name: /more details/i });
+  userEvent.click(moreDetails);
+  const favorite = getByLabelText('Pokémon favoritado?');
+  userEvent.click(favorite);
+  const { getByTestId } = render(<FavoritePokemons />);
+  const favoritePokemon = getByTestId('pokemon-name');
+  expect(favoritePokemon).toBeInTheDocument();
+});
