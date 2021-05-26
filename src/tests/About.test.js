a58972152e6e73b4dddb6f6634b786cf5f57d117
root <diegoserafim1@gmail.com>
2021-05-26 13:36:01 -0300

    criado o teste para verificar se a pagina FavoritePokemons contem o texto 'No favorite pokemon found'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,7 +3,7 @@ import { About } from '../components';
 import renderWithRouter from '../components/renderWithRouter';
 
 describe('Teste o componente <About.js /.', () => {
-  test('Test se a página contém um heading h2 com o texto About Pokédex.', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
     const { getByRole } = renderWithRouter(<About />);
     const heading = getByRole('heading', {
       level: 2,
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('É exibido a mensagem No favorite pokemon found, se não tiver favoritos', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
