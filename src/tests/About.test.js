6b27ee510a4a24b37b7d3971726cf09c2b6d06de
Marconi <marconieng@gmail.com>
2021-05-26 16:18:29 -0300

    Adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { About } from '../components';
+
+describe('Testing the about page', () => {
+  it('Testing whether the page contains an heading with the text About Pokédex.', () => {
+    const { getByText, getByRole, container } = render(<About />);
+    const heading = getByText('About Pokédex');
+    const paragraph = container.querySelectorAll('p');
+    const image = getByRole('img');
+
+    // https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+    const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(heading).toBeInTheDocument();
+    expect(paragraph.length).toBe(2);
+    expect(image).toHaveAttribute('src', imgURL);
+    expect(image).toHaveAttribute('alt', 'Pokédex');
+  });
+});
