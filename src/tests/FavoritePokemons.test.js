8ac287d4edaf97c778e35e4c7ef24b5cdaf5b5eb
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-05-26 20:48:57 -0300

    Resolvido o segundo requisito sem erros de lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -6,21 +6,21 @@ import App from '../App';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
-    renderWithRouter(<App />)
+    renderWithRouter(<App />);
     const linkFavPokemons = screen.getByRole('link', { name: /favorite pokémons/i });
     userEvent.click(linkFavPokemons);
     const pokemonNotFound = screen.getByText(/No favorite pokemon found/i);
     expect(pokemonNotFound).toBeInTheDocument();
   });
   it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
-    renderWithRouter(<App />)
+    renderWithRouter(<App />);
     const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
     userEvent.click(linkMoreDetails);
-    const checkBoxFavarite = screen.getByRole('checkbox', {  name: /pokémon favoritado\?/i});
-    userEvent.click(checkBoxFavarite);
+    const checkBoxFav = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkBoxFav);
     const linkFavPokemons = screen.getByRole('link', { name: /favorite pokémons/i });
     userEvent.click(linkFavPokemons);
-    const getImg = screen.getByRole('img', {  name: /pikachu sprite/i});
+    const getImg = screen.getByRole('img', { name: /pikachu sprite/i });
     expect(getImg).toBeInTheDocument();
   });
 });
