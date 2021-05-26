79efb9ec5e01de93bfc0ceb665e8fe068ec311d0
root <diegoserafim1@gmail.com>
2021-05-26 00:20:58 -0300

    criado o teste para verificar se tem o h2 com o devido texto passado na pagina About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Teste o componente <About.js /.', () => {
+  test('Test se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
