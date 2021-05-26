b37ec94b78617b3c18e15d4fd2fd88754d356c05
Carlos <cemargato@gmail.com>
2021-05-26 15:43:58 -0300

    requisto 1 e aplicaçao do renderwithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+describe('testes na App.js', () => {
+  test('Testando a exibição da tela inicial', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Encountered pokémons');
+    expect(home).toBeInTheDocument();
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  test('Testando se os links estão sendo renderizados', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favorite = getByText('Favorite Pokémons');
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+  });
+
+  test('Testando se ao clicar nos links apersenta o conteúdo da página', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favorite = getByText('Favorite Pokémons');
+
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+    expect(home).toBeInTheDocument();
+
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+    expect(about).toBeInTheDocument();
+
+    fireEvent.click(favorite);
+    expect(history.location.pathname).toBe('/favorites');
+    expect(favorite).toBeInTheDocument();
+  });
+  test('Exibição de tela NOT FOUND', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const notFound = '/pageError';
+    history.push(notFound);
+    const requestedPage = getByText('Page requested not found');
+    expect(requestedPage).toBeInTheDocument();
+  });
 });
-
