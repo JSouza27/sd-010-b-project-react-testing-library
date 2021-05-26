2fe3bfa871c845c0db06be4c389881d14ca2b019
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-26 17:45:47 -0300

    Os pokemons devem ser mostrados um a um ao clicar sucessivamente no botão

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,7 +4,22 @@ import App from '../App';
 import Pokedex from '../components/Pokedex';
 import renderWithRouter from './RenderWithRouter';
 import pokemons from '../data';
-import { render } from '@testing-library/react';
+
+const [
+  pikachu, charmander, caterpie, ekans, alakazam, mew, rapidash, snorlax, dragonair,
+] = pokemons;
+
+const isFavoritePokemons = {
+  [pikachu.id]: false,
+  [charmander.id]: false,
+  [caterpie.id]: false,
+  [ekans.id]: false,
+  [alakazam.id]: false,
+  [mew.id]: false,
+  [rapidash.id]: false,
+  [snorlax.id]: false,
+  [dragonair.id]: false,
+};
 
 describe('Testando o componente <Pokedex />', () => {
   it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
@@ -24,26 +39,24 @@ quando o botão Próximo pokémon é clicado.`, () => {
 
     it(`Os próximos Pokémons da lista devem ser mostrados, um a um,
 ao clicar sucessivamente no botão`, () => {
-      /* const { getByRole, getByTestId } = renderWithRouter(<App />);
+      const { getByRole, getByTestId } = renderWithRouter(
+        <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isFavoritePokemons } />,
+      );
       const btnNextPokemon = getByRole('button', { name: 'Próximo pokémon' });
       expect(btnNextPokemon).toBeInTheDocument();
-      userEvent.click(btnNextPokemon);
+      for (let index = 1; index < pokemons.length; index += 1) {
+        const { value, measurementUnit } = pokemons[index].averageWeight;
 
-      const nextPokemon = getByTestId('pokemon-name');
-      expect(nextPokemon).toHaveTextContent(/^charmander$/i); */
-      const [
-        pikachu,
-        charmander,
-        caterpie,
-        ekans,
-        alakazam,
-        mew,
-        rapidash,
-        snorlax,
-        dragonair
-      ] = pokemons;
+        userEvent.click(btnNextPokemon);
 
-      const {} = render(<Pokedex />);
+        const randomPokemonName = getByTestId('pokemon-name');
+        const randomPokemonType = getByTestId('pokemon-type');
+        const randomPokemonWeight = getByTestId('pokemon-weight');
+        expect(randomPokemonName).toHaveTextContent(pokemons[index].name);
+        expect(randomPokemonType).toHaveTextContent(pokemons[index].type);
+        expect(randomPokemonWeight)
+          .toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+      }
     });
   });
 });
