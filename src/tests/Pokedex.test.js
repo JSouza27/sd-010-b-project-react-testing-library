76a47330f2209b6b1203dd7fd1773c269951d660
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-05-26 11:44:42 -0300

    requisito 5 parcial 75%

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,9 +2,7 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-
-// import Pokedex from '../components/Pokedex';
-// import pokemons from '../data';
+import pokemons from '../data';
 import App from '../App';
 
 describe('Tests the Pokedex component', () => {
@@ -16,13 +14,61 @@ describe('Tests the Pokedex component', () => {
   });
 });
 
-describe('Tests the button component', () => {
+describe('Tests the buttons on the Pokedex component', () => {
   it('tests if the next pokemon apears in display', () => {
     const { getByRole, getByText } = renderWithRouter(<App />);
-    const firstPokemon = getByText('Pikachu');
-    const buttonNext = getByRole('button', { name: 'Próximo pokémon' });
-    expect(firstPokemon).toBeInTheDocument();
-    userEvent.click(buttonNext);
+
+    let pokemonName;
+    // Ver a possibilidade de usar um test.each ooou um map pra alternar entre a lista de pokemons e o evento de click
+    pokemons.forEach((pokemon) => {
+      pokemonName = getByText(pokemon.name);
+      const buttonNext = getByRole('button', { name: 'Próximo pokémon' });
+      expect(pokemonName).toBeInTheDocument();
+      userEvent.click(buttonNext);
+    });
   });
+
+  it('tests if only one Pokémon is shown at a time', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+
+    let pokemon;
+    pokemons.forEach((_pokemon, index) => {
+      pokemon = getByTestId('pokemon-name');
+      const firstPokemon = getByText('Pikachu');
+      expect(firstPokemon).toBeInTheDocument();
+
+      if (pokemon[index !== 0]) expect(pokemon).not.toBeInTheDocument();
+    });
+  });
+
+  it('tests if the Pokédex has the filter buttons', () => {
+    // const { getByText, findAllByTestId, getByRole } = renderWithRouter(<App />);
+    const { getByRole } = renderWithRouter(<App />);
+
+    let filterBtn;
+    pokemons.forEach((pokemon) => {
+      filterBtn = getByRole('button', { name: pokemon.type });
+      expect(filterBtn).toBeInTheDocument();
+      // expect(filterBtn.length).toBe(1);
+    });
+    // filterBtn = findAllByTestId('pokemon-type-button');
+    // console.log(filterBtn);
+  });
+
+  it('Test if the Pokédex contains a button to reset the filter', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const filterBtn = getByRole('button', { name: 'All' });
+    console.log(filterBtn);
+    const functionOnClick = filterBtn.getAttribute('onClick');
+    console.log(functionOnClick);
+    expect(functionOnClick).toBeDefined();
+  });
+
+  // it('tests whether a filter btn is created dynamically for each type of Pokémon', () => {
+
+  // });
+
+  // it('test if the Next Pokémon btn its disabled whenever theres only one Pokémon', () => {
+
+  // });
 });
-// Ver a possibilidade de usar um test.each ooou um map pra alternar entre a lista de pokemons e o evento de click
