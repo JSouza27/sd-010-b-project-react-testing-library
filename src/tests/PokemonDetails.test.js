9f140d0a69eb820d8bbf537a0a4da53a4978f12c
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-26 18:32:06 -0300

    testa se o pokemons esta sendo favoritado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -7,15 +7,42 @@ import renderWithRouter from './renderWithRouter';
 const getPikachu = pokemons.filter((pokemon) => pokemon.name === 'Pikachu');
 const { name, summary } = getPikachu[0];
 
+const DATAILS = 'More details';
+
 describe('testing component PokemonsDetails.js', () => {
   test('shows infos pokemon in page details', () => {
     const { getByText, getByRole } = renderWithRouter(<App />);
-    fireEvent.click(getByText('More details'));
+    fireEvent.click(getByText(DATAILS));
     const textDatails = getByText(`${name} Details`);
     expect(textDatails).toBeInTheDocument();
-    // const linkDetails = getByRole('link', { name: 'More details' });
+    // const linkDetails = getByRole('link', { name: DATAILS });
     const summaryDetails = getByRole('heading', { level: 2, name: 'Summary' });
     expect(summaryDetails).toBeInTheDocument();
     expect(getByText(`${summary}`)).toBeInTheDocument();
+  });
+
+  test('shows maps in page details', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(DATAILS));
+    const textDatails = getByText(`Game Locations of ${name}`);
+    expect(textDatails).toBeInTheDocument();
+  });
+
+  test('if user can favorite pokemon', () => {
+    const { getByText, getByRole, getByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByText(DATAILS));
+    const textFavorite = getByText('Pokémon favoritado?');
+    expect(textFavorite).toBeInTheDocument();
+    const checkbox = getByRole('checkbox');
+    expect(checkbox).toBeInTheDocument();
+    fireEvent.click(checkbox);
+    fireEvent.click(getByText(/favorite pokémons/i));
+    const pokemon = getByTestId('pokemon-name');
+    expect(pokemon).toBeInTheDocument();
+    fireEvent.click(getByText(DATAILS));
+    fireEvent.click(checkbox);
+    fireEvent.click(getByText(/favorite pokémons/i));
+    expect(pokemon).not.toBeInTheDocument();
+    fireEvent.click(getByText(DATAILS));
   });
 });
