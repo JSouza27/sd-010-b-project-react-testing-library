48f75e44ec12411235658f7c4c995f9019fe7bdb
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-05-26 18:23:49 -0300

    testa se as infos detalhadas do poke sao mostradas

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente PokemonDetails', () => {
+  it('Testa se as informações detalhadas do Pokémon são mostradas na tela', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    const details = getByRole('heading', { name: 'Pikachu Details' });
+    expect(details).toBeInTheDocument();
+    const summary = getByRole('heading', { name: 'Summary', level: 2 });
+    expect(summary).toBeInTheDocument();
+    expect(getByText(/roasts hard berries/i)).toBeInTheDocument();
+  });
+});
