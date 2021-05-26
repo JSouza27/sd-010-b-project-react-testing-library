701b1b3afce2d7ef1f53576c4eeef3c16c828beb
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-05-26 20:19:34 -0300

    Criado teste de redirecionamento ao clicar em Details

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import RenderWithRouter from './RenderWithRouter';
 
 import App from '../App';
@@ -23,5 +24,13 @@ describe('Testes do Componente <Pokemon />', () => {
       'src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
     );
     expect(imgPkemon).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+
+  test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes deste Pokémon.', () => {
+    const { history } = RenderWithRouter(<App />);
+    const btnDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(btnDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
   });
 });
