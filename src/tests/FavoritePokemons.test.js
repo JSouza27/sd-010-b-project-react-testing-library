e69543fde42207c2c71ca63ccd84595ceaae4388
root <diegoserafim1@gmail.com>
2021-05-26 18:36:10 -0300

    craido o teste para verificar se todos elementos favoritados estão com a classe 'favorite-pokemon'

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
+// import { render } from '@testing-library/react';
 import { FavoritePokemons } from '../components';
 import renderWithRouter from '../components/renderWithRouter';
+import pokemons from '../data';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   test('É exibido a mensagem No favorite pokemon found, se não tiver favoritos', () => {
     const { getByText } = renderWithRouter(<FavoritePokemons />);
     expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { queryAllByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ pokemons } />,
+    );
+    pokemons.forEach((parameter, id) => {
+      const img = queryAllByText((string,
+        { className }) => className === 'favorite-pokemon');
+      expect(img[id]).toBeInTheDocument();
+    });
   });
 });
