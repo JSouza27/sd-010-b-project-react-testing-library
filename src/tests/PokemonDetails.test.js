2f287e9f78fc0fd8dff1e2411a9c6f794da0312c
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-26 17:33:25 -0300

    testa se existe o texto summary e os detalhes do pokemon

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,13 +5,17 @@ import pokemons from '../data';
 import renderWithRouter from './renderWithRouter';
 
 const getPikachu = pokemons.filter((pokemon) => pokemon.name === 'Pikachu');
-const { name } = getPikachu[0];
+const { name, summary } = getPikachu[0];
 
 describe('testing component PokemonsDetails.js', () => {
   test('shows infos pokemon in page details', () => {
-    const { getByText } = renderWithRouter(<App />);
+    const { getByText, getByRole } = renderWithRouter(<App />);
     fireEvent.click(getByText('More details'));
     const textDatails = getByText(`${name} Details`);
     expect(textDatails).toBeInTheDocument();
+    // const linkDetails = getByRole('link', { name: 'More details' });
+    const summaryDetails = getByRole('heading', { level: 2, name: 'Summary' });
+    expect(summaryDetails).toBeInTheDocument();
+    expect(getByText(`${summary}`)).toBeInTheDocument();
   });
 });
