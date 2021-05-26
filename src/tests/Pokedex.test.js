429abf5e6926af483fa092081c00e41845832b37
RafaMaths <rmathscoach@gmail.com>
2021-05-26 10:58:18 -0300

    test H2 in Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
+import Pokedex from '../components/Pokedex';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se página contém um h2 com o texto Encountered pokémons.', async () => {
+    renderWithRouter(<App />);
+
+    const headingH2 = screen.getByRole('heading', { name: /encountered pokémons/i });
+    expect(headingH2).toBeInTheDocument();
+  });
+
+  // test('Teste se após clicar no botão próximo, o pokemon é exibido em tela.', () => {
+  //   renderWithRouter(<App />);
+
+  // });
+});
