6d3d839e1490c5cbe4221226b6e8c7ac8d9e4889
Junior030 <juniorhenrique030@gmail.com>
2021-05-26 08:32:11 -0300

    Implementando req 03 V1.0

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+test('The message No favorite pokemon found is displayed on the screen.', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const p = getByText('No favorite pokemon found').innerHTML;
+  expect(p).toBe('No favorite pokemon found');
+});
+
+test('Test whether all your favorite Pokémon cards are displayed.', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const img = getByText('Pikachu');
+  console.log(img);
+});
