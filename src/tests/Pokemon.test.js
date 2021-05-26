0bed1c00e1cfb260b3bf38a67003afc64d5b0725
tonpasso <wellpasso@gmail.com>
2021-05-26 10:41:10 -0300

    requisito 06 conlcuido

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,6 +5,8 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../RenderWithRouter';
 import App from '../App';
 
+const moreDetails = 'More details';
+
 test('Testa se card é renderizado com informações de um Pokemon', () => {
   const { getByTestId, getByRole } = renderWithRouter(<App />);
   const pokeName = getByTestId('pokemon-name');
@@ -20,16 +22,29 @@ test('Testa se card é renderizado com informações de um Pokemon', () => {
 
 test('Testa se card do pokemon possui link de navegação', () => {
   const { getByText } = renderWithRouter(<App />);
-  const linkDetails = getByText('More details');
-  expect(linkDetails.href).toBe('http://localhost/pokemons/25');
+  const linkDetails = getByText(moreDetails);
+  expect(linkDetails.href).toContain('/pokemons/25');
 });
 
 test('Ao clicar em More Details redireciona para (URL /pokemons/id)', () => {
   const { getByText, history } = renderWithRouter(<App />);
-  const details = getByText('More details');
+  const details = getByText(moreDetails);
   userEvent.click(details);
   const { pathname } = history.location;
   expect(pathname).toBe('/pokemons/25');
   const screenText = getByText('Pikachu Details');
   expect(screenText).toBeInTheDocument();
+});
+
+test('Verifica estrela no Pokemon favoritado', () => {
+  const { getByText, getByLabelText, getAllByRole } = renderWithRouter(<App />);
+  const pokemonDetails = getByText(moreDetails);
+  expect(pokemonDetails).toBeInTheDocument();
+  userEvent.click(pokemonDetails);
+  const favorite = getByLabelText('Pokémon favoritado?');
+  userEvent.click(favorite);
+
+  const favoriteImage = getAllByRole('img');
+  expect(favoriteImage[1].alt).toBe('Pikachu is marked as favorite');
+  expect(favoriteImage[1].src).toContain('/star-icon.svg');
 });
