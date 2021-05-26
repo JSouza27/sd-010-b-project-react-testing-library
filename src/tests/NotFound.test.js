8b8986b7cb039e46e86b63c636f2f2bea56ebcab
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-26 11:34:51 -0300

    Configurações iniciais dos testes do componente <NotFound>

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+describe('Testando o componente <NotFound />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      
+    });
+
+  it('Teste se página mostra a imagem <https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif>', () => {
+    
+  });
+});
