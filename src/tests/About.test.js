51e991856828a668462d800efaa0ad6fd3b5e400
Matheus H. Gois <matheus.hengo@gmail.com>
2021-05-26 17:40:59 -0300

    Verify if have h2 text

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testing About.js', () => {
+  it('Verifies if the page has pokedex h2 text', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const pokedexText = getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(pokedexText).toBeInTheDocument();
+  });
+});
