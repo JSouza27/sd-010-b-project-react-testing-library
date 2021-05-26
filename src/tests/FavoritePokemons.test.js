9f5aad3346caddfd47a706bfafd1479a3860b538
CamilaCode7 <CamilaCode7>
2021-05-26 19:51:24 -0300

    Adicionando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Requirement03', () => {
+  test('Testa se mensagem exibe na tela', () => {
+    const { getByText, history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/pagina/que-nao-existe');
+    const text = getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+});
