9e92ed95395a152e6ebdaf04d966aa6debd30ba2
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-05-26 16:05:05 -0300

    Teste 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testa o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se página contém um heading h2', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const altimg = getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(altimg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
