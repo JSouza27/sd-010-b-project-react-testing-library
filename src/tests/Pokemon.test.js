6af446be3d61fa4e2fb9b3bcbecfc1654cc0a076
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-26 16:38:20 -0300

    adiciona o teste para a imagem do pokemon na tela

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -6,16 +6,20 @@ import renderWithRouter from './renderWithRouter';
 
 describe('testing component Pokemon.js', () => {
   test('shows card info pokémons', () => {
-    const { getByTestId } = renderWithRouter(<App />);
-    const pokemonName = getByTestId('pokemon-name', { name: /pikachu/i });
-    expect(pokemonName).toHaveTextContent(/pikachu/i);
-    const pokemonType = getByTestId('pokemon-type', { name: /electric/i });
-    expect(pokemonType).toHaveTextContent(/electric/i);
+    const { getByTestId, getByRole } = renderWithRouter(<App />);
     const getPikachu = pokemons.filter((pokemon) => pokemon.name === 'Pikachu');
-    const { averageWeight: { value, measurementUnit } } = getPikachu[0];
+    const { averageWeight, image, name, type } = getPikachu[0];
+    const { value, measurementUnit } = averageWeight;
+    const pokemonName = getByTestId('pokemon-name', { name: `${name}` });
+    expect(pokemonName).toHaveTextContent(/pikachu/i);
+    const pokemonType = getByTestId('pokemon-type', { name: `${type}` });
+    expect(pokemonType).toHaveTextContent(/electric/i);
     const pokemonWeight = getByTestId('pokemon-weight', {
       name: `Average weight: ${value} ${measurementUnit}`,
     });
     expect(pokemonWeight).toHaveTextContent(/average weight: 6.0 kg/i);
+    const pokemonImage = getByRole('img', { name: `${name} sprite` });
+    expect(pokemonImage).toHaveAttribute('src', `${image}`);
+    expect(pokemonImage).toHaveAttribute('alt', 'Pikachu sprite');
   });
 });
