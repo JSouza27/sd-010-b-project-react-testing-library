d60a0dc724c53709327617a482c6a0e0d064ab3e
root <diegoserafim1@gmail.com>
2021-05-26 19:26:25 -0300

    criado teste para verificar se tem um título em H2 na pagina NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('Testa se contém um h2 com o texto Page requested not found 😭', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
