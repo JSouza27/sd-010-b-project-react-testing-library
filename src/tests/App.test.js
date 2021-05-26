7afb88eeb06f5b828c4a6c7332d0f41980f33b64
renatogsouza <renatomacedo1995@gmail.com>
2021-05-26 19:06:09 -0300

    requisito 1 feito com sucesso

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Routes', () => {
+  test('renders a reading with the text `Pokédex` and link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+
+    history.push('/');
+
+    const textHome = getByText('Home');
+
+    expect(textHome).toBeInTheDocument();
+  });
+  it('renders a reading with the link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/About');
+
+    const textHome = getByText('About');
+
+    expect(textHome).toBeInTheDocument();
+  });
+  it('renders a reading with the text `Favorite Pokémons`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/About');
+
+    const textHome = getByText('Favorite Pokémons');
+
+    expect(textHome).toBeInTheDocument();
+  });
 });
