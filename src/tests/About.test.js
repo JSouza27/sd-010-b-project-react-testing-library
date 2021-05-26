ac09039a19eb31c1ce5141eff948d08dbff1101a
legalvao1 <legalvao1@gmail.com>
2021-05-25 22:20:35 -0300

    Componente About - testando se tem a imagem da pokedex na pagina

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -14,4 +14,19 @@ describe('Test about page', () => {
 
     expect(pageTitle).toBeInTheDocument();
   });
+
+  test('if the page contains two paragraph about pokedex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphs = screen.getAllByText(/Pokémons/i);
+    expect(paragraphs.length).toBe(2);
+  });
+  // **Source https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src */
+  test('if the page contains the pokedex image', () => {
+    renderWithRouter(<About />);
+
+    const pokedexImage = screen.getByAltText('Pokédex');
+
+    expect(pokedexImage.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
