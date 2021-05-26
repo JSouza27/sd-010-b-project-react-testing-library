a7aad7e11716a08182ccc16a6a9a468f5ad5bb40
Cristian <cristian.bugs@gmail.com>
2021-05-25 21:19:12 -0300

    fourth task

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('check is there is a heading h2', () => {
+  renderWithRouter(<NotFound />);
+
+  const message = screen.getByRole('heading', {
+    level: 2,
+    name: 'Page requested not found Crying emoji',
+  });
+  expect(message).toBeInTheDocument();
+});
+
+test('check if the pages renders picture', () => {
+  renderWithRouter(<NotFound />);
+
+  const image = screen.getByAltText(/Pikachu crying because the page requested was not found/i);
+
+  expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
