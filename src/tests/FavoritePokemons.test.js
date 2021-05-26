89841721795d9afe4257fb5edce07aa565ceca8e
Leandro Reis <lleandrofr@gmail.com>
2021-05-26 11:36:25 -0300

    creates a test when a pokemons is selected as favorite

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Renders favorites Pokémons when needed', () => {
+  it('renders a message with text "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const message = getByText(/No favorite pokemon found/i);
+
+    expect(message).toBeInTheDocument();
+  });
+
+  it('renders a favorited pokemon when needed"', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const homeLink = getByText('Home');
+    const favoriteLink = getByText('Favorite Pokémons');
+    const moreDetailsLink = getByText('More details');
+
+    userEvent.click(homeLink);
+    userEvent.click(moreDetailsLink);
+
+    const favoriteBtn = getByText('Pokémon favoritado?');
+
+    userEvent.click(favoriteBtn);
+    userEvent.click(favoriteLink);
+
+    const pikachu = getByText('Pikachu');
+
+    expect(pikachu).toBeInTheDocument();
+  });
+});
