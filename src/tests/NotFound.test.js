77a407db10489a9ef0ad31930f6319e6b4d1d7b3
DennisMarcelo <dennismarcelo15@gmail.com>
2021-05-26 10:29:11 -0300

    requisiot 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('requisiot 4', () => {
+  test('page have text "Page requested not found" in the h2 tag', () => {
+    const altText = 'Pikachu crying because the page requested was not found';
+    renderWithRouter(<NotFound />);
+
+    const h2 = screen.getByRole('heading', {
+      level: 2, name: /Page requested not found/,
+    });
+    expect(h2).toBeInTheDocument();
+
+    const img = screen.getByAltText(altText);
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
