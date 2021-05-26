6f90bd12a7dcb8cad27c000e4aebb9bf449633f7
legalvao1 <legalvao1@gmail.com>
2021-05-25 21:44:06 -0300

    Teste do titulo do componente About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Test about page', () => {
+  test('pokedex title', () => {
+    renderWithRouter(<About />);
+
+    const pageTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+
+    expect(pageTitle).toBeInTheDocument();
+  });
+});
