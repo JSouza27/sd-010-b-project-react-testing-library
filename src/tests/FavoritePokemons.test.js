1e40f7113a5af7862f42a3b4144a55e4b253ca64
Roger Franco <rogerfrancogalt@gmail.com>
2021-05-25 22:28:59 -0400

    resolução do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+// import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+it('"No favorite pokemon found" is displayed, if does not have favorite pokemon.', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const notFoundFavorite = getByText(/No favorite pokemon found/);
+  expect(notFoundFavorite).toBeInTheDocument();
+});
+// it('if all favorite Pokémon cards are displayed.', () => {
+//   const { getByText, queryAllByAltText } = renderWithRouter(<FavoritePokemons />);
+//   fireEvent.click(getByText('More details'));
+//   fireEvent.click(getByText(/Pokemon favoritado?/i));
+//   fireEvent.click(getByText(/Favorite pokémons/i));
+//   const favoritePokemons = queryAllByAltText(/is marked as favorite/i).length;
+//   expect(favoritePokemons).toBe(1);
+// }); /* Perguntar por que está dando erro na linha 13 */
+it('no pokemon card is displayed, if it is not favored.', () => {
+  const { queryAllByAltText } = renderWithRouter(<FavoritePokemons />);
+
+  const noFavoritePokemons = queryAllByAltText(/is marked as favorite/i).length;
+  expect(noFavoritePokemons).toBe(0);
+});
