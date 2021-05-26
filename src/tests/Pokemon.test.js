34d024fcc9382314cedde13c7f0423f7c61f5754
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-26 16:46:24 -0300

    adiciona o teste do link details

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,15 +1,17 @@
 // import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/dom';
 import React from 'react';
 import App from '../App';
 import pokemons from '../data';
 import renderWithRouter from './renderWithRouter';
 
+const getPikachu = pokemons.filter((pokemon) => pokemon.name === 'Pikachu');
+const { averageWeight, image, name, type, id } = getPikachu[0];
+const { value, measurementUnit } = averageWeight;
+
 describe('testing component Pokemon.js', () => {
   test('shows card info pokémons', () => {
     const { getByTestId, getByRole } = renderWithRouter(<App />);
-    const getPikachu = pokemons.filter((pokemon) => pokemon.name === 'Pikachu');
-    const { averageWeight, image, name, type } = getPikachu[0];
-    const { value, measurementUnit } = averageWeight;
     const pokemonName = getByTestId('pokemon-name', { name: `${name}` });
     expect(pokemonName).toHaveTextContent(/pikachu/i);
     const pokemonType = getByTestId('pokemon-type', { name: `${type}` });
@@ -21,5 +23,16 @@ describe('testing component Pokemon.js', () => {
     const pokemonImage = getByRole('img', { name: `${name} sprite` });
     expect(pokemonImage).toHaveAttribute('src', `${image}`);
     expect(pokemonImage).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+
+  test('link details', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkDetails = getByText(/more details/i);
+    expect(linkDetails).toBeInTheDocument();
+    fireEvent.click(linkDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${id}`);
+    const textDatails = getByText(`${name} Details`);
+    expect(textDatails).toBeInTheDocument();
   });
 });
