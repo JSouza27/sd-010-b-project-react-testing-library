de5848631aad6c10066da5b478535e681f71139c
Hugo Simoes <hugolfsimoes@gmail.com>
2021-05-25 23:59:19 -0300

    feito os testes do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testando o componente NotFound.js ', () => {
+  test('Verifica se contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+    const notFound = getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+      level: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByAltText } = render(<NotFound />);
+    const img = getByAltText('Pikachu crying because the page requested was not found');
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
