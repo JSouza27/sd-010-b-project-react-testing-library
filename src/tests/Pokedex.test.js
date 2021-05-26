8640f0f311b7c261b0a5a59a93b5cd17376fbc7c
joao966 <joaooliveira966@gmail.com>
2021-05-26 01:10:07 -0300

    add: request-5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+
+import App from '../App';
+import data from '../data';
+
+const button = 'Próximo pokémon';
+const types = data.map(({ type }) => type);
+const buttonTypes = types.filter((type, index, array) => array.indexOf(type) === index);
+const getType = (value) => data.filter(({ type }) => type === value);
+
+describe('Testa pokedex', () => {
+  test('Testa h2 pokedex', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const pokemon = getByRole('heading', { level: 2 });
+    expect(pokemon).toHaveTextContent('Encountered pokémons');
+  });
+
+  test('Testa botão', () => {
+    const { getByText, getAllByText } = renderWithRouter(
+      <App />,
+    );
+
+    data.forEach(({ name }) => {
+      expect(getAllByText('More details').length).toBe(1);
+      expect(getByText(name)).toBeInTheDocument();
+      fireEvent.click(getByText(button));
+    });
+    expect(getByText(data[0].name)).toBeInTheDocument();
+  });
+
+  test('Testa pokemon por tipo`', () => {
+    const { getByRole, getByText, getAllByTestId } = renderWithRouter(<App />);
+    expect(getAllByTestId('pokemon-type-button').length).toBe(buttonTypes.length);
+    buttonTypes.forEach((name) => {
+      const btnType = getByRole('button', { name });
+      fireEvent.click(btnType);
+      getType(name).forEach(({ name: pokemon }) => {
+        expect(getByText(pokemon)).toBeInTheDocument();
+        fireEvent.click(getByText(button));
+      });
+    });
+
+    const buttonTodos = getByRole('button', { name: 'All' });
+    fireEvent.click(buttonTodos);
+    data.forEach(({ name }) => {
+      expect(getByText(name)).toBeInTheDocument();
+      fireEvent.click(getByText(button));
+    });
+  });
+});
