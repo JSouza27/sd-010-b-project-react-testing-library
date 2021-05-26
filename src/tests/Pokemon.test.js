b6fd2df738e876d41a259199e06f53200342afaa
RafaMaths <rmathscoach@gmail.com>
2021-05-26 19:30:18 -0300

    Test if a card is rendered with the information.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('Teste se renderiza um card com as informações de determinado pokémon.', () => {
+    renderWithRouter(<App />);
+
+    const namePokemon = screen.getByTestId('pokemon-name');
+    expect(namePokemon).toHaveTextContent('Pikachu');
+
+    const typePokemon = screen.getByTestId('pokemon-type');
+    expect(typePokemon).toHaveTextContent('Electric');
+
+    const weightPokemon = screen.getByTestId('pokemon-weight');
+    expect(weightPokemon).toHaveTextContent('Average weight: 6.0 kg');
+
+    const imagePokemon = screen.getByRole('img');
+    expect(imagePokemon).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
