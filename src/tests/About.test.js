f9cb863663a3c7682334d5660b9299bce8cd62dc
Paulo Xavier <pxavierf@gmail.com>
2021-05-26 16:30:53 -0300

    testa se contem o heading na pagina about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,3 +7,12 @@ test('Testa se contém informações sobre a Pokedex', () => {
   const p = getByText(/This application simulates/i);
   expect(p).toBeInTheDocument();
 });
+
+test('Testa se contém Heading com About Pokedex', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /About Pokédex/i,
+  });
+  expect(heading).toBeInTheDocument();
+});
