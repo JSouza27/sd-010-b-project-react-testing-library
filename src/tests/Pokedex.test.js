1aac49e591c473e7a0f4fc489ea8ddf9d28a63de
edersonrodara <edersonrodara@gmail.com>
2021-05-26 19:28:05 -0300

    requisito 5.4 completo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -31,6 +31,68 @@ describe('Testar o Pokedex.js', () => {
     expect(firstPokemon).toBeInTheDocument();
   });
 
+  test('Verifica se é exibido um pokémon por vez.', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const oneDetails = getByText(/More details/i);
+    expect(oneDetails).toBeInTheDocument();
+  });
+
+  test('Testando os botões filtro fire e all.', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const typeFire = screen.getByRole('button', {
+      name: /Fire/i,
+    });
+    userEvent.click(typeFire);
+    const Charmander = getByText(/Charmander/i);
+    expect(Charmander).toBeInTheDocument();
+
+    const nextPokemon = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    userEvent.click(nextPokemon);
+    const Rapidash = getByText(/Rapidash/i);
+    expect(Rapidash).toBeInTheDocument();
+
+    const allPokemon = screen.getByRole('button', {
+      name: /All/i,
+    });
+    userEvent.click(allPokemon);
+    const Pikachu = getByText(/Pikachu/i);
+    expect(Pikachu).toBeInTheDocument();
+  });
+
+  // test('Teste se a Pokédex tem os botões de filtro.', () => {
+  //   renderWithRouter(<App />);
+
+  //   const typesPokemon = [];
+  //   data.forEach(({ type }) => {
+  //     if (!typesPokemon.includes(type)) {
+  //       typesPokemon.push(type);
+  //     }
+  //   });
+
+  //   data.forEach(({ type }) => {
+  //     let quantity = 0;
+
+  //     if (typesPokemon.includes(type)) {
+  //       quantity += 1;
+  //     }
+  //     if (quantity > 1) {
+  //       const buttonType = screen.getByRole('button', {
+  //         name: type,
+  //       });
+  //       userEvent.click(buttonType);
+  //       const buttonNext = screen.getByRole('button', {
+  //         name: type,
+  //       });
+  //       userEvent.click(buttonNext);
+  //       expect(type).toBeInTheDocument();
+  //     }
+  //   });
+  // });
+
   // test('Testando se renderiza os pokémons favoritos', () => {
   //   renderWithRouter(<App />);
 
