d98250b165c5c3dcb19afeab00a5637637232ed7
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-26 15:13:24 -0300

    definindo uma const para o nome do botão que estva sendo reutilizada em varios lugares

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,6 +3,8 @@ import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
+const nameButton = 'Próximo pokémon';
+
 describe('Teste do quinto requisito', () => {
   test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
     renderWithRouter(<App />);
@@ -14,14 +16,14 @@ describe('Teste do quinto requisito', () => {
   describe('Proximo Pokémon da lista quando o botão Próximo pokémon é clicado', () => {
     test('Teste se o botão tem o texto Próximo pokémon', () => {
       renderWithRouter(<App />);
-      const btnText = screen.getByText('Próximo pokémon');
+      const btnText = screen.getByText(nameButton);
 
       expect(btnText).toBeInTheDocument();
     });
 
     test('Teste se os próximos Pokémons são mostrados', () => {
       renderWithRouter(<App />);
-      const btnText = screen.getByRole('button', { name: 'Próximo pokémon' });
+      const btnText = screen.getByRole('button', { name: nameButton });
       fireEvent.click(btnText);
       const testePokemon = screen.getByText('Charmander');
 
@@ -30,12 +32,12 @@ describe('Teste do quinto requisito', () => {
 
     test('O primeiro Pokémon da lista deve ser mostrado ao chegar no último', () => {
       renderWithRouter(<App />);
-      const btnText = screen.getByRole('button', { name: 'Próximo pokémon' });
+      const btnText = screen.getByRole('button', { name: nameButton });
       const testePokemon = screen.getByText('Pikachu');
 
       const numeroClick = 9;
       for (let i = 0; i < numeroClick; i += 1) {
-        userEvent.click(btnText);
+        fireEvent.click(btnText);
       }
 
       expect(testePokemon).toBeInTheDocument();
