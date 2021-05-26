b530659b99a5dda0db0d58421c7945fc0b99a442
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-05-26 17:47:26 -0300

    Implement the tests for the Pokemon component and begins the implementation of the tests for the Pokedex component

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+describe('tests the pokedex component', () => {
+  it('tests if the page has an h2 with the right text', () => {
+    const isFavorite = pokemons.reduce((acc, curr) => {
+      acc[curr.id] = false;
+      return acc;
+    }, {});
+    const { getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isFavorite }
+    />);
+    const enconunteredPokemons = getByText(/Encountered pokémons/i);
+    expect(enconunteredPokemons).toBeInTheDocument();
+    expect(enconunteredPokemons.nodeName).toBe('H2');
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from './renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+describe('tests the Pokemon component', () => {
+  it('tests if it render the information about a determined pokemon', () => {
+    const pikachu = pokemons[0];
+    const { getByText, getByRole } = renderWithRouter(<Pokemon
+      isFavorite={ false }
+      pokemon={ pikachu }
+    />);
+    const pokeName = getByText(/Pikachu/i);
+    const pokeType = getByText(/Electric/i);
+    const { value, measurementUnit } = pikachu.averageWeight;
+    const pokeWeigth = getByText(`Average weight: ${value} ${measurementUnit}`);
+    const pokeImg = getByRole('img');
+    expect(pokeName).toBeInTheDocument();
+    expect(pokeType).toBeInTheDocument();
+    expect(pokeWeigth).toBeInTheDocument();
+    expect(pokeImg).toBeInTheDocument();
+    expect(pokeImg.outerHTML).toContain(pikachu.image);
+    expect(pokeImg.outerHTML).toContain(`${pikachu.name} sprite`);
+  });
+  it('tests if there is a nav link to pokemonDetails and if it works', () => {
+    const pikachu = pokemons[0];
+    const { getByText, history } = renderWithRouter(<Pokemon
+      isFavorite={ false }
+      pokemon={ pikachu }
+    />);
+    const moreDetails = getByText(/More details/i);
+    expect(moreDetails).toBeInTheDocument();
+    fireEvent.click(moreDetails);
+    expect(history.location.pathname).toBe(`/pokemons/${pikachu.id}`);
+  });
+  it('tests if when the pokemon is favorited it shows the favorited logo', () => {
+    const pikachu = pokemons[0];
+    const { getAllByRole } = renderWithRouter(<Pokemon
+      isFavorite
+      pokemon={ pikachu }
+    />);
+    const allImgs = getAllByRole('img');
+    const favoriteIcon = allImgs[1];
+    expect(favoriteIcon).toBeInTheDocument();
+    expect(favoriteIcon.outerHTML).toContain('src="/star-icon.svg"');
+    const expectedAld = `alt="${pikachu.name} is marked as favorite"`;
+    expect(favoriteIcon.outerHTML).toContain(expectedAld);
+  });
+});
