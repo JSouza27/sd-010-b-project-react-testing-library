6a445556e8de7234d147e1d651d85992e737893e
nascimento <esio.392@protonmail.com>
2021-05-26 14:29:11 -0400

    concluido req 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import About from '../components/About';
 
 describe('Teste component About', () => {
   test('Pagina contem um heading com text About Pokedex', () => {
@@ -17,5 +18,12 @@ describe('Teste component About', () => {
       name: 'About Pokédex',
     });
     expect(aboutText).toBeInTheDocument();
+  });
+  test('Contém img de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const img = screen.getByAltText('Pokédex');
+
+    expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
