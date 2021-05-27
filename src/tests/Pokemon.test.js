d68a035633780aa04323866ec58d21ebe9b6c639
nascimento <esio.392@protonmail.com>
2021-05-27 15:45:26 -0400

    vqv terminar req 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import Data from '../data';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('O nome correto do Pokémon deve ser mostrado na tela', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const nome = getByTestId('pokemon-name');
+    expect(nome.innerHTML).toBe(Data[0].name);
+  });
+  test('O tipo correto do pokémon deve ser mostrado na tela.', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const tipo = getByTestId('pokemon-type');
+    expect(tipo.innerHTML).toBe(Data[0].type);
+  });
+  test('O peso médio do pokémon', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const peso = Data[0].averageWeight.value;
+    console.log(peso);
+    const unidade = Data[0].averageWeight.measurementUnit;
+    console.log(unidade);
+    const resul = getByTestId('pokemon-weight').innerHTML;
+    expect(resul).toBe(`Average weight: ${peso} ${unidade}`);
+  });
+});
