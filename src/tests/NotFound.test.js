c3f2182a570852ba740c5d81c3dff15ff78d129c
renatogsouza <renatomacedo1995@gmail.com>
2021-05-27 16:06:47 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/123');
+
+    const textNotFound = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(textNotFound).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getAllByRole, history } = renderWithRouter(<App />);
+
+    history.push('/123');
+
+    const imgNotFound = getAllByRole('img');
+    expect(imgNotFound[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
