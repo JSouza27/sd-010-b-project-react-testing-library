1edde06453586e6a060e3377df9b3bd271e5af12
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-05-27 20:31:35 -0300

    requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
 test('', () => {});
+
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+
+// import FavoritePokemons from '../components/FavoritePokemons';
+// import renderWithRouter from './renderWithRouter';
+
+// describe('Testando o componente <FavoritePokemons.js />', () => {
+//   it('Testanto se a pessoa não tiver pokémons favoritos', () => {
+//     const { getByText, getByAltText } = renderWithRouter(<FavoritePokemons />);
+// });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente <About.js />', () => {
+  it('Testanto se há informações sobre a Pokédex', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    const img = getByAltText('Pikachu crying because the page requested was not found');
+    expect(notFound).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
