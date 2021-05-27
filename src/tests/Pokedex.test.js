e4d7f237aa9102350058f36d8c839e261856b9a0
root <diegoserafim1@gmail.com>
2021-05-26 22:12:52 -0300

    o teste alvo foi mudado de Pokedex para App, e foi criado o teste para verificar se muda de pokemon ao clicar no botão proximo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,14 +1,27 @@
+import { fireEvent } from '@testing-library/dom';
 import React from 'react';
-import { Pokedex } from '../components';
+import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
+import pokemons from '../data';
 
 describe('Teste o componente <Pokedex.js />', () => {
   test('Testa se página contém um h2 com o texto Encountered pokémons.', () => {
-    const { getByRole } = renderWithRouter(<Pokedex />);
+    const { getByRole } = renderWithRouter(<App />);
     const heading = getByRole('heading', {
       level: 2,
       name: /Encountered pokémons/i,
     });
     expect(heading).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido o próximo Pokémon quando o botão Próximo é clicado.', () => {
+    const { getAllByText, getByText } = renderWithRouter(<App pokemons={ pokemons } />);
+    const buttonNext = 'Próximo pokémon';
+    pokemons.forEach(({ name }) => {
+      expect(getByText(name)).toBeInTheDocument();
+      expect(getAllByText('More details').length).toBe(1);
+      fireEvent.click(getByText(buttonNext));
+    });
+    expect(getByText(pokemons[0].name)).toBeInTheDocument();
   });
 });
