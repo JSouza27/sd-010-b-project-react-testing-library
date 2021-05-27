37f4510c5d900bae61ac681827d9535d35540c79
MarceloPelegrino <pelegrinocorrea@gmail>
2021-05-27 15:06:59 -0300

    Requirement 3 finalized

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('tests the <FavoritePokemons /> component', () => {
+  test('when the person does not have favorite Pokémon', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const message = getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+  test('if all favorite Pokémon cards are displayed', () => {
+    const { getByText, queryAllByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByText('More details'));
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    fireEvent.click(getByText('Favorite Pokémons'));
+    // const message = getByText('No favorite pokemon found');
+    // expect(message).not.toBeInTheDocument();
+    const cards = queryAllByTestId('pokemon-name');
+    expect(cards.length > 0).toBe(true);
+  });
+  test('if no Pokémon card is displayed, if it is not favored', () => {
+    const { queryAllByTestId } = renderWithRouter(<FavoritePokemons />);
+    const cards = queryAllByTestId('pokemon-name');
+    expect(cards.length === 0).toBe(true);
+  });
+});
