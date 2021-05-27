28f0ef765f1469bf688514a7549bfd944dfe13a9
Misael Bueno <misa.hbueno@gmail.com>
2021-05-27 14:55:43 -0300

    requisitos 5 e 7

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -5,6 +5,7 @@ import App from '../App';
 import pokemons from '../data';
 
 describe('Testes do requisito 5', () => {
+  const NEXT_POKEMON = 'next-pokemon'; // lint requer que seja feito uma const para não ser declarado 3 vezes.
   it('teste se página contém um heading h2 com o texto encountered pokémons', () => {
     const { getByRole } = renderWithRouter(<App />);
 
@@ -34,11 +35,13 @@ describe('Testes do requisito 5', () => {
   it('Verifica se os próximos pokémons da lista são mostrados', () => {
     const { getByTestId, getByText } = renderWithRouter(<App />);
 
-    const btn = getByTestId('next-pokemon');
+    const btn = getByTestId(NEXT_POKEMON);
     pokemons.forEach((pokemon) => {
       expect(getByText(pokemon.name)).toBeInTheDocument();
       fireEvent.click(btn);
     });
+
+    expect(getByText(pokemons[0].name)).toBeInTheDocument();
   });
 
   it('Testa se a Pokédex tem os botões de filtro', () => {
@@ -46,6 +49,21 @@ describe('Testes do requisito 5', () => {
     const btnType = getAllByTestId('pokemon-type-button');
 
     expect(btnType).toBeDefined();
+  });
+
+  it('Testa se a Pokédex circula somente pelos pokémons de um tipo selecionado', () => {
+    const { getAllByTestId, getByTestId, getByText } = renderWithRouter(<App />);
+
+    const btnNext = getByTestId(NEXT_POKEMON);
+    const btnType = getAllByTestId('pokemon-type-button');
+
+    fireEvent.click(btnType[1]);
+    pokemons.forEach((pokemon) => {
+      if (pokemon.type === 'Fire') {
+        expect(getByText(pokemon.name)).toBeInTheDocument();
+        fireEvent.click(btnNext);
+      }
+    });
   });
 
   it('Verifica se texto do botão corresponde ao nome do tipo', () => {
@@ -61,18 +79,28 @@ describe('Testes do requisito 5', () => {
   });
 
   it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
-    const { getByRole, getByText } = renderWithRouter(<App />);
+    const { getByRole, getByText, getByTestId } = renderWithRouter(<App />);
 
+    const btnNext = getByTestId(NEXT_POKEMON);
     const textBtn = getByRole('button', {
       name: 'All',
     });
     expect(textBtn).toBeInTheDocument();
-
+    fireEvent.click(textBtn);
     pokemons.forEach((pokemon) => {
-      fireEvent.click(textBtn);
-      if (pokemon.name === 'Pikachu') {
-        expect(getByText(pokemon.name)).toBeInTheDocument();
-      }
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(btnNext);
+    });
+  });
+
+  it('Testa se existe um botão de filtro para cada tipo de Pokémon', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    pokemons.forEach(({ type }) => {
+      const btnRole = getByRole('button', {
+        name: type,
+      });
+      expect(btnRole).toBeInTheDocument();
     });
   });
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,100 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes do requisito 7', () => {
+  const MORE_DETAILS = 'More details';
+  it('Teste se as informações detalhadas do Pokémon são mostradas na tela', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+
+    const link = getByRole('link', {
+      name: MORE_DETAILS,
+    });
+
+    expect(link).toBeInTheDocument();
+
+    fireEvent.click(link);
+    const text = getByText('Pikachu Details');
+    expect(text).toBeDefined();
+    expect(link).not.toBeInTheDocument();
+  });
+
+  it('Testa se seção de detalhes contem um heading h2 com o texto Summary', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    const link = getByRole('link', {
+      name: MORE_DETAILS,
+    });
+
+    fireEvent.click(link);
+
+    const heading = getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    });
+
+    expect(heading).toBeDefined();
+  });
+
+  it('Testa se seção de detalhes tem um parágrafo com o resumo do Pokémon', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+
+    const link = getByRole('link', {
+      name: MORE_DETAILS,
+    });
+    fireEvent.click(link);
+
+    const summary = getByText(pokemons[0].summary);
+    expect(summary).toBeInTheDocument();
+  });
+});
+
+describe('Teste se existe na página uma seção com os mapas de localizações', () => {
+  const MORE_DETAILS = 'More details';
+  it('Teste se deve existir um h2 com o texto Game Locations of <name>', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    const link = getByRole('link', {
+      name: MORE_DETAILS,
+    });
+    fireEvent.click(link);
+
+    const headingLoc = getByRole('heading', {
+      level: 2,
+      name: 'Game Locations of Pikachu',
+    });
+
+    expect(headingLoc).toBeInTheDocument();
+  });
+
+  it('testa se todas as localizações do Pokémon são mostradas ', () => {
+    const { getByRole, getAllByAltText } = renderWithRouter(<App />);
+
+    const link = getByRole('link', {
+      name: MORE_DETAILS,
+    });
+    fireEvent.click(link);
+
+    const alt = getAllByAltText('Pikachu location');
+    const found = pokemons[0].foundAt;
+
+    expect(alt[0]).toHaveAttribute('src', found[0].map);
+    expect(alt[1]).toHaveAttribute('src', found[1].map);
+  });
+
+  it('deve exibir um checkbox que permite favoritar o Pokémon', () => {
+    const { getByRole, getByLabelText } = renderWithRouter(<App />);
+
+    const link = getByRole('link', {
+      name: MORE_DETAILS,
+    });
+    fireEvent.click(link);
+
+    const checkbox = getByRole('checkbox');
+    const labelText = getByLabelText('Pokémon favoritado?');
+    expect(checkbox).toBeInTheDocument();
+    expect(labelText).toBeInTheDocument();
+  });
+});
