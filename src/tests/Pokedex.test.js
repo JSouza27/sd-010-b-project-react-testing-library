1ebf022f0e5c99f8581f93a26f263b47527eaef9
isTorres <ismaeltorres612@gmail.com>
2021-05-27 16:35:06 -0300

    add tests for pokedex component

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,6 +4,40 @@ import renderWithRouter from '../helper/renderWithRouter';
 // import Pokedex from '../components/Pokedex';
 import App from '../App';
 
+const pokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+  },
+  {
+    id: 10,
+    name: 'Caterpie',
+    type: 'Bug',
+    averageWeight: {
+      value: '2.9',
+      measurementUnit: 'kg',
+    },
+  },
+];
+
+global.fetch(jest.fn().mockResolvedValue({
+  json: jest.fn().mockResolvedValue(pokemons),
+}));
+
 describe('Testing the `Pokédex` component', () => {
   it('Test if pokedex page/home have a heading', () => {
     const { getByRole } = renderWithRouter(<App />);
@@ -14,40 +48,6 @@ describe('Testing the `Pokédex` component', () => {
   });
 
   it('test if the btn have text `Próximo pokémon`', () => {
-    const pokemons = [
-      {
-        id: 25,
-        name: 'Pikachu',
-        type: 'Electric',
-        averageWeight: {
-          value: '6.0',
-          measurementUnit: 'kg',
-        },
-      },
-      {
-        id: 4,
-        name: 'Charmander',
-        type: 'Fire',
-        averageWeight: {
-          value: '8.5',
-          measurementUnit: 'kg',
-        },
-      },
-      {
-        id: 10,
-        name: 'Caterpie',
-        type: 'Bug',
-        averageWeight: {
-          value: '2.9',
-          measurementUnit: 'kg',
-        },
-      },
-    ];
-
-    global.fetch(jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue(pokemons),
-    }));
-
     const { getByRole, getByText } = renderWithRouter(<App />);
     const firstPoke = getByText('Pikachu');
     expect(firstPoke).toBeInTheDocument();
@@ -66,7 +66,22 @@ describe('Testing the `Pokédex` component', () => {
     expect(firstPoke).toBeInTheDocument();
   });
 
-  // it('Teste se a Pokédex tem os botões de filtro', () => {
-  //   console.log('oi');
-  // });
+  it('Test if the filter button limit the pokedex', () => {
+    const { getByRole, getByTestId } = renderWithRouter(<App />);
+    const type = 'Electric';
+    const btnFilter = getByRole('button', { name: type });
+    expect(btnFilter).toBeInTheDocument();
+
+    fireEvent.click(btnFilter);
+    expect(getByTestId('pokemon-type').innerHTML).toBe(type);
+    const nextPoke = getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPoke.disabled).toBe(true);
+
+    const btnAll = getByRole('button', { name: /All/i });
+    expect(btnAll).toBeInTheDocument();
+    fireEvent.click(btnAll);
+    expect(nextPoke.disabled).toBe(false);
+
+
+  });
 });
