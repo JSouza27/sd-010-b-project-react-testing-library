a34c557d8df47d7cec4943ea1869cb04fcd1bdde
aline <aline.melryaa@gmail.com>
2021-05-27 18:36:37 -0400

    verifica favorito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -30,4 +30,14 @@ describe('renderize um conjunto de links', () => {
     const links = getAllByRole('link');
     expect(links[0].textContent).toBe('Home');
   });
+  it('Verifica se o segundo link possui o texto "About"', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const links = getAllByRole('link');
+    expect(links[1].textContent).toBe('About');
+  });
+  it('Verifica se o terceiro link possui o texto "Favorite Pokémons"', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const links = getAllByRole('link');
+    expect(links[2].textContent).toBe('Favorite Pokémons');
+  });
 });
