a7bfe481bf87990f05cd2a002a42b575329eee13
brunobastosfer <brunobastos@edu.unifor.br>
2021-05-27 19:18:28 -0300

    refatorando o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
+import data from '../data';
 
-test('Testando o component Pokemon', () => {
+test('Testando o pokemon favorito', () => {
   const { container } = render(
     <BrowserRouter>
-      <FavoritePokemons />
+      <FavoritePokemons pokemons={ [data[0]] } />
     </BrowserRouter>,
   );
   const textPokemon = screen.getByRole('heading', {
@@ -15,13 +16,18 @@ test('Testando o component Pokemon', () => {
   });
   expect(textPokemon).toBeInTheDocument();
 
-  const noFavorite = {
-    case1: screen.getByText('No favorite pokemon found'),
-    case2: screen.getByText('Favorite pokémons'),
-  };
-  const getByClass = container.querySelector('[class="favorite-pokemons"]');
-  const getChildren = getByClass !== null ? getByClass.children : null;
-  if (getChildren) {
-    expect(noFavorite.case1).toBeInTheDocument();
-  } else expect(noFavorite.case2).toBeInTheDocument();
+  const getFavorite = screen.getByText('Favorite pokémons');
+  expect(getFavorite).toBeInTheDocument();
+  const getByClass = container.querySelector('.favorite-pokemons');
+  expect(getByClass.childElementCount).toEqual(1);
+});
+
+test('Testando se não há pokemon favorito', () => {
+  render(
+    <BrowserRouter>
+      <FavoritePokemons />
+    </BrowserRouter>,
+  );
+  const getNotFavorite = screen.getByText('No favorite pokemon found');
+  expect(getNotFavorite).toBeInTheDocument();
 });
