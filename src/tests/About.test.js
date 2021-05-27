5de6d8630b7594c0c7d652caa6db1da86c09d292
renatogsouza <renatomacedo1995@gmail.com>
2021-05-27 14:41:23 -0300

    requisito 2 feito com sucesso

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testar se a pagina contém', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/about');
+
+    const textAboutPokemon = getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+
+    expect(textAboutPokemon).toBeInTheDocument();
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/about');
+
+    const imageAbout = getByRole('img');
+    expect(imageAbout).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
