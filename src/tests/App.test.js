5e08a8968a6a09f05c414826b4223f3893fe7493
walterlbf <walter.lbf.f@gmail.com>
2021-05-27 15:24:52 -0400

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('Págida principal é renderizada ao carregar no caminho de URL "/"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const route = '/';
+  history.push(route);
+
+  const appPage = getByText(/Encountered pokémons/i);
+  expect(appPage).toBeInTheDocument();
+});
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText(/Home/i);
+  const about = getByText(/About/i);
+  const favoritePokemons = getByText(/Favorite Pokémons/i);
+
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favoritePokemons).toBeInTheDocument();
+});
+
+test('É redirecionada para a página Not Found ao entrar em uma URL desconhecida', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/pageNotFound');
+  expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
 });
