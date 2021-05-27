d778488865b01074b9eda4652d55ec8a9485018d
Jonathan Souza <souzajonathan570@gmail.com>
2021-05-26 21:09:56 -0300

    correçao de lint no component About linha 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { queryAllByRole, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import About from '../components/About';
 
