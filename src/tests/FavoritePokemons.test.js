d117a481962f4f40ee4c33a05b75eaedf9d95848
Lara Capila <laracapilak@gmail.com>
2021-05-27 18:18:54 -0300

    requirement 3 - test if card is displayed

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,4 +1,6 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
+import App from '../App';
 import { FavoritePokemons } from '../components';
 import renderWithRouter from './renderWithRouter';
 
@@ -8,4 +10,25 @@ test('Test if "No favorite pokemon found", when you have no favorite pokemon', (
   const favoriteText = getByText(/No favorite pokemon found/i);
 
   expect(favoriteText).toBeInTheDocument();
+});
+
+test('test if card is displayed', () => {
+  const { getByRole, getAllByRole } = renderWithRouter(<App />);
+
+  const linkMoreDetailsHome = getByRole('link', {
+    name: /more details/i,
+  });
+  userEvent.click(linkMoreDetailsHome);
+
+  const checkPokemonFavorite = getByRole('checkbox');
+  userEvent.click(checkPokemonFavorite);
+
+  const linkFavoritePokemons = getByRole('link', {
+    name: /favorite pokémons/i,
+  });
+  userEvent.click(linkFavoritePokemons);
+
+  const imgFavoriteStar = getAllByRole('img');
+
+  expect(imgFavoriteStar[1]).toHaveAttribute('src', '/star-icon.svg');
 });
