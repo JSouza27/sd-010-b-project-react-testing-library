b23c87a01083dcfeeae048fe03b77188db1d4612
root <diegoserafim1@gmail.com>
2021-05-27 19:23:58 -0300

    criado o teste para verificar se não tem o link de detalhes do poke

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -8,8 +8,8 @@ import pokemons from '../data';
 describe('Testa o componente <PokemonDetails.js />', () => {
   test('Testa se as informações do Pokémon selecionado são mostradas.', () => {});
   const { getByRole, getByText } = renderWithRouter(<App />);
-  const { name } = pokemons[0];
   const details = /More details/i;
+  const { name } = pokemons[0];
   fireEvent.click(getByText(details));
   const heading = getByRole('heading', {
     level: 2,
@@ -18,10 +18,21 @@ describe('Testa o componente <PokemonDetails.js />', () => {
   expect(heading).toBeInTheDocument();
   expect(getByRole('heading', {
     level: 2,
-    name: `Game Locations of ${name}`,
+    name: /Summary/i,
   })).toBeInTheDocument();
   expect(getByRole('heading', {
     level: 2,
-    name: /Summary/i,
+    name: `Game Locations of ${name}`,
   })).toBeInTheDocument();
+
+  test('Testa se não há um link de navegação para os detalhes do Pokémon.', () => {
+    const { queryByText } = renderWithRouter(<App />);
+    // const details = /More details/i;
+    fireEvent.click(queryByText(details));
+    expect(queryByText(details)).toBeNull();
+  });
+
+  test('Testa se há uma seção com os mapas contendo as localizações do pokémon', () => {
+
+  });
 });
