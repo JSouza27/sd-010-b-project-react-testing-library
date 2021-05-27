f64e3fb2be5aba525ae8f2b274679eff5edfc2c2
brunobastosfer <brunobastos@edu.unifor.br>
2021-05-26 23:40:35 -0300

    refatorando a primeira questão

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -17,13 +19,55 @@ test('renders a reading with the text `Pokédex`', () => {
   });
   expect(homeLink).toBeInTheDocument();
 
+  userEvent.click(homeLink);
+
+  const homeTxt = screen.getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i,
+  });
+  expect(homeTxt).toBeInTheDocument();
+
   const aboutLink = screen.getByRole('link', {
     name: /about/i,
   });
   expect(aboutLink).toBeInTheDocument();
 
+  userEvent.click(aboutLink);
+
+  const AboutTxt = screen.getByRole('heading', {
+    level: 2,
+    name: /About Pokédex/i,
+  });
+  expect(AboutTxt).toBeInTheDocument();
+
   const favoriteLink = screen.getByRole('link', {
     name: /favorite pokémons/i,
   });
   expect(favoriteLink).toBeInTheDocument();
+
+  userEvent.click(favoriteLink);
+
+  const favoriteTxt = screen.getByRole('heading', {
+    level: 2,
+    name: /Favorite Pokémons/i,
+  });
+  expect(favoriteTxt).toBeInTheDocument();
+});
+
+test('Verifica se a página existe', () => {
+  const historyMock = createMemoryHistory();
+
+  render(
+    <Router history={ historyMock }>
+      <App />
+    </Router>,
+  );
+
+  historyMock.push('/rota-not-found');
+
+  const notFoundTxt = screen.getByRole('heading', {
+    name: /Page requested not found/i,
+    level: 2,
+  });
+  expect(notFoundTxt).toBeInTheDocument();
 });
