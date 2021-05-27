b2e843e9d2abee954bc46ccfd131253fe8dda468
EderPaiva <edergomesdepaiva12@gmail.com>
2021-05-27 17:04:36 -0300

    falta apenas arrumar a funçao auxiliar e fazer e testar o alt e svg de favoritado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+const getByTestId = (dataTestId, valueToMatch) => {
+  const testId = screen.getByTestId(dataTestId);
+  expect(testId.innerHTML).toMatch(valueToMatch);
+};
+
+describe('6 - Pokemons info, sprites, image, routes', () => {
+  test('1- Pokemon infos', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    // testando infos
+    getByTestId('pokemon-name', 'Pikachu');
+    getByTestId('pokemon-type', 'Electric');
+    getByTestId('pokemon-weight', '6.0 kg');
+    const spriteOfPikachu = screen.getByAltText('Pikachu sprite');
+    expect(spriteOfPikachu.alt).toMatch('Pikachu sprite');
+    expect(spriteOfPikachu.src).toMatch('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    const moreDetails = screen.getByRole('link', {
+      name: 'More details',
+    });
+    userEvent.click(moreDetails);
+    const { location: { pathname } } = history;
+    expect(pathname).toMatch('/pokemons/25');
+    const checkbox = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(checkbox);
+  });
+});
