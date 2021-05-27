5a50393b669e6d80dd8b3269244f4b9186ccaef3
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-05-27 16:21:46 -0300

    testando conjunto fixo de links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+// import { createMemoryHistory } from 'history';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+// Entendeimento da função MemoryRouter: https://reactrouter.com/web/api/MemoryRouter
+describe('Testando o componente <App.js />', () => {
+  it('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('Testando o conjunto fixo de links de navegação`/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
 });
