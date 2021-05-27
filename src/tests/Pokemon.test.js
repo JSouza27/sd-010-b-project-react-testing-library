31ba73fa0cc9ed8e2aedf61be2118727cd55c201
root <diegoserafim1@gmail.com>
2021-05-27 12:12:16 -0300

    criado o test para verificar as informação do Pokemon (nome, tipo, peso, imagem)

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { Pokemon } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+import pokemons from '../data';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('Testa se é renderizado um card com as informações do pokémon.', () => {
+    const { getByText, getByAltText } = renderWithRouter(
+      <Pokemon pokemon={ pokemons[0] } />,
+    );
+    const { name, type, averageWeight: { value, measurementUnit }, image } = pokemons[0];
+    expect(getByText(name)).toBeInTheDocument();
+    expect(getByText(type)).toBeInTheDocument();
+    expect(getByText(`Average weight: ${value} ${measurementUnit}`)).toBeInTheDocument();
+    expect(getByText(/More details/i)).toBeInTheDocument();
+    expect(getByText((params, { src }) => src === image)).toBeInTheDocument();
+    expect(getByAltText(`${name} sprite`)).toBeInTheDocument();
+  });
+});
