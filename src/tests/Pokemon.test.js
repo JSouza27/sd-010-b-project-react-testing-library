f314a2ccd6b5f81acee824202b8e1cf959c895d2
root <diegoserafim1@gmail.com>
2021-05-27 12:42:46 -0300

    criado o teste para verificar o link de navegação da pagina pokemon, verifica se mostra a pagina detalhes

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,3 +1,4 @@
+import { fireEvent } from '@testing-library/dom';
 import React from 'react';
 import { Pokemon } from '../components';
 import renderWithRouter from '../components/renderWithRouter';
@@ -6,7 +7,7 @@ import pokemons from '../data';
 describe('Teste o componente <Pokemon.js />', () => {
   test('Testa se é renderizado um card com as informações do pokémon.', () => {
     const { getByText, getByAltText } = renderWithRouter(
-      <Pokemon pokemon={ pokemons[0] } />,
+      <Pokemon pokemon={ pokemons[0] } isFavorite />,
     );
     const { name, type, averageWeight: { value, measurementUnit }, image } = pokemons[0];
     expect(getByText(name)).toBeInTheDocument();
@@ -15,5 +16,14 @@ describe('Teste o componente <Pokemon.js />', () => {
     expect(getByText(/More details/i)).toBeInTheDocument();
     expect(getByText((params, { src }) => src === image)).toBeInTheDocument();
     expect(getByAltText(`${name} sprite`)).toBeInTheDocument();
+  });
+
+  test('A Pokédex contém um link para exibir detalhes. <id> é o id do Pokémon;', () => {
+    const { getByText, history } = renderWithRouter(
+      <Pokemon pokemon={ pokemons[0] } isFavorite />,
+    );
+    const { id } = pokemons[0];
+    fireEvent.click(getByText(/More details/i));
+    expect(history.location.pathname).toBe(`/pokemons/${id}`);
   });
 });
