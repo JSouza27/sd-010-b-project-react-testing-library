51d5a3fbeb00642c08eed51e157e10c8b5618acd
root <diegoserafim1@gmail.com>
2021-05-26 21:00:40 -0300

    criado o teste para verificar se contem o texto indicado no H2 indicado na pagina Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { Pokedex } from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Testa se página contém um h2 com o texto Encountered pokémons.', () => {
+    const { getByRole } = renderWithRouter(<Pokedex />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
