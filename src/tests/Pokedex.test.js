5809a7abbe75e700f31d12146198a55f16fd2b11
Matheus H. Gois <matheus.hengo@gmail.com>
2021-05-27 20:14:14 -0300

    Verify if buttons are created dynamically

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,6 +2,63 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+const POKEMONS_TEST = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: '...',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: '...',
+  },
+];
+const FAVORITE_OBJ = { 4: false, 25: false };
 
 describe('Testing Pokedex.js', () => {
   const NEXT_BUTTON = 'Próximo pokémon';
@@ -105,5 +162,24 @@ describe('Testing Pokedex.js', () => {
 
     userEvent.click(nextButton);
     expect(getByText('Caterpie')).toBeInTheDocument();
+  });
+
+  it('Verify if buttons are created dinamically', () => {
+    const { getByRole, getAllByRole } = renderWithRouter(
+      <Pokedex pokemons={ POKEMONS_TEST } isPokemonFavoriteById={ FAVORITE_OBJ } />,
+    );
+
+    const electricType = getByRole('button', { name: 'Electric' });
+    const fireType = getByRole('button', { name: 'Fire' });
+    const allType = getByRole('button', { name: 'All' });
+
+    expect(electricType).toBeInTheDocument();
+    expect(fireType).toBeInTheDocument();
+    expect(allType).toBeInTheDocument();
+
+    const buttons = getAllByRole('button');
+    const MAX_BUTTONS_NUM = 4;
+    // All, Electric, Fire and Proximo Pokémon
+    expect(buttons.length).toBe(MAX_BUTTONS_NUM);
   });
 });
