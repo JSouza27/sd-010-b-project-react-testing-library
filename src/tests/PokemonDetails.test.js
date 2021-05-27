763f9cc5f7ce442b0412966692235e58a0528719
legalvao1 <legalvao1@gmail.com>
2021-05-27 11:27:29 -0300

    Requisito 7 -Testando se tem o titulo 'nome pokemon' details

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
+
+import renderWithRouter from './renderWithRouter';
+
+import App from '../App';
+import pokemons from '../data';
+
+describe('test component PokemonDetails', () => {
+  test('if page contais title "pokemon name" details', () => {
+    renderWithRouter(<App />);
+
+    const moreDetailsButton = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetailsButton);
+
+    const { name } = pokemons[0];
+    const pageTitle = screen.getByRole('heading', { name: `${name} Details` });
+    expect(pageTitle).toBeInTheDocument();
+  });
+});
