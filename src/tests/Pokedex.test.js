a7a7b3d1079d054fd12fdb1d9abd11a0ef3ddf1d
CamilaCode7 <CamilaCode7>
2021-05-26 21:32:35 -0300

    adicionando requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Requirement 05', () => {
+  test('Testa se contém title', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const heading = getByRole('heading',
+      {
+        level: 2,
+        name: 'Encountered pokémons',
+      });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
