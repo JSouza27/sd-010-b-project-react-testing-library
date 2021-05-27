f52d8d4b6046e1a298c3636b43ce24534aa99311
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-05-27 17:22:37 -0300

    requisito 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
-// import { createMemoryHistory } from 'history';
 
 // Entendeimento da função MemoryRouter: https://reactrouter.com/web/api/MemoryRouter
 describe('Testando o componente <App.js />', () => {
@@ -26,5 +26,58 @@ describe('Testando o componente <App.js />', () => {
     expect(getByText('Home')).toBeInTheDocument();
     expect(getByText('About')).toBeInTheDocument();
     expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  it('Testando o redirecionamento ao clicar no link Home irá para `/`', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testando o redirecionamento ao clicar no link About irá para `/about`', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    fireEvent.click(getByText('About'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testando o redirecionamento do link Favorite Pokémons para `/favorites`', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    fireEvent.click(getByText('Favorite Pokémons'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testando o redirecionamento de uma URL desconhecida', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const route = '/pagina-desconhecida';
+    history.push(route);
+
+    expect(getByText(/not found/i)).toBeInTheDocument();
   });
 });
