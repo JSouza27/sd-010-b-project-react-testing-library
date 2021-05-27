aab2a43668eda7d496ed840f875e4c47df19b463
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-27 19:03:38 -0300

    test if shows or not the favorite pokémons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 
+import App from '../App';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
 
@@ -10,5 +12,29 @@ describe('Tests of component FavoritePokémons.', () => {
     const message = getByText('No favorite pokemon found');
 
     expect(message).toBeInTheDocument();
+  });
+
+  it('verify if shows all the favorite Pokémons', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+
+    userEvent.click(getByRole('link', { name: 'More details' }));
+    userEvent.click(getByRole('checkbox', { name: 'Pokémon favoritado?' }));
+    userEvent.click(getByRole('link', { name: 'Favorite Pokémons' }));
+
+    expect(getByText(/pikachu/i)).toBeInTheDocument();
+    expect(getByRole('img', {
+      name: /pikachu is marked as favorite/i,
+    })).toBeInTheDocument();
+  });
+
+  it('verify if shows no favorite Pokémons', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+
+    userEvent.click(getByRole('link', { name: 'Favorite Pokémons' }));
+    userEvent.click(getByRole('link', { name: /More details/i }));
+    userEvent.click(getByRole('checkbox', { name: /Pokémon favoritado/i }));
+    userEvent.click(getByRole('link', { name: /Favorite Pokémons/ }));
+
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
 });
