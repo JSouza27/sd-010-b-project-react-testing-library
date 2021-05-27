16e13230c0a88d19f6bfbd2719ced64e319e2dcf
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-27 14:41:00 -0300

    Cria o primeiro teste do sexto requisito e imports

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes do sexto requisito ', () => {
+  test('O nome correto do Pokémon deve ser mostrado na tela', () => {
+    renderWithRouter(<App />);
+    const namePokemon = screen.getByTestId('pokemon-name');
+    const { name } = pokemons[0];
+
+    expect(namePokemon).toHaveTextContent(name);
+  });
+});
