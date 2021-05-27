fe2a55173381be30c8b353a73193a9a553ff4111
Carlos <cemargato@gmail.com>
2021-05-27 17:23:37 -0300

    requisito-5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { fireEvent, getAllByText, getByAltText, getByText } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import Data from '../data';
 // import Pokedex from '../components/Pokedex';
-
+const btnPokemon = 'Próximo pokémon';
 describe('testes na Pokedex.js', () => {
   test('Testando a exibição da H2', () => {
     const { getByRole } = renderWithRouter(<App />);
@@ -16,7 +16,7 @@ describe('testes na Pokedex.js', () => {
   });
   test('Testando a lista de pokemon é exibida', () => {
     const { getByText } = renderWithRouter(<App />);
-    const nextButton = getByText('Próximo pokémon');
+    const nextButton = getByText(btnPokemon);
     expect(nextButton).toBeInTheDocument();
     const pikachu = getByText('Pikachu');
     fireEvent.click(nextButton);
@@ -38,43 +38,55 @@ describe('testes na Pokedex.js', () => {
     expect(testId).toHaveLength(1);
   });
   test('Testando o botão de filtro', () => {
-    const { getByText } = renderWithRouter(<App />)
+    const { getByText } = renderWithRouter(<App />);
     const bug = getByText('Bug');
     fireEvent.click(bug);
     const caterpie = getByText('Caterpie');
-    expect(caterpie).toBeInTheDocument() 
+    expect(caterpie).toBeInTheDocument();
   });
   test('Testando botão para resetar o filtro', () => {
-    const { getByText } = renderWithRouter(<App />)
-    const all = getByText('All')
-    fireEvent.click(all)
-    const pikachu = getByText('Pikachu')
+    const { getByText } = renderWithRouter(<App />);
+    const all = getByText('All');
+    fireEvent.click(all);
+    const pikachu = getByText('Pikachu');
     expect(pikachu).toBeInTheDocument();
-    const next = getByText('Próximo pokémon');
-    fireEvent.click(next)
-    const charmander = getByText('Charmander')
-    expect(charmander).toBeInTheDocument()
-  })
-  test('Teste se é criado, dinamicamente, um botão de filtro para cada tipo de Pokémon', () => {
-    const { container } = renderWithRouter(<App />)
-    const button = container.querySelectorAll('button');
-    const array = ['All', 'Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon']
-    button.forEach((btn, i) => {
-     if (btn.innerHTML === array[i]){
-    //  if (btn.innerHTML === 'All'){
-    //   return expect(btn.textContent).toBe(array[i]);
-    //  }
-     expect(btn.textContent).toBe(array[i]);
-     expect(btn).toHaveAttribute('data-testid');
-    }
-    })
-  })
-  test('Testando o botão de Próximo se está desabilitado quando a lista é filtrada', () => {
-    const { getByText } = renderWithRouter(<App />)
-    const bug = getByText('Bug');
-    fireEvent.click(bug);
-    const next = getByText('Próximo pokémon')
-    expect(next).toHaveAttribute('disabled')
-  })
-  //https://stackoverflow.com/questions/56593840/check-that-button-is-disabled-in-react-testing-library
+    const nextPokemon = getByText(btnPokemon);
+    fireEvent.click(nextPokemon);
+    const charmander = getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+  });
+  test('Teste se é criado, um botão de filtro para cada tipo de Pokémon', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const array = [
+      'All',
+      'Electric',
+      'Fire',
+      'Bug',
+      'Poison',
+      'Psychic',
+      'Normal',
+      'Dragon',
+    ];
+    array.forEach((item) => {
+      const button = getByRole('button', {
+        name: item,
+      });
+      if (item === 'All') {
+        return expect(button.textContent).toBe(item);
+      }
+      expect(button.textContent).toBe(item);
+      expect(button).toHaveAttribute('data-TestId', 'pokemon-type-button');
+    });
+  });
+  test(
+    'Testando o botão de Próximo se está desabilitado quando a lista é filtrada',
+    () => {
+      const { getByText } = renderWithRouter(<App />);
+      const bug = getByText('Bug');
+      fireEvent.click(bug);
+      const next = getByText(btnPokemon);
+      expect(next).toHaveAttribute('disabled');
+    },
+  );
+  // https://stackoverflow.com/questions/56593840/check-that-button-is-disabled-in-react-testing-library
 });
