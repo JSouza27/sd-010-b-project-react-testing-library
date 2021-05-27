030c5747c4fe2402434d29ac0d264b881cf10853
Pedro Toselli <pedrotrybe@gmail.com>
2021-05-27 11:47:08 -0300

    Requisito 4 - completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+
+describe('', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const notFoundMessage = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem correta', () => {
+    render(<NotFound />);
+
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const img = screen.getAllByRole('img');
+
+    expect(img[1]).toHaveAttribute('src', imgUrl);
+  });
+});
