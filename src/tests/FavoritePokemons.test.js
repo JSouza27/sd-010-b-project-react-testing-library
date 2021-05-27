b562b6e52b29b3390657d320b281f483d99eb168
brunobastosfer <brunobastos@edu.unifor.br>
2021-05-27 00:54:32 -0300

    Terceira e quarta questão resolvidas

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Testando o component Pokemon', () => {
+  const { container } = render(
+    <BrowserRouter>
+      <FavoritePokemons />
+    </BrowserRouter>,
+  );
+
+  const textPokemon = screen.getByRole('heading', {
+    level: 2,
+    name: /Favorite pokémons/i,
+  });
+  expect(textPokemon).toBeInTheDocument();
+
+  const noFavorite = {
+    case1: screen.getByText('No favorite pokemon found'),
+    case2: screen.getByText('Favorite pokémons'),
+  };
+  const getByClass = container.querySelector('[class="favorite-pokemons"]');
+  const getChildren = getByClass !== null ? getByClass.children : null;
+  if (getChildren) {
+    expect(noFavorite.case1).toBeInTheDocument();
+  } else expect(noFavorite.case2).toBeInTheDocument();
+});
