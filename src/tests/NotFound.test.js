9908887ee3e9f02a7e75a594b172a386d0114b0c
bidaffonso <bidaffondo@gmail.com>
2021-05-27 18:13:28 -0400

    Soluçao Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../Helpers/renderWithRouter';
+import { NotFound } from '../components';
+
+test('shows a h2 element with text "Page requested not found 😭"', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const notFoundText = getByText('Page requested not found');
+  const notFoundImg = getByText('😭');
+  expect(notFoundText).toBeInTheDocument();
+  expect(notFoundImg).toBeInTheDocument();
+});
+
+test('renders pikachu gif', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const gif = getByAltText('Pikachu crying because the page requested was not found');
+  expect(gif).toHaveAttribute(
+    'src',
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  );
+});
