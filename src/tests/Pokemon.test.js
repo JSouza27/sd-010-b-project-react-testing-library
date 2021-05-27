09dc95392eee10dc16a29819de7c238ef7b4feb3
Cristian <cristian.bugs@gmail.com>
2021-05-26 21:52:48 -0300

    sixth and seventh task partially complete

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,34 +1,85 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Pokemon from '../components/Pokemon';
 import renderWithRouter from './RenderWithRouter';
+import Data from '../data';
 
 describe('check if the application page renders a pokemon card', () => {
   test('The correct name must be displayed', () => {
-    renderWithRouter(<Pokemon />);
+    renderWithRouter(<Pokemon
+      pokemon={ Data[0] }
+      isFavorite
+    />);
 
-    const pokeName = screen.getAllByTestId('pokemon-name');
+    const pokeName = screen.getByTestId('pokemon-name');
     expect(pokeName).toBeInTheDocument();
   });
 
   test('The correct type must be displayed', () => {
-    renderWithRouter(<Pokemon />);
+    renderWithRouter(<Pokemon
+      pokemon={ Data[0] }
+      isFavorite
+    />);
 
-    const pokeType = screen.getAllByTestId('pokemon-type');
+    const pokeType = screen.getByTestId('pokemon-type');
     expect(pokeType).toBeInTheDocument();
   });
 
   test('The correct average weight must be displayed', () => {
-    renderWithRouter(<Pokemon />);
+    renderWithRouter(<Pokemon
+      pokemon={ Data[0] }
+      isFavorite
+    />);
 
-    const pokeWeight = screen.getAllByTestId('pokemon-type');
+    const pokeWeight = screen.getByTestId('pokemon-weight');
     expect(pokeWeight).toBeInTheDocument();
   });
 
   test('An image must be displayed', () => {
-    renderWithRouter(<Pokemon />);
-
+    renderWithRouter(<Pokemon
+      pokemon={ Data[0] }
+      isFavorite
+    />);
     const pokeImage = screen.getByAltText(/sprite/i);
     expect(pokeImage).toBeInTheDocument();
   });
+});
+
+test('Check if there is a link to the display detais', () => {
+  const { history } = renderWithRouter(<Pokemon
+    pokemon={ Data[1] }
+    isFavorite
+  />);
+  const link = screen.getByRole('link', {
+    name: /More Details/i,
+  });
+  userEvent.click(link);
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/pokemons/4');
+});
+
+// test('Check if by clicking the user is redirect', () => {
+//   const { history } = renderWithRouter(<Pokemon
+//     pokemon={ Data[0] }
+//     isFavorite
+//   />);
+//   const link = screen.getByRole('link', {
+//     name: /More Details/i,
+//   });
+//   userEvent.click(link);
+//   const { pathname } = history.location;
+
+//   expect(pathname).toBe(`/pokemons/${data1}`);
+// });
+
+test('The icon must have an image', () => {
+  renderWithRouter(<Pokemon
+    pokemon={ Data[0] }
+    isFavorite
+  />);
+
+  const pokeImage = screen.getByAltText(/is marked as favorite/i);
+  expect(pokeImage).toHaveAttribute('src', '/star-icon.svg');
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import PokemonDetails from '../components/PokemonDetails';
+
+describe('Check if the pokemon info are displayed', () => {
+  test('The page must have a text called Details', () => {
+    renderWithRouter(<PokemonDetails isFavoritebyId />);
+
+    const pokeName = screen.getByText(/details/i);
+    expect(pokeName).toBeInTheDocument();
+  });
+
+  test('The page must not have a link do Pokemon details', () => {
+    renderWithRouter(<PokemonDetails
+      isFavoritebyId
+    />);
+
+    const pokeDetails = screen.getByRole('link', {
+      name: /details/i,
+    });
+    expect(pokeDetails).not.toBeInTheDocument();
+  });
+
+  test('The page must have a h2 with the text Summary', () => {
+    renderWithRouter(<PokemonDetails isFavoritebyId />);
+
+    const pokeHeading2 = screen.getByRole('heading', {
+      level: 2,
+      name: /Summary/i,
+    });
+    expect(pokeHeading2).toBeInTheDocument();
+  });
+
+  test('The page must have a paragraph with a ', () => {
+    const { container } = renderWithRouter(<PokemonDetails isFavoritebyId />);
+    const paragraph = container.querySelector('p');
+
+    expect(paragraph.length).toBeInTheDocument();
+  });
+});
