d2f2b69673bc1693634269def5841ded5edadbb4
MarceloPelegrino <pelegrinocorrea@gmail>
2021-05-27 15:57:35 -0300

    Requirement 4 finalized

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import NotFound from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('tests the <FavoritePokemons /> component', () => {
+  test('if page contains an h2 heading with the text Page requested not found', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    // Porque al renderizar direto o componete <NotFound /> não funciona?
+    history.push('pagina-que-nao-existe');
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('whether page shows the specific image', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('pagina-que-nao-existe');
+    const image = document.querySelector('img');
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
