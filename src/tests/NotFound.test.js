f509175a66de13963c355f9a39e04d7bfab8605c
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-27 20:33:54 -0300

    test h2 with emoji

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Tests the NotFound component', () => {
+  it('verify heading h2 with specific text', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const heading = getByRole('heading', { level: 2 });
+    const emoji = getByRole('img', { name: 'Crying emoji' });
+
+    expect(heading).toBeInTheDocument();
+    expect(heading.innerHTML).toContain('Page requested not found');
+    expect(emoji).toBeInTheDocument();
+    expect(emoji.innerHTML).toBe(' 😭');
+  });
+});
