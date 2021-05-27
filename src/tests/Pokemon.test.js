5663f184e57c5930413b379695ca9ba1afba2082
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-05-27 16:35:07 -0300

    Foi feito o teste do pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -13,17 +13,40 @@ test('Teste se é renderizado um card com as informações de determinado pokém
   });
   pokemons.forEach(({ name, type, image, averageWeight: { value, measurementUnit } }) => {
     const pokemonName = screen.getByText(name);
-    const pokemonType = screen.getByText(type);
+    const pokemonType = screen.getAllByText(type);
     const pokemonWeight = screen.getByText(`Average weight: ${value} ${measurementUnit}`);
     const pokemonImg = screen.getByRole('img', {
-      name: `${pokemonName} sprite`,
+      name: `${name} sprite`,
     });
 
     expect(pokemonName).toBeInTheDocument();
-    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonType).toHaveLength(2);
     expect(pokemonWeight).toBeInTheDocument();
     expect(pokemonImg).toHaveAttribute('alt', `${name} sprite`);
     expect(pokemonImg).toHaveAttribute('src', image);
     userEvent.click(nextPokemon);
   });
+});
+
+test('Testa o card do Pokémon se tem link de navegação e o redirecionamento'
++ 'e testa se existe um ícone de estrela nos Pokémons favoritados. ', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkDetails = screen.getByRole('link', {
+    name: /more details/i,
+  });
+  const { id, name } = pokemons[0];
+  expect(linkDetails).toHaveAttribute('href', `/pokemons/${id}`);
+
+  userEvent.click(linkDetails);
+
+  const { pathname } = history.location;
+  const checkboxFavorite = screen.getByRole('checkbox', {
+    name: 'Pokémon favoritado?' });
+  userEvent.click(checkboxFavorite);
+  const imageStar = screen.getByRole('img', {
+    name: `${name} is marked as favorite`,
+  });
+
+  expect(pathname).toBe(`/pokemons/${id}`);
+  expect(imageStar).toHaveAttribute('src', '/star-icon.svg');
 });
