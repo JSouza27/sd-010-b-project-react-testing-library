63948d460cdd05e787414dcf37ce43d155c11799
Lara Capila <laracapilak@gmail.com>
2021-05-27 15:50:27 -0300

    requirement 3 - if "No favorite pokemon found", when you have no favorite pokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+test('Test if "No favorite pokemon found", when you have no favorite pokemon', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+  const favoriteText = getByText(/No favorite pokemon found/i);
+
+  expect(favoriteText).toBeInTheDocument();
+});
