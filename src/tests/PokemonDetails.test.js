18e071600d1a1dba5a7c8052409f01d42a837354
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-05-27 17:30:27 -0300

    PokemonDetails.test iniciado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RenderWithRouter from './RenderWithRouter';
+
+import App from '../App';
+
+describe('Testes do Componente <PkemonDetails/>', () => {
+  test('Teste as informações detalhadas do Pokémon selecionado.', () => {
+    RenderWithRouter(<App />);
+
+    const linkMoreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(linkMoreDetails);
+
+    const text = screen.getByRole('heading', {
+      name: /pikachu details/i,
+    });
+    expect(text).toBeInTheDocument();
+    expect(linkMoreDetails).not.toBeInTheDocument();
+  });
+});
