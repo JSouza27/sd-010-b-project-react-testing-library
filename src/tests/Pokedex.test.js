85eda0c342478265a493d8bc03e6e982327533a7
Matheus H. Gois <matheus.hengo@gmail.com>
2021-05-27 19:02:08 -0300

    Test h2 text

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testing Pokedex.js', () => {
+  it('verify if have a h2 text with "Encountered pokémons"', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const encounteredPokemons = getByRole('heading', {
+      level: 2,
+      name: /encountered pok[eé]mons/i,
+    });
+
+    expect(encounteredPokemons).toBeInTheDocument();
+  });
+});
