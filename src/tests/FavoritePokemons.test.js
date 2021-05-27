29126ebbfdeaf341a92d2920e6ebcc98bf3e4b4a
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-26 22:57:10 -0300

    Req 3: test if show the message for no favorite Pokémons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Tests of component FavoritePokémons.', () => {
+  test('shows the message for no favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const message = getByText('No favorite pokemon found');
+
+    expect(message).toBeInTheDocument();
+  });
+});
