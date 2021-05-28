45cda4584aeae1bc4e1da6eeb02e9a7acde2aed0
Lucas Ribeiro <lucasrb04@gmail.com>
2021-05-28 00:59:29 -0300

    adicionando requisito 4 e parte do 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('renders a reading with the text `No favorite pokemon found`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+  const noPokemon = getByText(/No favorite pokemon found/i);
+  expect(noPokemon).toBeInTheDocument();
+});
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(getByText('About')).toBeInTheDocument();
+  expect(getByText('Home')).toBeInTheDocument();
+  expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('renders a reading with the text `About Pokédex`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const heading = getByText(/Page requested not found/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders a reading with the Pokédex image', () => {
+  render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  expect(screen.getByAltText('Pikachu crying because the page requested was not found'))
+    .toHaveAttribute('src', expect.stringContaining('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'));
+});
