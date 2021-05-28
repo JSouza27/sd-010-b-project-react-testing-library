5a57fc3f297166ac3f0ca900d8a155ba26e6fcec
André Arnoni <andre_arnoni@yahoo.com.br>
2021-05-27 22:14:22 -0300

    Executado requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,20 +1,30 @@
-// import React from 'react';
-// import renderWithRouter from '../components/renderWithRouter';
-// import FavoritePokemons from '../components/FavoritePokemons';
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
 
-// describe('Testing the FavoritePokemons.js', () => {
-//   test('if show the message No favorite pokemon found if no favorited pokemon', () => {
-//     const { getByText } = renderWithRouter(<FavoritePokemons />);
-//       const not = getByText(/no favorite pokemon found/i);
-//       expect(not).toBeInTheDocument();
-//   });
+describe('Testing the FavoritePokemons.js', () => {
+  test('if show the message No favorite pokemon found if no favorited pokemon', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const favoriteLink = getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    fireEvent.click(favoriteLink);
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
 
-//   test('if all the pokemon cards are renderized if favorited', () => {
-//     const { getByRole } = renderWithRouter(<FavoritePokemons />);
-
-//   });
-
-//   test('if anyone card is renderized if not favorited', () => {
-//     const { getByRole } = renderWithRouter(<FavoritePokemons />);
-//   });
-// });
+  test('if shows the favorited pokemon cards', () => {
+    const { getByRole, getByAltText, getByText } = renderWithRouter(<App />);
+    const detailsLink = getByRole('link', {
+      name: 'More details',
+    });
+    fireEvent.click(detailsLink);
+    const check = getByRole('checkbox');
+    fireEvent.click(check);
+    const favoriteLink = getByText('Favorite Pokémons');
+    fireEvent.click(favoriteLink);
+    const card = getByAltText(/pikachu is marked as favorite/i);
+    expect(card).toBeInTheDocument();
+  });
+});
