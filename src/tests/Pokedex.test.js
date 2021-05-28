1023a48cc8e4da51061b7e923b0c2b8cc6062866
Roger Franco <rogerfrancogalt@gmail.com>
2021-05-27 23:01:34 -0400

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import { screen } from '@testing-library/dom';
+import { fireEvent, screen } from '@testing-library/dom';
 import renderWithRouter from './renderWithRouter';
 import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
 
 it('Test if the page contains an h2 heading with the text Encountered Pokémon', () => {
-  const { getByText } = renderWithRouter(<Pokedex />);
-  const pokedex = getByText(/Encountered pokémons/);
-  expect(pokedex).toBeInTheDocument();
+  renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
   const pokedexText = screen.getByRole('heading', {
-    level: 2,
     name: /Encountered pokémons/i,
   });
   expect(pokedexText).toBeInTheDocument();
 });
 
 it('Next Pokémon is displayed when the Next Pokémon button is clicked.', () => {
-  // renderWithRouter(<Pokedex />);
-  RenderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
   const pikachu = screen.getByText(/Pikachu/);
   expect(pikachu).toBeInTheDocument();
   const nextPokemon = screen.getByRole('button', { name: /Próximo pokémon/ });
@@ -24,31 +21,37 @@ it('Next Pokémon is displayed when the Next Pokémon button is clicked.', () =>
   const charmander = screen.getByText(/Charmander/);
   expect(charmander).toBeInTheDocument();
 });
-test('only one Pokémon is shown at a time', () => {
-  RenderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
-  const poke = screen.getAllByTestId('pokemon-name');
-  expect(poke).toHaveLength(1);
-  expect(poke[0]).toBeInTheDocument();
-});
-test('Pokédex has the filter buttons.', () => {
-  // renderWithRouter(<Pokedex />);
-  RenderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
-  const btns = screen.getAllByTestId('pokemon-type-button');
-  const types = 7;
-  expect(btns.length).toBe(types);
 
-  const eletric = screen.getByRole('button', { name: /Electric/ });
-  const fire = screen.getByRole('button', { name: /Fire/ });
-  const bug = screen.getByRole('button', { name: /Bug/ });
-  const poison = screen.getByRole('button', { name: /Poison/ });
-  const psy = screen.getByRole('button', { name: /Psychic/ });
-  const normal = screen.getByRole('button', { name: /Normal/ });
-  expect(eletric).toBeEnabled();
-  expect(fire).toBeEnabled();
-  expect(bug).toBeEnabled();
-  expect(poison).toBeEnabled();
-  expect(psy).toBeEnabled();
-  expect(normal).toBeEnabled();
+test('only one Pokémon is shown at a time', () => {
+  renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  const pokemon = screen.getAllByTestId('pokemon-name');
+  expect(pokemon).toHaveLength(1);
+  expect(pokemon[0]).toBeInTheDocument();
 });
-// test('', () => { });
-// test('', () => { });
+
+test('If has the filter buttons', () => {
+  renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  const buttons = screen.getAllByTestId('pokemon-type-button');
+  const types = 7;
+  expect(buttons.length).toBe(types);
+});
+
+test('If contains a button to reset the filter', () => {
+  renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  const electric = screen.getByRole('button', { name: /Electric/ });
+  fireEvent.click(electric);
+  const nextPokemon = screen.getByRole('button', { name: /Próximo pokémon/ });
+  expect(nextPokemon).toBeDisabled();
+
+  const allButton = screen.getByRole('button', { name: /All/ });
+  fireEvent.click(allButton);
+  expect(nextPokemon).toBeEnabled();
+});
+
+test('Next Pokémon button should be disabled when has only one Poké in the list.', () => {
+  renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+  const electric = screen.getByRole('button', { name: /Electric/ });
+  fireEvent.click(electric);
+  const nextPokemon = screen.getByRole('button', { name: /Próximo pokémon/ });
+  expect(nextPokemon).toBeDisabled();
+});
