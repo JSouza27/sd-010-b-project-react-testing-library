f9329e39507e5aec1e9747ff595ea2627bf9bc65
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-05-28 13:28:03 -0300

    req1 refactoring

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,45 +1,31 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('Exibe a Pokedex quando a rota é `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-
+  const { getByText } = renderWithRouter(<App />);
   const initialPage = getByText(/Pokédex/i);
+
   expect(initialPage).toBeInTheDocument();
 });
 
 test('Topo da aplicação contém link com o texto `Home`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const homeLink = getByText(/Home/i);
+
   expect(homeLink).toBeInTheDocument();
 });
 
 test('Topo da aplicação contém link com o texto `About`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const aboutLink = getByText(/About/i);
+
   expect(aboutLink).toBeInTheDocument();
 });
 
 test('Topo da aplicação contém link com o texto `Favorite Pokémons`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const favoritePokemonsLink = getByText(/Favorite Pokémons/i);
+
   expect(favoritePokemonsLink).toBeInTheDocument();
 });
