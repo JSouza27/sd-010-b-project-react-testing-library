289cbf7cadd89c5b89345415aa913385e0aa12b1
root <diegoserafim1@gmail.com>
2021-05-27 21:49:01 -0300

    criado o teste para verificar se na pagina PokemonDetais contem o paragrafo com o texto inicio 'This intelligent Pokémon'

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -6,33 +6,38 @@ import renderWithRouter from '../components/renderWithRouter';
 import pokemons from '../data';
 
 describe('Testa o componente <PokemonDetails.js />', () => {
-  test('Testa se as informações do Pokémon selecionado são mostradas.', () => {});
-  const { getByRole, getByText } = renderWithRouter(<App />);
-  const details = /More details/i;
-  const { name } = pokemons[0];
-  fireEvent.click(getByText(details));
-  const heading = getByRole('heading', {
-    level: 2,
-    name: `${name} Details`,
+  test('Testa se as informações do Pokémon selecionado são mostradas.', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const details = /More details/i;
+    const { name } = pokemons[0];
+    fireEvent.click(getByText(details));
+    const heading = getByRole('heading', {
+      level: 2,
+      name: `${name} Details`,
+    });
+    expect(heading).toBeInTheDocument();
+    expect(getByRole('heading', {
+      level: 2,
+      name: /Summary/i,
+    })).toBeInTheDocument();
+    expect(getByRole('heading', {
+      level: 2,
+      name: `Game Locations of ${name}`,
+    })).toBeInTheDocument();
   });
-  expect(heading).toBeInTheDocument();
-  expect(getByRole('heading', {
-    level: 2,
-    name: /Summary/i,
-  })).toBeInTheDocument();
-  expect(getByRole('heading', {
-    level: 2,
-    name: `Game Locations of ${name}`,
-  })).toBeInTheDocument();
 
   test('Testa se não há um link de navegação para os detalhes do Pokémon.', () => {
-    const { queryByText } = renderWithRouter(<App />);
-    // const details = /More details/i;
+    const { queryByText } = renderWithRouter(<App pokemons={ pokemons } />);
+    const details = /More details/i;
     fireEvent.click(queryByText(details));
     expect(queryByText(details)).toBeNull();
   });
 
-  test('Testa se há uma seção com os mapas contendo as localizações do pokémon', () => {
-
+  test('A seção de detalhes deve conter um parágrafo com o resumo do Pokémon.', () => {
+    const { queryByText } = renderWithRouter(<App />);
+    const details = /More details/i;
+    fireEvent.click(queryByText(details));
+    const paragraph = queryByText((params, tag) => tag.tagName === 'P' && params.includes('This intelligent Pokémon'));
+    expect(paragraph).toBeInTheDocument();
   });
 });
