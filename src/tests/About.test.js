b94a2e80d5a980100986651502fc7baf2d374d52
Lucas Ribeiro <lucasrb04@gmail.com>
2021-05-27 23:42:44 -0300

    adicionando requisito 2 e matando todos os mutnates

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+test('renders a reading with the text `About Pokédex`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const heading = getByText(/About Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders a reading with the Pokédex image', () => {
+  render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  expect(screen.getByAltText('Pokédex'))
+    .toHaveAttribute('src', expect.stringContaining('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'));
+});
