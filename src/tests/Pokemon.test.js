e8f165f8e447e92f57faec692febeed68287eedc
isTorres <ismaeltorres612@gmail.com>
2021-05-28 00:04:14 -0300

    fix tests for pokemon component - add expt to img's

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -12,6 +12,19 @@ const pokemons = [
       value: '6.0',
       measurementUnit: 'kg',
     },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity.',
   },
   {
     id: 4,
@@ -21,15 +34,27 @@ const pokemons = [
       value: '8.5',
       measurementUnit: 'kg',
     },
-  },
-  {
-    id: 10,
-    name: 'Caterpie',
-    type: 'Bug',
-    averageWeight: {
-      value: '2.9',
-      measurementUnit: 'kg',
-    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force.',
   },
 ];
 
@@ -70,13 +95,18 @@ describe('Tests for pokemon component', () => {
     const { location: { pathname } } = history;
     expect(pathname).toBe(`/pokemons/${pokemons[0].id}`);
 
+    const imgPokemon = getByAltText('Pikachu sprite');
     const btnFav = getByRole('checkbox', { name: /Pokémon favoritado/i });
+    expect(imgPokemon).toBeInTheDocument();
+    expect(imgPokemon.src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
     expect(btnFav).toBeInTheDocument();
+    expect(btnFav.checked).toBe(false);
 
     fireEvent.click(btnFav);
     expect(btnFav.checked).toBe(true);
 
-    const star = getByAltText(`${pokemons[0].name} is marked as favorite`);
+    const star = getByRole('img', { name: 'Pikachu is marked as favorite' });
     expect(star).toBeInTheDocument();
+    expect(star.src).toContain('/star-icon.svg');
   });
 });
