159c09e37d34899acceed9e3583cbf16ff0760c3
Flavio Andrade <foa12381@gmail.com>
2021-05-28 16:23:24 -0300

    requesito3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('testa componente favoritepokemon', () => {
+  it('msg exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<App />);
+    const favoritepokemon = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    userEvent.click(favoritepokemon);
+    const msg = /No favorite pokemon found/i;
+
+    expect(screen.getByText(msg)).toBeInTheDocument();
+  });
+});
