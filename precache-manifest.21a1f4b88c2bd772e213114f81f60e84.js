084e1f3111968a51fc79301d2737b16d2978140c
root <diegoserafim1@gmail.com>
2021-05-28 15:47:47 -0300

    Updates

---
+++ precache-manifest.21a1f4b88c2bd772e213114f81f60e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05ebf55f4d0c48d19b2e62c30df67ea",
+    "url": "//app-toot-pokedex/index.html"
+  },
+  {
+    "revision": "2de62a3947d6f2609c2c",
+    "url": "//app-toot-pokedex/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "1c83f9ae1223d71fbc81",
+    "url": "//app-toot-pokedex/static/js/2.9cbb527b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "//app-toot-pokedex/static/js/2.9cbb527b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de62a3947d6f2609c2c",
+    "url": "//app-toot-pokedex/static/js/main.5754a4fc.chunk.js"
+  },
+  {
+    "revision": "818ab22fc9de113c507a",
+    "url": "//app-toot-pokedex/static/js/runtime-main.616ab8f1.js"
+  }
+]);
