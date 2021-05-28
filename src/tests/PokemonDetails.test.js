5ca989988ff3a569da83ac31d8c7b759de9ad8b5
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-27 22:24:39 -0300

    Teste se as informações detalhadas do pokémon são exibidas na tela

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,59 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import PokemonDetails from '../components/PokemonDetails';
+import renderWithRouter from './RenderWithRouter';
+import pokemons from '../data';
+import { updateFavoritePokemons } from '../services/pokedexService';
+
+const [
+  pikachu, charmander, caterpie, ekans, alakazam, mew, rapidash, snorlax, dragonair,
+] = pokemons;
+
+const isFavoritePokemons = {
+  [pikachu.id]: false,
+  [charmander.id]: false,
+  [caterpie.id]: false,
+  [ekans.id]: false,
+  [alakazam.id]: false,
+  [mew.id]: false,
+  [rapidash.id]: false,
+  [snorlax.id]: false,
+  [dragonair.id]: false,
+};
+
+const { id, name } = ekans;
+const parsedId = id.toString();
+
+describe(`Teste se as informações detalhadas do Pokémon
+selecionado são mostradas na tela`, () => {
+  it(`A página deve conter um texto <name> Details,
+onde <name> é o nome do Pokémon`, () => {
+    const { getByRole } = renderWithRouter(
+      <PokemonDetails
+        isPokemonFavoriteById={ isFavoritePokemons }
+        match={ { params: { id: parsedId } } }
+        pokemons={ pokemons }
+        onUpdateFavoritePokemons={ (pokemonId, isFavorite) => (
+          updateFavoritePokemons(pokemonId, isFavorite)
+        ) }
+      />,
+    );
+    const heading2 = getByRole('heading', {
+      level: 2,
+      name: `${name} Details`,
+    });
+    expect(heading2).toBeInTheDocument();
+  });
+});
+
+/*
+<PokemonDetails
+  isPokemonFavoriteById={ isPokemonFavoriteById }
+  match={ { match: { params: { id } } } }
+  pokemons={ pokemons }
+  onUpdateFavoritePokemons={ (pokemonId, isFavorite) => (
+    updateFavoritePokemons(pokemonId, isFavorite)
+  ) }
+/>,
+
+*/
