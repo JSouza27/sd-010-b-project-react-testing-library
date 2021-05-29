32df73a186fed588373f17fc3346df3343cb957a
Pedro Amaral <amaralph1995@gmail.com>
2021-05-29 14:55:50 -0300

    Testa se imagem tem link correto (about)

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('show if about page display pokedex info', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const pokedexInfo = getByText(/About Pokédex/i);
+
+  expect(pokedexInfo).toBeInTheDocument();
+});
+test('show if page display a specific image', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const pokedexImage = getByRole('img');
+
+  expect(pokedexImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
