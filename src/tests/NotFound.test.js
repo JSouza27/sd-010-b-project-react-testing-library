df2f1006c8471afd1831ac2ea22cecb0bfcbba93
Pedro Amaral <amaralph1995@gmail.com>
2021-05-29 16:00:29 -0300

    Testa imagem e heading de NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('show if notFound page shows text', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const heading = getByRole('heading');
+
+  expect(heading).toHaveTextContent(/Page requested not found/i);
+});
+test('show if notFound image exists', () => {
+  const { getByAltText } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const image = getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
