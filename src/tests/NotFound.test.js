39cbfe34eee77e8de2019bbaa26b2886ea9056da
Flavio Andrade <foa12381@gmail.com>
2021-05-29 12:40:20 -0300

    requesito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -5,18 +5,17 @@ import renderWithRouter from './renderWithRouter';
 describe('4. Teste o componente `<NotFound.js />', () => {
   it('Teste pági contém heading h2 texto `Page requested not found 😭`;', () => {
     const { getByText, getByRole } = renderWithRouter(<NotFound />);
-    const imgi = getByRole('img', { name: 'Crying emoji' });
-
-    expect(getByText(/Page request not found/i)).toBeInTheDocument();
-    expect(imgi).toBeInTheDocument();
+    const emoji = getByRole('img', { name: 'Crying emoji' });
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+    expect(emoji).toBeInTheDocument();
   });
 
   it('Teste se página mostra a imagem', () => {
     const { getByRole } = renderWithRouter(<NotFound />);
-    const imgi = getByRole(img, {
+    const imagem = getByRole('img', {
       name: 'Pikachu crying because the page requested was not found',
     });
     const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
-    expect(imgi.src).to.equal(src);
+    expect(imagem.src).toBe(src);
   });
 });
