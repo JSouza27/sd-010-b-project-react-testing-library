d5d89535c4a09dc0f45d27bcbce7afab7011b82c
isTorres <ismaeltorres612@gmail.com>
2021-05-29 02:22:12 -0300

    add tests for pokemon details component

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
 
@@ -23,7 +24,7 @@ const pokemons = [
         map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
       },
     ],
-    summary: 'This intelligent Pokémon roasts hard berries with electricity.',
+    summary: 'This intelligent Pokémon roasts hard berries with electricity',
   },
   {
     id: 4,
@@ -61,14 +62,55 @@ global.fetch(jest.fn().mockResolvedValue({
   json: jest.fn().mockResolvedValue(pokemons),
 }));
 
+const summary1 = 'This intelligent Pokémon roasts hard berries with';
+const summary2 = 'electricity to make them tender enough to eat.';
+
 describe('Name of the group', () => {
   it('test if the info of the pokemon selected are rendered', () => {
-    const { getByRole, getByText, history } = renderWithRouter(<App />);
+    const { getByRole, getByText, getByTestId, history } = renderWithRouter(<App />);
     history.push(`/pokemons/${pokemons[0].id}`);
+
     expect(getByText(`${pokemons[0].name} Details`));
 
-    expect(getByRole('heading', { level: 2, name: 'Summary' })).toBeInTheDocument();
+    const summary = getByRole('heading', { level: 2, name: 'Summary' });
+    expect(summary).toBeInTheDocument();
+    expect(summary.innerHTML).toBe('Summary');
 
-    expect(getByText('This intelligent Pokémon')).toBeInTheDocument();
+    expect(getByTestId('pokemon-dale')).toBeInTheDocument();
+    expect(getByTestId('pokemon-dale').innerHTML).toBe(`${summary1} ${summary2}`);
+  });
+
+  it('test if the page have a section for maps of the locations of pokemon', () => {
+    const { getByRole, getByText, getAllByAltText, history } = renderWithRouter(<App />);
+    const pikachu = pokemons[0];
+    history.push(`/pokemons/${pikachu.id}`);
+
+    expect(getByRole('heading', {
+      level: 2,
+      name: `Game Locations of ${pikachu.name}`,
+    }));
+
+    expect(getByText(pikachu.foundAt[0].location)).toBeInTheDocument();
+    expect(getByText(pikachu.foundAt[1].location)).toBeInTheDocument();
+    const pikachuLocation = getAllByAltText('Pikachu location');
+
+    expect(pikachuLocation[0].src).toContain('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(pikachuLocation[1].src).toContain('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
+
+  it('test if this pokemon can be favorited', () => {
+    const { getByRole, getByLabelText, history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${pokemons[0].id}`);
+
+    const checkFav = getByRole('checkbox');
+    expect(checkFav).toBeInTheDocument();
+
+    fireEvent.click(checkFav);
+    expect(checkFav.checked).toBe(true);
+
+    fireEvent.click(checkFav);
+    expect(checkFav.checked).toBe(false);
+
+    expect(getByLabelText(/Pokémon Favoritado?/i)).toBeInTheDocument();
   });
 });
