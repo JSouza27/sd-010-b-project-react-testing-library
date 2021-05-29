560429d4b434bba1c89141f32f6ac9d6ed945267
Aderson Santos <assaderson@gmail.com>
2021-05-29 20:00:33 -0300

    Teste se um conjunto fixo de links de navegação conseguem ser clicados.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
@@ -49,7 +50,7 @@ describe('Requisito 1', () => {
     expect(linkEmA1).toBeInTheDocument();
   });
 
-  test('Verifica se o primeiro texto possui o texto home', () => {
+  test('Verifica se o segundo texto possui o texto about', () => {
     render(
       <MemoryRouter initialEntries={ ['/'] }>
         <App />
@@ -61,7 +62,7 @@ describe('Requisito 1', () => {
     expect(linkEmA2).toBeInTheDocument();
   });
 
-  test('Verifica se o primeiro texto possui o texto home', () => {
+  test('Verifica se o terceiro texto possui o texto favorite pokemons', () => {
     render(
       <MemoryRouter initialEntries={ ['/'] }>
         <App />
@@ -71,5 +72,59 @@ describe('Requisito 1', () => {
       name: 'Favorite Pokémons',
     });
     expect(linkEmA3).toBeInTheDocument();
+  });
+
+  test('Verifica se a aplicação é redirecionada para a página de home', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkHome = screen.getByRole('link', {
+      name: 'Home',
+    });
+    userEvent.click(linkHome);
+
+    const textoH1Home = screen.getByRole('heading', {
+      level: 1,
+      name: 'Pokédex',
+    });
+    expect(textoH1Home).toBeInTheDocument();
+  });
+
+  test('Verifica se a aplicação é redirecionada para a página de about', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkAbout = screen.getByRole('link', {
+      name: 'About',
+    });
+    userEvent.click(linkAbout);
+
+    const textoH1About = screen.getByRole('heading', {
+      level: 1,
+      name: 'Pokédex',
+    });
+    expect(textoH1About).toBeInTheDocument();
+  });
+  
+  test('Verifica se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkFavorite = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    userEvent.click(linkFavorite);
+
+    const textoH1About = screen.getByRole('heading', {
+      level: 1,
+      name: 'Pokédex',
+    });
+    expect(textoH1About).toBeInTheDocument();
   });
 });
