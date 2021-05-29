ed7f560fd885504b25860a7e22e571403aa2ddac
Luiz Vicentin <luizpavanello@gmail.com>
2021-05-29 13:57:03 -0300

    Teste o componente <FavoritePokemon.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('É exibido na tela `No favorite pokemon found`, caso não tenha favoritos.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const errorMessage = 'No favorite pokemon found';
+    expect(getByText(errorMessage)).toBeInTheDocument();
+  });
+});
