ccc915923916ce8b8c12d79a859b4817edba273a
Pedro Amaral <amaralph1995@gmail.com>
2021-05-29 15:48:46 -0300

    Testa pagina favoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('show if messege appears when no pokemon is favorite', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+  spyOn(localStorage, 'setItem');
+  localStorage.setItem('favoritePokemons', []);
+
+  const nonePokemon = getByText('No favorite pokemon found');
+
+  expect(nonePokemon).toBeInTheDocument();
+});
