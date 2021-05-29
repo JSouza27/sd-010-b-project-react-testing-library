a875b94596092fba60f2a80a4de8d442bcc53e9e
aline <aline.melryaa@gmail.com>
2021-05-29 10:49:13 -0400

    NotFound mutation test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../RenderWithRouter';
+import { NotFound } from '../components';
+
+test('Testa se tem h2 com Page requested not found ', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /Page requested not found Crying emoji/i,
+  });
+  expect(heading).toBeInTheDocument();
+});
+
+test('Testa se pagina mostra ', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const img = getAllByRole('img');
+  expect(img[1].src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
