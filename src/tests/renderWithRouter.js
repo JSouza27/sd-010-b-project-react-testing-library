23dc8a93020e4cce4b58001ea63c1abb54739fbf
LucasPeixoto <peixotolucas516@gmail.com>
2021-05-30 18:31:59 -0300

    renderWithRouter refactored

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Router } from  'react-router-dom';
+import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
 
-		return {
-			...render(
-				<Router history={ history }>
-					{component}
-				</Router>,
-			),
-			history,
-    };
+	return {
+		...render(
+			<Router history={ history }>
+				{component}
+			</Router>,
+		),
+		history,
+  };
 };
 
 export default renderWithRouter;
