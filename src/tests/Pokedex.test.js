6b280e9914f5db287e543d480e5745758c314cbe
Mariana Savoldi <marianasavodi123@gmail.com>
2021-05-30 03:27:25 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 // import Pokedex from '../components/Pokedex';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-// import pokemons from '../data';
+import pokemons from '../data';
 import App from '../App';
+
+const prxpkm = 'Próximo pokémon';
 
 describe('Requirement 5', () => {
   test('There is a <h2> with text `Encountered pokémons`', () => {
@@ -17,7 +19,7 @@ describe('Requirement 5', () => {
 
   test('Show up the next pokémon when click the button', () => {
     const { getByText } = renderWithRouter(<App />);
-    const button = getByText('Próximo pokémon');
+    const button = getByText(prxpkm);
     expect(button).toBeInTheDocument();
 
     userEvent.click(button);
@@ -27,12 +29,38 @@ describe('Requirement 5', () => {
 
   test('Shows the first pokémon on the list, if it`s on the last', () => {
     const { getByText } = renderWithRouter(<App />);
-    const button = getByText('Próximo pokémon');
+    const button = getByText(prxpkm);
     const clicked = 9;
     for (let clicks = 0; clicks < clicked; clicks += 1) {
       userEvent.click(button);
     }
     const first = getByText('Pikachu');
     expect(first).toBeInTheDocument();
+  });
+
+  test('There is filter buttons at Pokédex', () => {
+    const { getByTestId, getAllByTestId } = renderWithRouter(<App />);
+    const filterButton = getAllByTestId('pokemon-type-button');
+    filterButton.forEach((elem) => {
+      userEvent.click(elem);
+      for (let i = 0; i < pokemons.length; i += 1) {
+        expect(getByTestId('pokemon-type').textContent).toBe(elem.textContent);
+        userEvent.click(getByTestId('next-pokemon'));
+      }
+    });
+  });
+
+  // https://github.com/tryber/sd-010-b-project-react-testing-library/pull/11/files
+  test('There is a reset button with the text `All`', () => {
+    const { getAllByRole, getByText } = renderWithRouter(<App />);
+    const button = getAllByRole('button');
+    expect(button[0]).toHaveTextContent('All');
+
+    userEvent.click(button[0]);
+
+    const nextPokemon = getByText('Próximo pokémon');
+    userEvent.click(nextPokemon);
+    const next = getByText(/charmander/i);
+    expect(next).toBeInTheDocument();
   });
 });
