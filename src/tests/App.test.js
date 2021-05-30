cb961042a0c08bd3d7e77ac20e353a10b9f66496
Mariana Savoldi <marianasavodi123@gmail.com>
2021-05-29 23:48:56 -0300

    Completei o que faltava no req 1 e comecei do 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
@@ -9,5 +10,23 @@ describe('Requirement 1', () => {
     expect(links[0]).toHaveTextContent(/Home/i);
     expect(links[1]).toHaveTextContent(/About/i);
     expect(links[2]).toHaveTextContent(/Favorite Pokémons/i);
+  });
+
+  test('Tests paths given by links', () => {
+    const { getByText, getAllByRole, history } = renderWithRouter(<App />);
+    const navLink = getAllByRole('link');
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(navLink[0]);
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(navLink[1]);
+    expect(history.location.pathname).toBe('/about');
+
+    userEvent.click(navLink[2]);
+    expect(history.location.pathname).toBe('/favorites');
+
+    history.push('/notfound');
+    expect(getByText('Page requested not found')).toBeInTheDocument();
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+// import Pokedex from '../components/Pokedex';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+// import pokemons from '../data';
+import App from '../App';
+
+describe('Requirement 5', () => {
+  test('There is a <h2> with text `Encountered pokémons`', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const h2 = 'Encountered pokémons';
+    const heading = getByRole('heading', {
+      level: 2,
+    });
+    expect(heading).toHaveTextContent(h2);
+  });
+
+  test('Show up the next pokémon when click the button', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const button = getByText('Próximo pokémon');
+    expect(button).toBeInTheDocument();
+
+    userEvent.click(button);
+    const pokemon = getByText('Charmander');
+    expect(pokemon).toBeInTheDocument();
+  });
+
+  test('Shows the first pokémon on the list, if it`s on the last', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const button = getByText('Próximo pokémon');
+    const clicked = 9;
+    for (let clicks = 0; clicks < clicked; clicks += 1) {
+      userEvent.click(button);
+    }
+    const first = getByText('Pikachu');
+    expect(first).toBeInTheDocument();
+  });
+});
