12d1ed11652a3f0d6eee27915a981c641e590b83
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-05-31 08:05:42 -0300

    Teste se existe os mapas da localizações

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -25,10 +25,22 @@ describe('Teste o componente <PokemonDetails.js />', () => {
   it('Teste se existe os mapas contendo as localizações do pokémon', () => {
     renderWithRouter(<App />);
 
+    const btnPoison = screen.getByRole('button', {  name: /poison/i})
+    userEvent.click(btnPoison);
+
     const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
     userEvent.click(linkMoreDetails);
 
-    const Location = screen.getByRole('heading', {  name: /game locations of pikachu/i});
+    const Location = screen.getByRole('heading', {  name: /game locations of ekans/i});
     expect(Location).toBeInTheDocument();
+
+    const nameLocation = screen.getByText(/goldenrod game corner/i);
+    expect(nameLocation).toBeInTheDocument();
+
+    const mapLocationImg = screen.getByRole('img', {  name: /ekans location/i});
+    expect(mapLocationImg).toBeInTheDocument();
+    expect(mapLocationImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png');
+    expect(mapLocationImg).toHaveAttribute('alt', 'Ekans location');
+
   });
 });
