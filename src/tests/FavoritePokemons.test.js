029492a9be366960c678271f8fdd56bb3cacf3cd
aythan azevedo <aythan16@gmail.com>
2021-05-31 16:58:27 -0300

    not found img  refatorado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -7,14 +7,14 @@ describe('testando  pokemons favoritos', () => {
   it('titulo', () => {
     renderWithRouter(<FavoritePokemons />);
     const titulo = screen.getByRole('heading', {
-      name: /Favorite pokéons /i,
+      name: /Favorite pokémons/i,
       level: 2,
     });
     expect(titulo).toBeInTheDocument();
   });
   it(' verifica quando não tem pokemons favoritos ', () => {
     renderWithRouter(<FavoritePokemons />);
-    const Notification = screen.getByText('No favorite pokemon found');
-    expect(Notification).toBeInTheDocument();
+    const notf = screen.getByText('No favorite pokemon found');
+    expect(notf).toBeInTheDocument();
   });
 });
