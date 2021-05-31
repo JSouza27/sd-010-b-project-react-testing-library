ac905cdc9b2f6ccc5afd66b9fe5ae666d223626a
Malbathan <malbathanlima@gmail.com>
2021-05-31 12:52:20 -0300

    App test finished

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './RenderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Routes', () => {
+  test('renders a reading with the text `Pokédex` and link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+
+    history.push('/');
+
+    const textHome = getByText('Home');
+
+    expect(textHome).toBeInTheDocument();
+  });
+  it('renders a reading with the link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/About');
+
+    const textHome = getByText('About');
+
+    expect(textHome).toBeInTheDocument();
+  });
+  it('renders a reading with the text `Favorite Pokémons`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/favorites');
+
+    const textHome = getByText('Favorite Pokémons');
+    expect(textHome).toBeInTheDocument();
+  });
+  it('tests if founds "Not Found" when going to a unknow path',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+
+      history.push('/not-existing-page');
+
+      const notFound = getByText(/not found/i);
+      expect(notFound).toBeInTheDocument();
+    });
 });
