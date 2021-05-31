f745f87a6f302c5185f8eb3fd80f4825b475a61e
CamilaCode7 <CamilaCode7>
2021-05-31 11:31:16 -0300

    Adicionando requisito 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
@@ -14,5 +15,31 @@ describe('Requirement 06', () => {
     expect(type).toHaveLength(2);
     expect(averageWeight).toBeInTheDocument();
     expect(img).toBeInTheDocument();
+  });
+  test('Testa se passa informações do link corretamente ', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link = getByText(/More details/i);
+    userEvent.click(link);
+    const title = getByText('Summary');
+    expect(title).toBeInTheDocument();
+  });
+  test('Testa se o conteudo da url muda', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const link = getByText(/More details/i);
+    userEvent.click(link);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/pokemons/25');
+  });
+  test('Testa se existe um ícone de estrela', () => {
+    const { getByText, getByAltText } = renderWithRouter(<App />);
+    const link = getByText(/More details/i);
+    userEvent.click(link);
+    const favorite = getByText(/Pokémon Favoritado?/i);
+    userEvent.click(favorite);
+    const favoriteimg = getByAltText(/is marked as favorite/);
+
+    expect(favoriteimg).toBeInTheDocument();
+    expect(favoriteimg).toHaveAttribute('src', '/star-icon.svg');
   });
 });
