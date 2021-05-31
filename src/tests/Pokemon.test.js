063fdbd0396a44ad997e891bb4d5b6b515b1c6be
CamilaCode7 <CamilaCode7>
2021-05-31 00:07:31 -0300

    Adicionando requirement 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Requirement 06', () => {
+  test('Testa informações corretas do pokemon', () => {
+    const { getByText, getByAltText, getAllByText } = renderWithRouter(<App />);
+    const name = getByText('Pikachu');
+    const type = getAllByText('Electric');
+    const averageWeight = getByText('Average weight: 6.0 kg');
+    const img = getByAltText('Pikachu sprite');
+
+    expect(name).toBeInTheDocument();
+    expect(type).toHaveLength(2);
+    expect(averageWeight).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+  })
+})
