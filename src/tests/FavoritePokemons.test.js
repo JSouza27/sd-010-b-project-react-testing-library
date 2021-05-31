bd3b43ed02252170a95a7e7507ad124e0ab7484d
aythan azevedo <aythan16@gmail.com>
2021-05-31 16:36:53 -0300

    pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testando  pokemons favoritos', () => {
+  it('titulo', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const titulo = screen.getByRole('heading', {
+      name: /Favorite pokéons /i,
+      level: 2,
+    });
+    expect(titulo).toBeInTheDocument();
+  });
+  it(' verifica quando não tem pokemons favoritos ', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const Notification = screen.getByText('No favorite pokemon found');
+    expect(Notification).toBeInTheDocument();
+  });
+});
