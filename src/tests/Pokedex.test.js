748de5cab4855c1b0373ced077e3c2be1f70aaac
CamilaCode7 <CamilaCode7>
2021-05-30 23:29:24 -0300

    Adicionando requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,3 +1,5 @@
+import { fireEvent } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
@@ -13,4 +15,53 @@ describe('Requirement 05', () => {
 
     expect(heading).toBeInTheDocument();
   });
+  test('Testa a funçao do botão', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const btn = getByText(/Próximo pokémon/);
+    userEvent.click(btn);
+    const charmander = getByText('Charmander');
+
+    expect(btn).toBeInTheDocument();
+    expect(charmander).toBeInTheDocument();
+    userEvent.click(btn);
+    userEvent.click(btn);
+    userEvent.click(btn);
+    userEvent.click(btn);
+    userEvent.click(btn);
+    userEvent.click(btn);
+    userEvent.click(btn);
+    userEvent.click(btn);
+    const pokemons = getByText('Pikachu');
+    expect(pokemons).toBeInTheDocument();
+  });
+  test('Testa se tem botão para resetar o filtro', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const btn = getAllByTestId('pokemon-type-button');
+    const types = [
+      'Electric',
+      'Fire',
+      'Bug',
+      'Poison',
+      'Psychic',
+      'Normal',
+      'Dragon',
+    ];
+    btn.forEach((button, index) => {
+      // Não lembrava como aplicar forEach. https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+      expect(button.textContent).toBe(types[index]);
+    });
+  });
+  test('Testa se botão all funciona corretamente', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const btn = getByRole('button', {
+      name: 'All',
+    });
+    fireEvent.click(btn);
+    const currentPokemon = getByText('Pikachu');
+
+    expect(btn).toBeInTheDocument();
+    expect(currentPokemon).toBeInTheDocument();
+  });
 });
+
+// https://github.com/tryber/sd-09-project-react-testing-library/blob/ChrisMoura2000-react-testing-library/src/tests/Pokedex.test.js
