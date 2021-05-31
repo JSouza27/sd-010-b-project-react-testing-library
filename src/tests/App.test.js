c6bc2709d55f82152ea99331e61000c86faffb70
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-05-30 21:59:35 -0300

    ajuste lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,37 @@
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+import React from 'react';
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Routes', () => {
+  test('renders a reading with the text `Pokédex` and link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+
+    history.push('/');
+
+    const textHome = getByText('Home');
+
+    expect(textHome).toBeInTheDocument();
+  });
+  it('renders a reading with the link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/About');
+
+    const textHome = getByText('About');
+
+    expect(textHome).toBeInTheDocument();
+  });
+  it('renders a reading with the text `Favorite Pokémons`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/About');
+
+    const textHome = getByText('Favorite Pokémons');
+
+    expect(textHome).toBeInTheDocument();
+  });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem `No favorite pokemon found`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/favorites');
+
+    const textFavorite = getByText('No favorite pokemon found');
+
+    expect(textFavorite).toBeInTheDocument();
+  });
+});
