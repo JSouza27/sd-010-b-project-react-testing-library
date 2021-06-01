6bfdf86bfa862eaef9099e6444efca08b0338296
walterlbf <walter.lbf.f@gmail.com>
2021-06-01 10:20:00 -0400

    requisito 3 100% no stryker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
-test('se é exibido na tela No favorite pokemon found, se não tiver favorito', () => {
+test('Se é exibido na tela No favorite pokemon found, se não tiver favorito', () => {
   const { getByText } = renderWithRouter(<FavoritePokemons />);
   const phrase = getByText(/No favorite pokemon found/i);
   expect(phrase).toBeInTheDocument();
+});
+
+test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const details = getByText(/More details/i);
+  userEvent.click(details);
+  const favorite = getByText(/Pokémon favoritado?/i);
+  userEvent.click(favorite);
+  const favoritePokemons = getByText(/Favorite Pokémons/i);
+  userEvent.click(favoritePokemons);
+  const namePokemon = getByText(/Pikachu/i);
+  expect(namePokemon).toBeInTheDocument();
 });
