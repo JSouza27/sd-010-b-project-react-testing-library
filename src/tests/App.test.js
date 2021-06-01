e975937c3ada579cff57e115c544d49d33a72a0d
Aladino Borges <eualadino@gmail.com>
2021-06-01 12:58:26 -0300

    Adiciona renderWithRouter e refatora o primeiro teste.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -13,7 +14,7 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
+/* test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -22,4 +23,15 @@ test('shows the Pokédex when the route is `/`', () => {
 
   const homepageText = getByText('Encountered pokémons');
   expect(homepageText).toBeInTheDocument();
+}); */
+
+describe('Testa o componente <App.js />', () => {
+  test('Teste se a página principal da App é renderizada ao caminho de URL `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const homepageText = getByText('Encountered pokémons');
+    const URL = '/';
+
+    history.push(URL);
+    expect(homepageText).toBeInTheDocument();
+  });
 });
