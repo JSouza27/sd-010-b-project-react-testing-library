39fdf34d5890f39764de4e6b230c48921c0c584c
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-05-31 23:15:20 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../components/RenderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testando App', () => {
+  test('Testar se a um H1 com o texto pokedex', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const h1 = getByRole('heading', { level: 1 });
+    expect(h1).toHaveTextContent(/Pokédex/i);
+  });
 });
