4ad581626d4b05d8cee9cfdf3a1eb468f7bba23a
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-06-01 11:03:14 -0300

    req3 implementation

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,28 +2,28 @@ import React from 'react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('Exibe a Pokedex quando a rota é `/`', () => {
+test('Verifica se exibe a Pokedex quando a rota é `/`', () => {
   const { getByText } = renderWithRouter(<App />);
   const initialPage = getByText(/Pokédex/i);
 
   expect(initialPage).toBeInTheDocument();
 });
 
-test('Topo da aplicação contém link com o texto `Home`', () => {
+test('Verifica se topo da aplicação contém link com o texto `Home`', () => {
   const { getByText } = renderWithRouter(<App />);
   const homeLink = getByText(/Home/i);
 
   expect(homeLink).toBeInTheDocument();
 });
 
-test('Topo da aplicação contém link com o texto `About`', () => {
+test('Verifica se topo da aplicação contém link com o texto `About`', () => {
   const { getByText } = renderWithRouter(<App />);
   const aboutLink = getByText(/About/i);
 
   expect(aboutLink).toBeInTheDocument();
 });
 
-test('Topo da aplicação contém link com o texto `Favorite Pokémons`', () => {
+test('Verifica se topo da aplicação contém link com o texto `Favorite Pokémons`', () => {
   const { getByText } = renderWithRouter(<App />);
   const favoritePokemonsLink = getByText(/Favorite Pokémons/i);
 
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,3 +1,23 @@
-// import React from 'react';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
 
-test('', () => {});
+test('Verifica se mensagem `No favorite pokemon found` aparece na tela', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+  const notFoundMessage = getByText('No favorite pokemon found');
+
+  expect(notFoundMessage).toBeInTheDocument();
+});
+
+test('Verifica se é exibido os cards dos pokémons favoritados', () => {
+  const { getAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+  expect(getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+});
+
+test('Verifica se nenhum card é exibido, se ele não estiver favoritado', () => {
+  const queryAllByTestId = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+  expect(queryAllByTestId('pokemon-name').length).toBe(0);
+});
