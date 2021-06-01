05889404c10eba848bbe0825ef064eeb3ba0e6f6
walterlbf <walter.lbf.f@gmail.com>
2021-06-01 11:12:42 -0400

    requisito 5 verif texto no botao e se possui proximo pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
@@ -10,4 +11,20 @@ test('Teste se página contém um heading com o texto Encountered pokémons', ()
   });
 
   expect(heading).toBeInTheDocument();
+});
+
+test('O botão deve conter o texto "Próximo Pokémon"', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const button = getByRole('button', {
+    name: /Próximo pokémon/i,
+  });
+  expect(button).toBeInTheDocument();
+});
+
+test('Botão deve conter Próximo pokémon', () => {
+  const nameId = 'next-pokemon';
+  const { getByTestId } = renderWithRouter(<App />);
+  const button = getByTestId(nameId);
+  userEvent.click(button);
+  expect(button).toBeInTheDocument();
 });
