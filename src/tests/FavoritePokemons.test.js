075d1185dd5e689590dfa30f92db1ff924cdfed0
walterlbf <walter.lbf.f@gmail.com>
2021-06-01 10:11:52 -0400

    requisito 3, verifica se possui a mensagem no favorite pokemon found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('se é exibido na tela No favorite pokemon found, se não tiver favorito', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const phrase = getByText(/No favorite pokemon found/i);
+  expect(phrase).toBeInTheDocument();
+});
