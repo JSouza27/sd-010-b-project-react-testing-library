01bc49f4d6d86f0e803b087d554e1a769fc07c78
walterlbf <walter.lbf.f@gmail.com>
2021-06-01 11:00:56 -0400

    requisito 5 verifica h2 com texto encountered pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+test('Teste se página contém um heading com o texto Encountered pokémons', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i,
+  });
+
+  expect(heading).toBeInTheDocument();
+});
