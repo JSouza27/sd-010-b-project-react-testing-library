ab803c32b73712d77b4f20c06f493fdf14fc2990
walterlbf <walter.lbf.f@gmail.com>
2021-06-01 10:37:19 -0400

    requisito 4 not found 100% no stryker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('Se página contém um h2 com o texto Page requested not found', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+
+  const pageNotFound = getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(pageNotFound).toBeInTheDocument();
+});
+test('Teste se página mostra a imagem', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const img = getByAltText(/Pikachu crying because the page requested was not found/i);
+
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
