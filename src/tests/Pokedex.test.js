57354a706be6082662d494304085e72ceaf4d1e5
Luiz Vicentin <luizpavanello@gmail.com>
2021-06-01 10:55:50 -0300

    Started the → 5. Teste o componente <Pokedex.js />

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokedex.js /', () => {
+  test('Teste se página contém um heading h2 com o texto `Encountered pokémons`.', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+});
