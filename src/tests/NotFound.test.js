7afbc784d5f56faa6253cffcfb26fc740ea89a21
Aderson Santos <assaderson@gmail.com>
2021-06-01 21:50:27 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Requisito 4', () => {
+  test('Verificar se página contém um heading h2 com o texto.', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+
+    const texto = getByText('Page requested not found');
+    expect(texto).toBeInTheDocument();
+  });
+
+  test('Verifica se página mostra a imagem.', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+
+    const imagem = getAllByRole('img')[1];
+    expect(imagem.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
